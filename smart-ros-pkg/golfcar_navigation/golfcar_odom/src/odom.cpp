232e61a37c0b4cc2b40c290a9cfaa252fbcddaf8
Demian Chong <chongzj@nus.edu.sg>
2011-07-08 10:03:17 +0000

    Odometry package update

---
+++ smart-ros-pkg/golfcar_navigation/golfcar_odom/src/odom.cpp
@@ -0,0 +1,72 @@
+#include <odom.h>
+
+namespace golfcar_odometry{
+//Constructor
+        golfcar_odometry::golfcar_odometry(ros::NodeHandle nh_):n(nh_)
+	{
+	        sampling_rate=200;
+		yaw = 0;
+		sub = n.subscribe("golfcar_sampler", 1000, &golfcar_odometry::samplerCallBack, this);
+		lodo = n.subscribe("pose2D", 1000, &golfcar_odometry::pose2DCallBack, this);
+		odom_pub = n.advertise<nav_msgs::Odometry>("odom", 100);
+		
+	}
+	
+	void golfcar_odometry::pose2DCallBack(geometry_msgs::Pose2D pose2d)
+	{
+		yaw = pose2d.theta;
+	}
+		
+	void golfcar_odometry::samplerCallBack(golfcar_halsampler::odo sampler)
+	{
+		
+		if(pose_init==0)
+		{
+			//initialize position as zero and take first reading of yaw_rate
+			pose_pre = sampler.pose;
+			time_pre = ros::Time::now().toSec();
+			speed_pre_ = sampler.vel;
+			pose_init++;
+			return;
+		}
+		
+		odom.twist.twist.linear.x = sampler.vel;
+
+		//calculate absolute x and y from heading and wheel encoder pose
+		double distance = sampler.pose - pose_pre;
+		pose_pre = sampler.pose;
+		odom.header.frame_id = "odom";
+		odom.header.stamp = ros::Time::now();
+		odom.pose.pose.position.x += distance * cos(yaw);
+		odom.pose.pose.position.y += distance * sin(yaw);
+		geometry_msgs::Quaternion odom_quat = tf::createQuaternionMsgFromYaw(yaw);
+		odom.pose.pose.orientation = odom_quat;
+		
+		static tf::TransformBroadcaster broadcaster_b;
+		
+		broadcaster_b.sendTransform(
+			tf::StampedTransform(
+				tf::Transform(tf::createQuaternionFromYaw(yaw), tf::Vector3(odom.pose.pose.position.x, odom.pose.pose.position.y, 0.0)),
+				ros::Time::now(),"odom", "base_link"));
+		odom_pub.publish(odom);
+	}
+	
+	
+  };
+	
+	
+
+int main(int argc, char **argv)
+{
+	ros::init(argc, argv, "golfcar_odom");
+	ros::NodeHandle nh_;
+	golfcar_odometry::golfcar_odometry *odom_ = new golfcar_odometry::golfcar_odometry(nh_);
+	
+	odom_->pose_init =0;
+	odom_->pose_pre=0.0;
+	odom_->pose_zero=0.0;
+	
+	ros::spin();
+	
+	return 0;
+}
---
+++ smart-ros-pkg/golfcar_navigation/golfcar_odom/src/odom_imu2.cpp
@@ -0,0 +1,89 @@
+#include <odom_imu2.h>
+
+
+namespace golfcar_odometry_imu{
+//Constructor
+        golfcar_odometry_imu::golfcar_odometry_imu(ros::NodeHandle nh_):n(nh_)
+	{
+		sub = n.subscribe("golfcar_sampler", 1000, &golfcar_odometry_imu::samplerCallBack, this);
+		imu = n.subscribe("imu/data", 1000, &golfcar_odometry_imu::imuCallBack, this);
+		
+		odom_pub = n.advertise<nav_msgs::Odometry>("odom", 100);
+		initialized = false;
+		
+	}
+	
+	void golfcar_odometry_imu::imuCallBack(sensor_msgs::Imu imu)
+	{
+		imuQ = imu.orientation;
+	}
+
+	void golfcar_odometry_imu::samplerCallBack(golfcar_halsampler::odo sampler)
+	{
+
+	   static tf::TransformBroadcaster broadcaster_b;
+		tf::Quaternion qt_temp;
+		btScalar pitch, roll, yaw;
+		tf::Quaternion qt;
+		
+		tf::quaternionMsgToTF(imuQ, qt);
+		
+		btMatrix3x3(qt).getRPY(roll, pitch, yaw);
+		//handle the nan issue. This occur when the imu is restarted 
+		if(isnan(yaw)||isnan(pitch)||isnan(roll)) 
+			return;
+
+		
+		btMatrix3x3 btm;
+		
+		if(!initialized) 
+		{
+			yaw_pre = yaw;
+			pose_pre = sampler.pose;
+			initialized = true;
+		}
+		//Ensure that the orientation always start from yaw=0. That's the assumption made for odometry calculation
+		yaw -= yaw_pre;
+		btm.setRPY(roll, pitch, yaw);
+		btm.getRotation(qt_temp);
+		geometry_msgs::Quaternion qMsg;
+		tf::quaternionTFToMsg(qt_temp,qMsg);
+		
+		double r11 = cos(yaw)*cos(pitch);
+		double r21 = sin(yaw)*cos(pitch);
+		double r31 = sin(pitch);
+		double distance = sampler.pose - pose_pre;
+		pose_pre = sampler.pose;
+		
+		odom.header.frame_id = "odom";
+		odom.header.stamp = ros::Time::now();
+		
+		odom.pose.pose.position.x+=distance * r11;
+		odom.pose.pose.position.y+=distance * r21;
+		odom.pose.pose.position.z-=distance * r31;
+		odom.pose.pose.orientation = qMsg;
+		
+		broadcaster_b.sendTransform(
+			tf::StampedTransform(
+				tf::Transform(qt_temp,tf::Vector3(odom.pose.pose.position.x, odom.pose.pose.position.y, odom.pose.pose.position.z)),
+				ros::Time::now(),"odom", "base_link"));
+		
+		odom_pub.publish(odom);
+	}
+	
+	
+  };
+	
+	
+
+int main(int argc, char **argv)
+{
+	ros::init(argc, argv, "golfcar_odom_imu");
+	ros::NodeHandle nh_;
+	golfcar_odometry_imu::golfcar_odometry_imu *odom_ = new golfcar_odometry_imu::golfcar_odometry_imu(nh_);
+	
+
+	ros::spin();
+	
+	return 0;
+}
