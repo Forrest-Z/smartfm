d817018f6a36e078a0b982bd3d6f475ad7d320e6
Pratik Chaudhari <pratikac@mit.edu>
2011-07-14 06:19:57 +0000

    Destructor + tranformation of map

--- smart-ros-pkg/golfcar_perception/local_map/include/local_map.h
+++ smart-ros-pkg/golfcar_perception/local_map/include/local_map.h
@@ -1,24 +1,37 @@
 #include <iostream>
 #include <stdlib.h>
 #include <vector>
+#include <math.h>
+#include <string.h>
 using namespace std;
 
-#define MAP_RES     (0.05)
+#define MAP_RES     (0.1)
 #define MAP_WIDTH   (5.0)
-#define MAP_HEIGHT  (5.0)
+#define MAP_HEIGHT  (10.0)
+
+#define XORIGIN (xsize/2.0)
+#define YORIGIN (ysize/4.0)
 
 #define CELL_LIN(x, y)      ((x) + (y)*ysize)
 
-#define REDUCE_BELIEF_DELTA     (3)
-#define INCREASE_BELIEF_DELTA   (3)
+// essentially says p(obs|laser) / p(not obs|laser) = 10
+#define REDUCE_BELIEF_DELTA     (1)
+#define INCREASE_BELIEF_DELTA   (10)
 
-struct Point
+class Pose
 {
-    float x[3];
+    public:
+        float x[3];
+        Pose(){};
+        Pose(float xin, float yin, float thin)
+        {
+            x[0] = xin;
+            x[1] = yin;
+            x[2] = thin;
+        }
 };
-typedef struct Point Point;
 
-class local_map
+class Local_map
 {
     private:
         int xsize;
@@ -26,14 +39,19 @@ class local_map
 
     public:
         
-        local_map();
-        ~local_map(){};
+        Local_map();
+        ~Local_map();
 
+        // map of obstacle beliefs
         unsigned char *map;
-        void map_init();
-        void map_free();
-       
-        void reduce_belief();
-        void process_points( vector<Point> points);
+        unsigned char *map_copy;
 
+        void reinit_map();
+
+        inline int get_cell_num(Pose p, int &x, int &y);
+        inline Pose get_cell_coor(int x, int y);
+        void reduce_belief();
+        void process_points( vector<Pose> points);
+        inline Pose transform_pose(Pose src, Pose trans);
+        void transform_map(Pose prev, Pose curr);
 };
--- smart-ros-pkg/golfcar_perception/local_map/src/local_map.cpp
+++ smart-ros-pkg/golfcar_perception/local_map/src/local_map.cpp
@@ -1,19 +1,33 @@
 #include <local_map.h>
 
-
-local_map::local_map()
+Local_map::Local_map()
 {
     xsize = MAP_WIDTH/MAP_RES;
     ysize = MAP_HEIGHT/MAP_RES;
+    
+    map = new unsigned char[ xsize*ysize];
+    map_copy = new unsigned char[ xsize*ysize];
 }
 
-// returns 1 if point outside the local map
-inline int get_cell_num(Point p, int &x, int &y)
+Local_map::~Local_map()
 {
-    float px = Point.x[0];
-    float py = Point.x[1];
-    x = px/MAP_RES + xsize/2.0;
-    y = py/MAP_RES + ysize/2.0;
+    delete[] map;
+    delete[] map_copy;
+}
+
+void Local_map::reinit_map()
+{
+    memset(map, 0, xsize*ysize);
+    memset(map_copy, 0, xsize*ysize);
+}
+
+// returns 1 if pose outside the local map
+inline int Local_map::get_cell_num(Pose p, int &x, int &y)
+{
+    float px = p.x[0];
+    float py = p.x[1];
+    x = px/MAP_RES + XORIGIN;
+    y = py/MAP_RES + YORIGIN;
 
     if( ( (x > xsize) || (x < 0) ) || ( (y > ysize) || (y < 0) ) )
         return 1;
@@ -21,29 +35,33 @@ inline int get_cell_num(Point p, int &x, int &y)
         return 0;
 }
 
-void local_map::map_init()
+inline Pose Local_map::get_cell_coor(int x, int y)
 {
-    map = (unsigned char *) calloc(sizeof(unsigned char), (int)(xsize*ysize));
+    Pose pret;
+    pret.x[0] = (x - XORIGIN)*MAP_RES;
+    pret.x[1] = (y - YORIGIN)*MAP_RES;
+
+    return pret;
 }
 
-void local_map::reduce_belief()
+void Local_map::reduce_belief()
 {
     for(int i=0; i < xsize; i++)
     {
         for(int j=0; j< ysize; j++)
         {
             unsigned char val = map[ CELL_LIN(i, j) ];
-            map[ CELL_LIN(i, j) ] = MAX(0, val- REDUCE_BELIEF_DELTA);
+            map[ CELL_LIN(i, j) ] = max(0, val- REDUCE_BELIEF_DELTA);
         }
     }
 }
 
-void local_map::process_points(vector<Points> points)
+void Local_map::process_points(vector<Pose> points)
 {
     reduce_belief();
 
-    float xcell, ycell;
-    for(int i=0; i< points.size(); i++)
+    int xcell, ycell;
+    for(unsigned int i=0; i< points.size(); i++)
     {
 
         int res = get_cell_num(points[i], xcell, ycell);
@@ -51,8 +69,59 @@ void local_map::process_points(vector<Points> points)
         if(res == 0)
         {
             unsigned char val = map[map_loc  ];
-            map[ map_loc ] = MIN(val+INCREASE_BELIEF_DELTA, 255);
+            map[ map_loc ] = min(val+INCREASE_BELIEF_DELTA, 255);
         }
     }
 };
+
+inline Pose Local_map::transform_pose(Pose src, Pose trans)
+{
+    Pose dst;
+    float cth = cos(trans.x[2]);
+    float sth = sin(trans.x[2]);
+
+    dst.x[0] = src.x[0]*cth - src.x[1]*sth + trans.x[0];
+    dst.x[1] = src.x[0]*sth + src.x[1]*cth + trans.x[1];
+
+    return dst;
+}
+
+void Local_map::transform_map(Pose prev, Pose curr)
+{
+    float delx = curr.x[0] - prev.x[0];
+    float dely = curr.x[1] - prev.x[1];
+    float delth= curr.x[2] - prev.x[2];
+    
+    memcpy(map_copy, map, xsize*ysize);
+
+    for(int i=0; i < xsize; i++)
+    {
+        for(int j=0; j< ysize; j++)
+        {
+            Pose curr_point = get_cell_coor(i, j);
+            Pose new_point = transform_pose(curr_point, Pose(delx, dely, delth) );
+            
+            int newx, newy;
+            int res = get_cell_num(new_point, newx, newy);
+            if(res == 0)
+            {
+                // copy old val into new map
+                map[ CELL_LIN(newx, newy)] = map_copy[ CELL_LIN(i, j) ];     
+            }
+        }
+    }
+    memcpy(map_copy, map, xsize*ysize);
+}
+
+int main()
+{
+    Local_map lmap;
+    Pose p1(10, 10, 0);
+    Pose trans(1, 1, 0.2);
+
+    Pose newp = lmap.transform_pose(p1, trans);
+    cout<<"newp: "<< newp.x[0] <<" " << newp.x[1] <<" "<< newp.x[2] << endl;
+
+    return 0;
+}
 
