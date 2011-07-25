eeb596b6854d2140880ad1b10b3222e315aaedb4
Leon Liuxue <leonliuxue@gmail.com>
2011-07-25 09:01:07 +0000

    added communicate with car, website

--- smart-ros-pkg/server/src/fmui/ClientData.java
+++ smart-ros-pkg/server/src/fmui/ClientData.java
@@ -2,6 +2,7 @@ package fmui;
 
 import java.net.*;
 import java.io.*;
+import java.util.*;
 
 class ClientData {
     
@@ -15,8 +16,18 @@ class ClientData {
     public String wait = "0";
     public String carID = "0";
     public String taskID = "0";
+    public String latitude = "0";
+    public String longitude = "0";
     public BufferedReader in;
     public PrintWriter out;
+    
+       
+    private final int USER = 1;
+    private final int SCHEDULER = 2;
+    private final int CAR = 3;
+    private final int DESKTOP = 4;
+    private final int ERR = 5;
+    
 	
     public ClientData(Socket socket) throws IOException {
         this.socket = socket;
@@ -24,28 +35,17 @@ class ClientData {
         out = new PrintWriter(socket.getOutputStream(), true);
     }
 	
-    public void readMsg() throws IOException {
+    public boolean readMsg(String IDcheck) throws IOException {
         String msg = null; 
-        while(in.ready() && (msg = in.readLine())!=null) {    	
+        while(in.ready()&&(msg = in.readLine())!=null) {    	
             if (msg.startsWith(";")) {
             	int f = msg.indexOf(":");
             	ID = msg.substring(1,f);
 				
 				System.out.println("CHeck msg to Server: " + msg);
 				
-            	// store User data. --- ;ID:taskID:pickup:dest
-            	if (ID.compareTo("0")!=0)
-            	{
-	            	int s = msg.indexOf(":", f+1);
-	            	int t = msg.indexOf(":", s+1);
-	            	
-	            	taskID = msg.substring(f+1,s);
-	                pickup = msg.substring(s+1,t);
-	                dest = msg.substring(t+1);
-	                break;
-                }
-            	else // store Scheduler data. --- ;ID:user_id:taskID:wait:carID
-            	{
+				 // store Scheduler data. --- ;ID:user_id:taskID:wait:carID
+				if(IDcheck.compareTo("0")==0&&ID.compareTo("0")==0) {
 	            	int s = msg.indexOf(":", f+1);
 	            	int t = msg.indexOf(":", s+1);
 	            	int i = msg.indexOf(":", t+1);
@@ -54,11 +54,86 @@ class ClientData {
 	            	taskID = msg.substring(s+1,t);
 	                wait = msg.substring(t+1,i);
 	                carID = msg.substring(i+1);
-	                break;
-            	}
+	                //break;
+	                return true;
+            	} else if(IDcheck.compareTo("car")==0&&ID.compareTo("car")==0){
+                                     
+	            	int s = msg.indexOf(":", f+1);
+	            	int t = msg.indexOf(":", s+1);
+
+                    taskID = msg.substring(f+1,s);
+                    latitude = msg.substring(s+1,t);
+                    longitude = msg.substring(t+1);
+                    //break;
+                    return true;
+                } else if(IDcheck.compareTo("Desktop")==0&&ID.compareTo("Desktop")==0) {
+                	//break;
+                	return true;
+                } else  {	// store User data. --- ;ID:taskID:pickup:dest
+	            	int s = msg.indexOf(":", f+1);
+	            	int t = msg.indexOf(":", s+1);
+	            	
+	            	taskID = msg.substring(f+1,s);
+	                pickup = msg.substring(s+1,t);
+	                dest = msg.substring(t+1);
+	                //break;
+	                return true;
+                }  
             }
         }
+        return false;
     }
+    
+    public String checkID() throws IOException{
+    	String msg = in.readLine();
+    	String ID = null;
+    	int f = msg.indexOf(":");
+    	
+    	if (msg.startsWith(";")){
+    		ID = msg.substring(1,f);
+    		return ID;
+    	} 
+    	
+    	return ID;
+    	
+    }
+    
+    
+    
+    
+     public int initConnection() throws IOException{
+		String msg = null;
+		
+
+		
+    	while((msg = in.readLine())!=null) {   
+    	
+    	System.out.println("CHeck msg to Server: " + msg);
+    	
+    	if (msg.startsWith(";")){
+    		int f = msg.indexOf(":");
+    		ID = msg.substring(1,f);
+    	}
+    	
+    	if (ID!=null) {
+    	if (ID.compareTo("0")==0)
+    		return SCHEDULER;
+    	else if (ID.compareTo("car")==0)
+    		return CAR;
+    	else if (ID.compareTo("Desktop")==0)
+    		return DESKTOP;
+    	else
+    		return USER;
+    	} else 
+    		return ERR;   	
+    	}
+
+    	return ERR;
+    }
+    
+    
+    
+    
     
     public void reconnect(ClientData c) throws IOException {
         socket = c.socket;
--- smart-ros-pkg/server/src/fmui/Server.java
+++ smart-ros-pkg/server/src/fmui/Server.java
@@ -2,12 +2,11 @@ package fmui;
 
 import java.net.*;
 import java.io.*;
-
+import java.util.*;
 import java.lang.Runnable;
 
 public class Server implements Runnable {
-    
-    /** A server instance.
+ /** A server instance.
 	 Listens for incoming connections on a given port. Once a client connects,
 	 update the central database.
 	 */
@@ -19,12 +18,25 @@ public class Server implements Runnable {
     int port = -1;
     boolean toScheduler = false;
     boolean toUser = false;
+    //boolean toWeb = false;
+    boolean toWeb = true;
     String pickup = "0";
     String dest = "0";
     String user_id = "0";
     String wait = "0";
     String carID = "0";
     String taskID = "0";
+    String latitude = "0";
+    String longitude = "0";
+    
+    
+     private int clientType = -1;
+    
+    private final int USER = 1;
+    private final int SCHEDULER = 2;
+    private final int CAR = 3;
+    private final int DESKTOP = 4;
+    private final int ERR = 5;
     
     
     public Server(int port, CentralDataBase db) throws IOException {
@@ -35,9 +47,12 @@ public class Server implements Runnable {
 	
     public void run() {
 		while (true) {
+
             ClientData c = null;
             try {
+            	System.out.println(serverSocket.toString());
                 c = new ClientData(serverSocket.accept());
+                System.out.println("after"+serverSocket.toString());
             } catch (IOException e) {
                 // Print the error and exit
                 // A better mechanism should be implemented to continue accepting connections...
@@ -45,15 +60,29 @@ public class Server implements Runnable {
                 System.exit(1);
             }
 			try{
-				c.readMsg();
+
 				
-				if (c.ID.compareTo("0")==0) {
-					System.out.println("Server");
+				clientType = c.initConnection();
+				
+				
+				if (clientType==SCHEDULER) {
+					System.out.println("Scheduler");
 					new ServerThread(c).start();
-				} else {
+				} else if (clientType==DESKTOP){
+                    System.out.println("Desktop");
+                    new DesktopThread(c).start();
+                } else if (clientType==CAR){
+                    System.out.println("Car");
+                    new CarThread(c).start();
+                } else if (clientType==USER){
 					System.out.println("User");
 					new UserThread(c).start();
-				}
+			    } else
+			    	System.out.println("Unknown type." );
+	               // System.exit(1);
+	                
+	                
+                
 			} catch (IOException e) {
                 System.err.println("IOException occured: " + e.getMessage());
                 continue;
@@ -88,12 +117,15 @@ public class Server implements Runnable {
 				}	
 				System.out.println("Number of clients: " + db.clients.size() +"\n\n");
 			}
+			
+			
+			/*
 			// Sleep 2s
             try {
                 Thread.sleep(2000);
             } catch( InterruptedException e ) {
             }
-			
+			*/
 			
     	}
 	}
@@ -125,12 +157,14 @@ public class Server implements Runnable {
 							dest = "0";
 							
 							try {
-             				   Thread.sleep(1000);
+             				   Thread.sleep(500);
 					            } catch( InterruptedException e ) {
             				}
 						} else{ // if not order, keep reading from Scheduler
 							// read msgs from Scheduler
-							cd.readMsg();
+							
+							if (cd.readMsg("0")){
+							
 							// Scheduler sends feedback to none-zero User ID
 							if (cd.user_id.compareTo("0")!=0)
 							{
@@ -145,6 +179,7 @@ public class Server implements Runnable {
 								cd.user_id = "0";
 								cd.wait="0";
 								cd.carID="0";
+							}
 							}
 						}
 					} catch (IOException e) {
@@ -178,12 +213,12 @@ public class Server implements Runnable {
 							toUser = false;
 							
 							try {
-             				   Thread.sleep(1000);
+             				   Thread.sleep(500);
 					            } catch( InterruptedException e ) {
             				}
 						} else {
 							// read msg from User
-							cd.readMsg();
+							if (cd.readMsg("user")) {
 							// if User sends order, receive it
 							if (cd.pickup.compareTo("0")!=0&&cd.dest.compareTo("0")!=0)
 							{
@@ -199,6 +234,7 @@ public class Server implements Runnable {
 								cd.dest = "0";
 								cd.user_id = "0";
 							}	
+							}
 						}
 					}		
 				}catch (IOException e) {
@@ -208,4 +244,96 @@ public class Server implements Runnable {
 			}
     	} 
     }
+
+    private class CarThread extends Thread{
+        private ClientData cd = null;
+
+        public CarThread(ClientData cd){
+            this.cd = cd;
+        }
+
+        public void run(){
+            while(true){
+                try{  
+                    synchronized(lockObject){
+
+                        if (cd.readMsg("car")){
+                        
+                        if(cd.taskID.compareTo("1")==0){
+                            System.out.println("Car-->Server");
+                            latitude = cd.latitude;
+                            longitude = cd.longitude;
+
+                            System.out.println("lat: "+latitude+" long: "+longitude);                            
+                            
+                            
+                            toWeb = true;
+                        } 
+                            }
+
+                    }
+                } catch (IOException e) {
+                	System.err.println("IOException occured: " + e.getMessage());
+					continue;
+                }
+            }
+        }
+
+    }
+    
+    private class DesktopThread extends Thread {
+    	
+	public int count =0;
+	private ClientData cd = null;
+	String coordinates = 	"1.298529" +" "+ "103.770853" +"\n"+
+				"1.298718" +" "+ "103.770778" +"\n"+
+				"1.299048" +" "+ "103.770617" +"\n"+
+				"1.299070" +" "+ "103.770118" +"\n"+
+				"1.299352" +" "+ "103.769957" +"\n"+
+				"1.300279" +" "+ "103.770150" +"\n"+
+				"1.300775" +" "+ "103.770702" +"\n"+
+				"1.300579" +" "+ "103.771263";
+	
+	public DesktopThread(ClientData cd) {
+    		this.cd = cd;
+    	}
+		
+    	public void run() {
+			while(true){
+				try{
+					synchronized(lockObject){
+						if (toWeb) {
+						//Put code here
+						
+						/*
+						ArrayList arr = new ArrayList(); //size determined from arr.size();
+						arr.add(coordinates);
+						if (!arr.isEmpty()) {
+						  Object temp = arr.get(arr.size()-1);
+						  cd.out.println(temp);		
+							toWeb = false;
+                        }
+                        */
+                        
+                        cd.out.println(latitude+" "+longitude);
+                        toWeb = false;
+                        
+                        
+                         try {
+             				   		Thread.sleep(500);
+					        } catch( InterruptedException e ) {
+								System.out.println("Interrupted");
+            				}
+            			}
+					}
+				} catch (Exception e) {
+					System.err.println("IOException occured: " + e.getMessage());
+					continue;
+				}
+			}
+    	} 
+    }
+    
+    
+    
 }
