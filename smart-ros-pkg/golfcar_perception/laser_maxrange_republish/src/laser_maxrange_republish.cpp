113aeb56199a285acf105fd804b923ebac7afbee
Demian <chongzj@nus.edu.sg>
2011-11-14 11:52:14 +0800

    add laser_maxrange_republish for sick laser

---
+++ smart-ros-pkg/golfcar_perception/laser_maxrange_republish/src/laser_maxrange_republish.cpp
@@ -0,0 +1,43 @@
+/*
+ * laser_maxrange_republish.cpp
+ *
+ *  Created on: Nov 12, 2011
+ *      Author: golfcar
+ */
+
+#include "ros/ros.h"
+#include "std_msgs/String.h"
+#include "sensor_msgs/LaserScan.h"
+
+ros::Publisher *laser_pub_;
+void scanCallback(const sensor_msgs::LaserScan msg)
+{
+	sensor_msgs::LaserScan ls;
+	ls = msg;
+	double max_range_buffer = 0.5;
+	for (unsigned int i=0;i<ls.ranges.size();i++)
+	{
+		if(ls.ranges[i]>ls.range_max)
+		{
+			ls.ranges[i]=ls.range_max-max_range_buffer;
+		}
+		else
+		{
+			ls.ranges[i]=ls.range_max+max_range_buffer;
+		}
+	}
+	laser_pub_->publish(ls);
+}
+
+int main(int argc, char **argv)
+{
+
+  ros::init(argc, argv, "listener");
+  ros::NodeHandle n;
+  ros::Publisher laser_pub= n.advertise<sensor_msgs::LaserScan>("sick_scan2_maxrange",1);
+  laser_pub_ = &laser_pub;
+  ros::Subscriber sub = n.subscribe("sick_scan2", 1, scanCallback);
+  ros::spin();
+
+  return 0;
+}
