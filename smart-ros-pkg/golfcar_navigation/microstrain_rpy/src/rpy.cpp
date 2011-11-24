2c2e912fe53b7bdeff4c35aeda6def639f3dafa2
Brice Rebsamen <brice.rebsamen@gmail.com>
2011-11-24 23:43:03 +0800

    minor edit

--- smart-ros-pkg/golfcar_navigation/microstrain_rpy/src/rpy.cpp
+++ smart-ros-pkg/golfcar_navigation/microstrain_rpy/src/rpy.cpp
@@ -1,56 +1,51 @@
-/*
- * laser_maxrange_republish.cpp
- *
- *  Created on: Nov 14, 2011
- *      Author: golfcar
- */
+/// Republishes IMU messages as imu_rpy messages (roll, pitch and yaw in degrees).
 
-#include "ros/ros.h"
-#include "sensor_msgs/Imu.h"
-#include "microstrain_rpy/imu_rpy.h"
-#include "tf/transform_datatypes.h"
-#include "tf/transform_broadcaster.h"
+#include <ros/ros.h>
+#include <tf/transform_datatypes.h>
+#include <tf/transform_broadcaster.h>
+#include <sensor_msgs/Imu.h>
+#include <microstrain_rpy/imu_rpy.h>
 
 ros::Publisher *imu_pub_;
+
 void imuCallback(const sensor_msgs::Imu::ConstPtr msg)
 {
-	
-	//added to publish rpy and tf
-	static tf::TransformBroadcaster broadcaster_b;
-	tf::Quaternion qt_temp;
-	btScalar pitch, roll, yaw;
-	tf::Quaternion qt;
-	tf::quaternionMsgToTF(msg->orientation, qt);
-	btMatrix3x3(qt).getRPY(roll, pitch, yaw);
+    //added to publish rpy and tf
+    static tf::TransformBroadcaster broadcaster_b;
+    tf::Quaternion qt_temp;
+    btScalar pitch, roll, yaw;
+    tf::Quaternion qt;
+    tf::quaternionMsgToTF(msg->orientation, qt);
+    btMatrix3x3(qt).getRPY(roll, pitch, yaw);
 
-	//correct the roll angle from +-180 to +-0
-	//if(roll>=0) roll-=M_PI;
-	//else roll+=M_PI;
+    //correct the roll angle from +-180 to +-0
+    //if(roll>=0) roll-=M_PI;
+    //else roll+=M_PI;
 
-	btMatrix3x3 btm;
-	btm.setRPY(roll, pitch, yaw);
-	btm.getRotation(qt_temp);
-	microstrain_rpy::imu_rpy rpy;
-	
-	rpy.roll = roll/M_PI*180;
-	rpy.pitch = pitch/M_PI*180;
-	rpy.yaw = yaw/M_PI*180;
-	broadcaster_b.sendTransform(
-			tf::StampedTransform(
-				tf::Transform(qt_temp, tf::Vector3(0,0,0)),
-				ros::Time::now(),"ms_imu_base", "ms_imu"));
-	imu_pub_->publish(rpy);
+    btMatrix3x3 btm;
+    btm.setRPY(roll, pitch, yaw);
+    btm.getRotation(qt_temp);
+    microstrain_rpy::imu_rpy rpy;
+
+    rpy.roll = roll/M_PI*180;
+    rpy.pitch = pitch/M_PI*180;
+    rpy.yaw = yaw/M_PI*180;
+    broadcaster_b.sendTransform(
+        tf::StampedTransform(
+            tf::Transform(qt_temp, tf::Vector3(0,0,0)),
+                             ros::Time::now(), "ms_imu_base", "ms_imu"));
+    imu_pub_->publish(rpy);
 }
+
 
 int main(int argc, char **argv)
 {
+    ros::init(argc, argv, "microstrain_rpy");
+    ros::NodeHandle n;
+    ros::Publisher imu_pub= n.advertise<microstrain_rpy::imu_rpy>("rpy",1);
+    imu_pub_ = &imu_pub;
+    ros::Subscriber sub = n.subscribe("data", 1, imuCallback);
+    ros::spin();
 
-  ros::init(argc, argv, "microstrain_rpy");
-  ros::NodeHandle n;
-  ros::Publisher imu_pub= n.advertise<microstrain_rpy::imu_rpy>("rpy",1);
-  imu_pub_ = &imu_pub;
-  ros::Subscriber sub = n.subscribe("data", 1, imuCallback);
-  ros::spin();
-
-  return 0;
+    return 0;
 }
