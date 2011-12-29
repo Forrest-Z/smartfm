1511eac0453a6de01ec211891644dc5eaab31e92
Brice Rebsamen <brice.rebsamen@gmail.com>
2011-12-29 12:16:03 +0800

    Simplified golfcar_route_planner/simple_goal. Using StationList for listing and prompting stations.

--- smart-ros-pkg/golfcar_navigation/golfcar_route_planner/src/simple_goal.cpp
+++ smart-ros-pkg/golfcar_navigation/golfcar_route_planner/src/simple_goal.cpp
@@ -1,65 +1,53 @@
+#include <iostream>
+
 #include <ros/ros.h>
 #include <geometry_msgs/PoseStamped.h>
 #include <geometry_msgs/PointStamped.h>
 #include <tf/transform_listener.h>
 #include <tf/transform_datatypes.h>
 
+#include "StationPath.h"
+
 using namespace std;
-class SimpleGoal
-{
-public:
-    SimpleGoal();
-    void goalCallback(geometry_msgs::PoseStamped map_pose);
-    void on_goal_timer(const ros::TimerEvent& e);
-
-    ros::NodeHandle n;
-    ros::Subscriber sub;
-    ros::Publisher waypoint_pub_;
-    ros::Timer goal_timer;
-    tf::TransformListener listener;
-    
-    double current_point;
-};
 
 
+/**
+ * Prompts passenger for destination (station number), and publishes it on
+ * "move_base_simple/goal" as geometry_msgs::PoseStamped messages:
+ * goal.x is the current station and goal.y is the destination station.
+ */
 
-SimpleGoal::SimpleGoal()
-{
-    waypoint_pub_ = n.advertise<geometry_msgs::PoseStamped>("move_base_simple/goal", 100);
-    goal_timer = n.createTimer(ros::Duration(0.5), &SimpleGoal::on_goal_timer, this);
-    current_point = -1;
-}
-
-void SimpleGoal::on_goal_timer(const ros::TimerEvent& e)
-{
-    geometry_msgs::PoseStamped goal;
-    double n;
-    if(current_point==-1) 
-    {
-        cout<<"Current station: ";
-        cin>>current_point;
-        
-        cout<<"Current station is "<<current_point<<endl;
-    }
-    goal.header.stamp = ros::Time::now();
-    goal.header.frame_id = "/map";
-    
-    goal.pose.position.x = current_point;
-    cout<<"Next station: ";
-    cin>>n;
-    cout<<"Next station is "<<n<<endl;
-    goal.pose.position.y = n;
-    current_point = n;
-    goal.pose.orientation.z = 1.0;
-    ROS_INFO("send goal: [%f %f]", goal.pose.position.x, goal.pose.position.y);
-    waypoint_pub_.publish(goal);
-    //(int)goal.pose.position.y;
-}
 
 int main(int argc, char **argv)
 {
     ros::init(argc, argv, "goal_talker");
-    SimpleGoal sg;
-    ros::spin();
+    ros::NodeHandle nh;
+    ros::Publisher waypoint_pub_ = nh.advertise<geometry_msgs::PoseStamped>("move_base_simple/goal", 100);
+
+    geometry_msgs::PoseStamped goal;
+    StationList stations;
+
+    stations.print();
+    Station s = stations.prompt("Current station ?");
+    cout <<"Current station is " <<s.str() <<endl;
+    goal.pose.position.x = (double) s.number();
+
+    while( ros::ok() )
+    {
+        stations.print();
+        s = stations.prompt("Next station ?");
+        cout <<"Next station is " <<s.str() <<endl;
+        
+        goal.header.stamp = ros::Time::now();
+        goal.header.frame_id = "/map";
+        
+        goal.pose.position.y = (double) s.number();
+        goal.pose.orientation.z = 1.0;
+        waypoint_pub_.publish(goal);
+        
+        ROS_INFO("sent goal: [%f %f]", goal.pose.position.x, goal.pose.position.y);
+        goal.pose.position.x = goal.pose.position.y;
+    }
+
     return 0;
 }
