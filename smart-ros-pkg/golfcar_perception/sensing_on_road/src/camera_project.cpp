719c56b2d7c9d3ebe166403ff53353003a664327
Brice Rebsamen <brice.rebsamen@gmail.com>
2012-02-09 15:18:20 +0800

    in sensing_on_road: deleted camera_project.h and put all the content in the cpp file.

--- smart-ros-pkg/golfcar_perception/sensing_on_road/src/camera_project.cpp
+++ smart-ros-pkg/golfcar_perception/sensing_on_road/src/camera_project.cpp
@@ -2,10 +2,24 @@
  * This class is to project object position in 3D world onto 2D image;
  * Reference: "Fast Extrinsic Calibration of a Laser Rangefinder to a Camera"
  * http://www.ri.cmu.edu/publication_view.html?pub_id=5293
- */ 
+ */
 
 
-#include "camera_project.h"
+#include <ros/ros.h>
+#include <sensor_msgs/PointCloud.h>
+#include <geometry_msgs/Point32.h>
+#include <vector>
+#include <tf/transform_listener.h>
+#include <cmath>
+
+#include <tf/message_filter.h>
+#include <message_filters/subscriber.h>
+
+#include "sensing_on_road/pedestrian_laser.h"
+#include "sensing_on_road/pedestrian_laser_batch.h"
+#include "sensing_on_road/pedestrian_vision.h"
+#include "sensing_on_road/pedestrian_vision_batch.h"
+
 
 using namespace std;
 using namespace ros;
@@ -14,13 +28,68 @@ using namespace ros;
 #define PIXEL_ALLOWANCE 30
 #define LASER_MOUNTING_HEIGHT 1.0
 
-namespace camera_projector
+
+
+class camera_calib
 {
+public:
+    double fc[2];   //focal_length
+    double cc[2];   //principal_point
+    double alpha_c; //skew_coeff
+    double kc[5];   //distortions
+
+    float raw_image_width;
+    float raw_image_height;
+    float scaled_image_width;
+    float scaled_image_height;
+
+    camera_calib()
+    {
+        fc[0]=471.21783;
+        fc[1]=476.11993;
+        cc[0]=322.35359;
+        cc[1]=183.44632;
+        alpha_c=0;
+
+        kc[0]=0.00083;
+        kc[1]=-0.03521;
+        kc[2]=0.00135;
+        kc[3]=0.00048;
+        kc[4]=0;
+
+        raw_image_width=640;
+        raw_image_height=360;
+    }
+
+    ~camera_calib() {}
+};
+
+
+class camera_project
+{
+public:
+    camera_project();
+
+private:
+    std::string ldmrs_single_id_, camera_frame_id_;
+    camera_calib webcam_;
+    ros::Subscriber pd_laser_batch_sub_;
+    void project_to_image(const sensing_on_road::pedestrian_laser_batch &pd_laser_para);
+    void projection (const geometry_msgs::Point32 &temp3Dpara, sensing_on_road::pedestrian_vision &tempprpara);
+    ros::Publisher pd_vision_pub_;
+    sensing_on_road::pedestrian_vision_batch pd_vision_batch_;
+
+    void pcl_callback(const sensor_msgs::PointCloud::ConstPtr& pcl_in);
+    message_filters::Subscriber<sensor_msgs::PointCloud> pd_pcl_sub_;
+    tf::TransformListener tf_;
+    tf::MessageFilter<sensor_msgs::PointCloud> * tf_filter_;
+};
+
 
 camera_project::camera_project()
 {
     ros::NodeHandle nh;
-    
+
     nh.param("laser_frame_id",     ldmrs_single_id_,      std::string("ldmrsAssemb"));
     nh.param("camera_frame_id",    camera_frame_id_,      std::string("usb_cam"));
 
@@ -28,14 +97,14 @@ camera_project::camera_project()
     tf_filter_ = new tf::MessageFilter<sensor_msgs::PointCloud>(pd_pcl_sub_, tf_, camera_frame_id_, 10);
     tf_filter_->registerCallback(boost::bind(&camera_project::pcl_callback, this, _1));
     tf_filter_->setTolerance(ros::Duration(0.05));
-    
+
     pd_laser_batch_sub_ = nh.subscribe("ped_laser_batch", 2, &camera_project::project_to_image, this);
     pd_vision_pub_ = nh.advertise<sensing_on_road::pedestrian_vision_batch>("pd_vision_batch", 2);
 }
 
 void camera_project::pcl_callback(const sensor_msgs::PointCloud::ConstPtr& pcl_in)
 {
-    
+
 }
 
 void camera_project::project_to_image(const sensing_on_road::pedestrian_laser_batch &pd_laser_para)
@@ -129,14 +198,13 @@ void camera_project::projection(const geometry_msgs::Point32 &temp3Dpara, sensin
     }
 }
 
-} //namespace
 
 
 
 int main(int argc, char** argv)
 {
     ros::init(argc, argv, "camera_projector");
-    camera_projector::camera_project camera_project_node;
+    camera_project camera_project_node;
     ros::spin();
 }
 
