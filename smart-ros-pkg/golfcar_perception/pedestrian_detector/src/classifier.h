bda10f18e6b536e98518ff95dc333536cc7446b5
Demian <chongzj@nus.edu.sg>
2012-02-11 15:28:43 +0800

    -Classifier now use camera_project library and subscribe directly to ped cluster -Minor tweak in cv_helper to include minimum windows size allowed by HOG

--- smart-ros-pkg/golfcar_perception/pedestrian_detector/src/classifier.h
+++ smart-ros-pkg/golfcar_perception/pedestrian_detector/src/classifier.h
@@ -5,13 +5,19 @@
 #include "cv_bridge/cv_bridge.h"
 #include "sensing_on_road/pedestrian_vision_batch.h"
 #include "sensing_on_road/pedestrian_vision.h"
+#include "sensing_on_road/camera_project.h"
 #include "opencv2/gpu/gpu.hpp"
 #include "opencv2/highgui/highgui.hpp"
 
+#include <tf/message_filter.h>
 #include <message_filters/subscriber.h>
 #include <message_filters/time_synchronizer.h>
 #include <message_filters/synchronizer.h>
 #include <message_filters/sync_policies/approximate_time.h>
+#include <feature_detection/clusters.h>
+#include "cv_helper.h"
+
+#define WIN_SIZE Size(48,96)
 
 using namespace std;
 using namespace cv;
@@ -34,7 +40,7 @@ private:
     ros::NodeHandle n_;
     image_transport::ImageTransport it_;
     image_transport::SubscriberFilter image_sub_;
-    message_filters::Subscriber<sensing_on_road::pedestrian_vision_batch> people_rects_sub_;
+    message_filters::Subscriber<feature_detection::clusters> people_rects_sub_;
     ros::Publisher people_roi_pub_;
     ros::Publisher people_detect_pub_;
     ros::Publisher people_ver_pub_;
@@ -43,7 +49,7 @@ private:
 
     void ScaleWithDistanceRatio(Mat *img, double disz, double norm_distance, Size img_size, Size smallest_size, double *ratio);
     void detectPedestrian(Point offset, double ratio, gpu::GpuMat& gpu_img, sensing_on_road::pedestrian_vision_batch *detect_rects);
-    void syncCallback(const sensing_on_road::pedestrian_vision_batchConstPtr pr, const sensor_msgs::ImageConstPtr);
+    void syncCallback(const feature_detection::clustersConstPtr pr_ptr, const sensor_msgs::ImageConstPtr image);
     void updateParameter();
     template <class T>
     void checkParamChanged(T &a, T &b);
@@ -51,7 +57,24 @@ private:
     cv::Mat img;
     int count;
     bool new_image, show_processed_image, black_front_roi,write_image;
+    bool scaleWithDistance;
     double hit_threshold, scale, group_threshold, norm_dist;
     std::string path;
+
+    //project camera
+    sensing_on_road::pedestrian_vision_batch transformClusterToPedVisionBatch(const feature_detection::clustersConstPtr cl_ptr);
+    camera_project::camera_projector camera_project;
+    void setRectMsg(const camera_project::CvRectangle & rect, sensing_on_road::pedestrian_vision * msg)
+    {
+        msg->x          = rect.upper_left.x;
+        msg->y          = rect.upper_left.y;
+        msg->width      = rect.lower_right.x - rect.upper_left.x;
+        msg->height     = rect.lower_right.y - rect.upper_left.y;
+
+        msg->cvRect_x1  = rect.upper_left.x;
+        msg->cvRect_y1  = rect.upper_left.y;
+        msg->cvRect_x2  = rect.lower_right.x;
+        msg->cvRect_y2  = rect.lower_right.y;
+    }
 };
 
