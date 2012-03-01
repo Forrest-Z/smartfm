d6acd8d8b6e4b1293bc6f444e73816e3e24f9853
Brice Rebsamen <brice.rebsamen@gmail.com>
2012-03-01 10:45:02 +0800

    In infrastructure_road_monitoring: took BlobTracker and TrackMatcher out of library as a preparation to a tighter ROS integration.

--- smart-ros-pkg/golfcar_perception/infrastructure_road_monitoring/src/BlobTracker.h
+++ smart-ros-pkg/golfcar_perception/infrastructure_road_monitoring/src/BlobTracker.h
@@ -1,79 +1,13 @@
 #ifndef BLOBTRACKER_H_
 #define BLOBTRACKER_H_
 
-#include <vector>
-#include <list>
 #include <limits>
 
 #include <boost/function.hpp>
 
-#include <cv.h>
-
-#include "Blob.h"
 #include <fmutil/fm_filter.h>
 
-/// A class to hold Observation data
-class Observation : public Blob
-{
-public:
-    bool observed;  ///< was it observed
-    cv::Point disp; ///< displacement w.r.t. prev position
-
-    /// Creates an unobserved Observation
-    Observation();
-
-    /// Creates an observation from a Blob
-    Observation(const Blob & blob);
-
-    /// Creates an Observation from current Blob information and previous observation
-    Observation(const Blob & blob, const Observation & pre);
-};
-
-typedef std::vector<Observation> Observations;
-
-/// A class to hold history of observations
-class Track
-{
-    /// ID counter
-    static unsigned counter;
-
-public:
-    static double vel_filter_tau;
-
-public:
-    Observations observations;
-    unsigned id;
-    fmutil::LowPassFilter vel_x, vel_y;
-
-
-public:
-    /// Creates a Track with an ID of 0
-    Track();
-
-    /// Creates a Track with a given ID
-    Track(unsigned);
-
-    /// Creates a new Track with next available ID
-    static Track newTrack();
-
-    /// Creates a new Track with next available ID, and fill with
-    // an observation from the blob
-    static Track newTrack(const Blob & blob);
-
-    /// computes the distance between a blob and the latest observed Observation in the track
-    double distance(const Blob & blob) const;
-
-    void addObservation(const Blob & blob);
-
-    Observations::reverse_iterator latestObserved();
-    Observations::const_reverse_iterator latestObserved() const;
-
-    void display(cv::Mat displayFrame, cv::Scalar color=CV_RGB(255,0,0));
-};
-
-
-/// A typedef for collection of tracks
-typedef std::list<Track> Tracks;
+#include "Tracks.h"
 
 
 /// Generates a threshold for the nearest neighbors matcher, given a blob and
