825990c8f3dd364d99bea416a89bc04cebc2bc9a
Demian <chongzj@nus.edu.sg>
2012-03-11 02:46:08 +0800

    data_assoc properly handles spliting and merging

--- smart-ros-pkg/dataAssoc_experimental/src/data_assoc.h
+++ smart-ros-pkg/dataAssoc_experimental/src/data_assoc.h
@@ -39,14 +39,17 @@ struct local_lPedInView
     int id;
     geometry_msgs::PointStamped location;
 };
+
+
 class data_assoc
 {
 public:
     data_assoc(int argc, char** argv);
     //data_assoc();
     ~data_assoc();
+
+private:
     void pedClustCallback(feature_detection::clustersConstPtr cluster_vector);
-    void pedVisionCallback(sensing_on_road::pedestrian_vision_batchConstPtr pedestrian_vision_vector);
     void pedVisionAngularCallback(sensor_msgs::PointCloudConstPtr pedestrian_vision_angular);
     void publishPed();
     bool transformPointToGlobal(std_msgs::Header header, geometry_msgs::Point32 input_point, geometry_msgs::Point32& output_point);
@@ -54,21 +57,25 @@ public:
     void getLatestLaserCluster(vector<centroid_local_global> &clg_copy);
     void cleanUp();
     void increaseConfidence(int id);
+    void updatelPedInViewWithID(int id, sensing_on_road::pedestrian_vision& pd_vector);
+    void updatelPedInView(sensing_on_road::pedestrian_vision& update_source, sensing_on_road::pedestrian_vision& update_dest);
+    void resetLPedInViewDecisionflag();
+    bool getlPedInViewCentroid(int id, geometry_msgs::Point32& centroid);
+    void erase_merge_lists(int id);
     ros::Publisher pedPub_;
     ros::Publisher visualizer_;
     string frame_id_, global_frame_, camera_frame_;
     bool use_sim_time_;
     tf::TransformListener *listener_;
     tf::MessageFilter<feature_detection::clusters> * laser_tf_filter_;
-    tf::MessageFilter<sensing_on_road::pedestrian_vision_batch> * vision_tf_filter_;
     tf::MessageFilter<sensor_msgs::PointCloud> * vision_angular_tf_filter_;
     message_filters::Subscriber<feature_detection::clusters> pedClustSub_;
-    message_filters::Subscriber<sensing_on_road::pedestrian_vision_batch> pedVisionSub_;
     message_filters::Subscriber<sensor_msgs::PointCloud> pedVisionAngularSub_;
     sensing_on_road::pedestrian_vision_batch lPedInView;
     double time_out_, poll_inc_, poll_dec_, threshold_;
     camera_project::camera_projector projector;
     std::vector<geometry_msgs::Point32> laser_latest_global_, laser_latest_local_;
+    vector<vector<int> > merge_lists;
     //double robotx_, roboty_, robotspeedx_;//pedx_, pedy_;
 
     //double robotspeedx_;
