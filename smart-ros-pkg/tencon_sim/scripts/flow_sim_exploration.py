1f97dbf86f4c3749b3f956bdfa519189b8202e36
Brice Rebsamen <brice.rebsamen@gmail.com>
2012-05-23 13:28:17 +0800

    tencon: logging more data
    - logging position and vel of each mobiles
    - log every one second, but also log each time a new mobile is
      added or a vehicle is removed
    - each mobile now gets a unique id
    - thus also had to change the data extraction part
    - saving the result of each simulation to its own file in a temp
      dir then tar
    - using marshal instead of pickle: less memory intensive

--- smart-ros-pkg/tencon_sim/scripts/flow_sim_exploration.py
+++ smart-ros-pkg/tencon_sim/scripts/flow_sim_exploration.py
@@ -3,8 +3,11 @@
 
 '''Run a number of flow simulations with different parameters values.'''
 
-import sys
-import cPickle as pickle
+import sys, traceback
+#import cPickle as pickle
+import marshal
+import itertools
+import os, tarfile, tempfile
 
 sys.path.append('src')
 from tencon_sim import *
@@ -18,22 +21,22 @@ params = {}
 
 # time step of the simulation:
 params['sim_time_step'] = 0.1
-    
+
 # position of the beginning of the pedestrian crossing on the pedestrian axis
 params['ped_crossing_length'] = 2
-    
+
 # position of the beginning of the pedestrian crossing on the vehicle axis
 params['ped_crossing_width'] = 1
-    
+
 # Pedestrian velocity
 params['ped_vel'] = 1
-params['ped_start_pos'] = -20    
+params['ped_start_pos'] = -20
 
 params['veh_start_pos'] = -100
 params['veh_max_vel'] = 3
 params['veh_acc'] = 3
 params['safety_dist'] = 2
-    
+
 params['infra_fov_dist']= 30
 
 params['lambda_ped'] = 0.1
@@ -41,45 +44,45 @@ params['lambda_veh'] = 0.3
 
 
 #------------------------------------------------------------------------------
-# exploration parameters, and result container
+# exploration parameters
 #
 
 lambda_veh = [0.01, 0.05, 0.1, 0.25, 0.5, 0.75, 1]
 lambda_ped = [0.01, 0.05, 0.1, 0.25, 0.35, 0.5]
-
-# Each cell holds the results of one 
-# parameter configuration as a dictionary.
-results = []
-for lv in lambda_veh:
-    for lp in lambda_ped:
-        results.append({'lambda_veh':lv, 'lambda_ped': lp})
-
-
-
+#lambda_veh = [1]
+#lambda_ped = [0.5]
 
 #------------------------------------------------------------------------------
 # Simulation
 
 ok = True
-for r in results:
-    print 'Simulating with lambda_veh=%g and lambda_ped=%g' % (r['lambda_veh'], r['lambda_ped'])
-    params['lambda_ped'] = r['lambda_ped']
-    params['lambda_veh'] = r['lambda_veh']
+logdir = tempfile.mkdtemp()
+print 'logging in', logdir
+for lv, lp in itertools.product(lambda_veh, lambda_ped):
+    print 'Simulating with lambda_veh=%g and lambda_ped=%g' % (lv, lp)
+    params['lambda_ped'] = lp
+    params['lambda_veh'] = lv
     flow_sim = FlowSim(params)
     while ok:
         try:
             flow_sim.step()
-            ndtb = len(flow_sim.dt['base'])
-            ndti = len(flow_sim.dt['infra'])
-            if min([ndtb, ndti]) > 300:
+            #print flow_sim.t, flow_sim.nvehs
+            if min([flow_sim.nvehs['base'], flow_sim.nvehs['infra']]) > 300:
                 break
         except KeyboardInterrupt:
             ok = False
         except:
-            print 'Caught exception', sys.exc_info()[0]
+            print '-'*60
+            traceback.print_exc()
+            print '-'*60
     if not ok:
         break
-    r['dt'] = flow_sim.dt
-    
-with open('result.pickle.dat', 'w') as f:
-    pickle.dump(results, f)
+    filename = '%s/lv_%03d_lp_%03d.dat' % (logdir, int(lv*100), int(lp*100))
+    with open(filename, 'wb') as f:
+        marshal.dump(flow_sim.log, f)
+
+
+tar = tarfile.open("result_logs.tar", "w")
+for name in os.listdir(logdir):
+        tar.add(os.path.join(logdir,name), name)
+tar.close()
--- smart-ros-pkg/tencon_sim/scripts/flow_sim_exploration_analysis.py
+++ smart-ros-pkg/tencon_sim/scripts/flow_sim_exploration_analysis.py
@@ -2,7 +2,10 @@
 # -*- coding: utf-8 -*-
 
 import sys
-import cPickle as pickle
+#import cPickle as pickle
+import marshal
+import os, os.path, tarfile, tempfile, re
+
 import numpy as np
 import scipy.stats
 import matplotlib.pyplot as plt
@@ -11,18 +14,75 @@ from mpl_toolkits.mplot3d import Axes3D
 
 
 class PlotResults:
-    
-    def __init__(self, filename):
-        with open(filename,'r') as f:
-            self.results = pickle.load(f)
 
-        self.lps = [r['lambda_ped'] for r in self.results]
-        self.lvs = [r['lambda_veh'] for r in self.results]
+    def __init__(self):
+        self.lps = []
+        self.lvs = []
+        self.dts = []
+
+    def load_from_tar(self, tarfile):
+        logdir = tempfile.mkdtemp()
+        tar = tarfile.open(tarfile)
+        tar.extractall(path=logdir)
+        tar.close()
+        self.load_from_dir(logdir)
+
+    def load_from_dir(self, logdir):
+        self.logdir = logdir
+        self.logfiles = sorted(os.listdir(logdir))
+        for f in self.logfiles:
+            m = re.search('lv_(?P<lv>\d*)_lp_(?P<lp>\d*)', f)
+            self.lvs.append( float(m.group('lv'))/100 )
+            self.lps.append( float(m.group('lp'))/100 )
+            self.dts.append( self.extract_transit_times(self.logdir+'/'+f) )
         self.lp = sorted(set(self.lps))
         self.lv = sorted(set(self.lvs))
-        self.dt = [np.mean(r['dt']['base']) - np.mean(r['dt']['infra']) for r in self.results]
-        self.t_dt = [scipy.stats.ttest_ind(r['dt']['base'], r['dt']['infra'])[0] for r in self.results]
 
+        self.dt = [np.mean(dt['base']) - np.mean(dt['infra']) for dt in self.dts]
+        self.t_dt = [scipy.stats.ttest_ind(dt['base'], dt['infra'])[0] for dt in self.dts]
+
+    def extract_transit_times(self, filename):
+        '''Goes through the raw data and extracts the transit times of all vehicles
+        in both the base and infra condition.
+        '''
+        with open(filename, 'rb') as f:
+            data = marshal.load(f)
+
+        # For each base/infra condition
+        dts = {'base':[], 'infra':[]}
+        for k in ('base','infra'):
+
+            # maintain a record of entry times
+            entry = {} #{id: t}
+            # initialize with t0
+            for vid in data[k][0]:
+                entry[vid] = data['t'][0]
+
+            # for each row, compare with previous row to find out
+            # vehicle ids that have been added and those that have
+            # been removed. Sets are useful for that.
+            prev_set = set(data[k][0])
+            for i in xrange(1, len(data['t'])):
+                new_set = set(data[k][i])
+                #print 'prev_set:', prev_set
+                #print 'new_set:', new_set
+                #print 'new vehicles (new_set-prev_set):', (new_set-prev_set)
+                #print 'removed vehicles (prev_set-new_set):', (prev_set-new_set)
+                for vid in new_set-prev_set:
+                    # vehicles that have appeared
+                    entry[vid] = data['t'][i]
+                for vid in prev_set-new_set:
+                    # vehicles that have disappeared
+                    dts[k].append(data['t'][i-1] - entry[vid])
+                    del entry[vid]
+                prev_set = new_set
+
+        print '%s: base=%d,%f, infra=%d,%f, dt=%f' % \
+            ( os.path.basename(filename), \
+            len(dts['base']), np.mean(dts['base']), \
+            len(dts['infra']), np.mean(dts['infra']), \
+            np.mean(dts['base'])-np.mean(dts['infra']) )
+        return dts
 
     def plot_3D_hist(self, zvar, name='', fig=None, subplot=111):
         '''Plots the dt results as a 3D histogram.'''
@@ -30,18 +90,18 @@ class PlotResults:
             fig = plt.figure()
         ax = fig.add_subplot(subplot, projection='3d')
         ax.set_title(name)
-        
+
         # position of the bars origin
         xpos, ypos = np.meshgrid(np.arange(len(self.lp))-0.25, np.arange(len(self.lv))-0.25)
         xpos = xpos.flatten()
         ypos = ypos.flatten()
         zpos = np.zeros_like(xpos)
-    
-        # size of the bars    
+
+        # size of the bars
         dx = 0.5 * np.ones_like(xpos)
         dy = dx.copy()
         dz = np.asarray(zvar)
-        
+
         ax.bar3d(xpos, ypos, zpos, dx, dy, dz, color='b')
         ax.set_xlabel('lambda_ped')
         ax.set_ylabel('lambda_veh')
@@ -50,24 +110,24 @@ class PlotResults:
         #ax.set_yticks(range(len(self.lv)))
         #ax.set_xticklabels([str(l) for l in self.lp])
         #ax.set_yticklabels([str(l) for l in self.lv])
-    
-        
+
+
     def plot_color(self, zvar, name='', fig=None, subplot=111):
         '''Plots the dt results as a color plot.'''
         if fig is None:
             fig = plt.figure()
         ax = fig.add_subplot(subplot)
         ax.set_title(name)
-        
+
         def linspace(v):
             m = min(v)
             M = max(v)
             r = M-m
             return np.linspace(m-0.1*r, M+0.1*r, 500)
-        
+
         xi = linspace(self.lp)
         yi = linspace(self.lv)
-        
+
         zi = matplotlib.mlab.griddata(self.lps, self.lvs, zvar, xi, yi, interp='linear')
         #ax.contour(xi, yi, zi, 15, linewidths=0.5, colors='k')
         contour = ax.contourf(xi, yi, zi, 15, cmap=plt.cm.jet)
@@ -77,22 +137,27 @@ class PlotResults:
         ax.set_ylim(min(self.lv),max(self.lv))
         ax.set_xlabel('lambda_ped')
         ax.set_ylabel('lambda_veh')
-    
-    
-    
+
+
+
 if __name__=='__main__':
-    results = PlotResults('result.pickle.dat')
-    
+    results = PlotResults()
+    if os.path.isdir(sys.argv[1]):
+        results.load_from_dir(sys.argv[1])
+    elif os.path.isfile(sys.argv[1]):
+        results.load_from_tar(sys.argv[1])
+
+
     #results.plot_3D_hist(results.t_dt, 'time gain (t value)')
     #results.plot_3D_hist(results.dt, 'time gain (raw value)')
-    
+
     #results.plot_color(results.t_dt, 'time gain (t value)')
     #results.plot_color(results.dt, 'time gain (raw value)')
 
     fig = plt.figure()
     results.plot_3D_hist(results.t_dt, 'time gain (t value)', fig, 221)
-    results.plot_3D_hist(results.dt, 'time gain (raw value)', fig, 222)    
+    results.plot_3D_hist(results.dt, 'time gain (raw value)', fig, 222)
     results.plot_color(results.t_dt, 'time gain (t value)', fig, 223)
     results.plot_color(results.dt, 'time gain (raw value)', fig, 224)
-    
+
     plt.show()
