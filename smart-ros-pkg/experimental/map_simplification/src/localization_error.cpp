e5d9180673dc243431d9f215e2a01b0d70d20d11
Demian <demianjie@hotmail.com>
2012-06-14 18:00:27 +0800

    - Fixed rolling windows bug on laser_evidence - Seperate move_base launch file for map_simplification and change error output to absolute - Update visualizer.cpp

--- smart-ros-pkg/experimental/map_simplification/src/localization_error.cpp
+++ smart-ros-pkg/experimental/map_simplification/src/localization_error.cpp
@@ -35,78 +35,78 @@ using namespace message_filters;
 class localization_error
 {
 public:
-	localization_error();
-	void syncCallback(const geometry_msgs::PoseWithCovarianceStampedConstPtr amcl, const nav_msgs::OdometryConstPtr ground_truth);
-	message_filters::Subscriber<geometry_msgs::PoseWithCovarianceStamped> amcl_pose_sub_;
-	message_filters::Subscriber<nav_msgs::Odometry> ground_truth_sub_;
-	ros::Publisher error_pub_, lat_error_pub_, lon_error_pub_, amcl_point_pub_;
-	sensor_msgs::PointCloud lat_error, lon_error;
+    localization_error();
+    void syncCallback(const geometry_msgs::PoseWithCovarianceStampedConstPtr amcl, const nav_msgs::OdometryConstPtr ground_truth);
+    message_filters::Subscriber<geometry_msgs::PoseWithCovarianceStamped> amcl_pose_sub_;
+    message_filters::Subscriber<nav_msgs::Odometry> ground_truth_sub_;
+    ros::Publisher error_pub_, lat_error_pub_, lon_error_pub_, amcl_point_pub_;
+    sensor_msgs::PointCloud lat_error, lon_error;
 };
 
 localization_error::localization_error()
 {
-	ros::NodeHandle nh;
-	amcl_pose_sub_.subscribe(nh, "amcl_pose", 20);
-	ground_truth_sub_.subscribe(nh, "base_pose_ground_truth", 20);
-	error_pub_ = nh.advertise<map_simplification::error>("map_error", 10);
-	lat_error_pub_ = nh.advertise<sensor_msgs::PointCloud>("lat_error", 10);
-	lon_error_pub_ = nh.advertise<sensor_msgs::PointCloud>("lon_error", 10);
-	amcl_point_pub_ = nh.advertise<sensor_msgs::PointCloud>("amcl_point", 10);
-	typedef sync_policies::ExactTime<geometry_msgs::PoseWithCovarianceStamped, nav_msgs::Odometry> syncPolicy;
-	Synchronizer<syncPolicy> sync(syncPolicy(10), amcl_pose_sub_, ground_truth_sub_);
-	//message_filters::TimeSynchronizer<geometry_msgs::PoseWithCovarianceStamped, nav_msgs::Odometry> syncPolicy(amcl_pose_sub_, ground_truth_sub_, 20);
-	sync.registerCallback(boost::bind(&localization_error::syncCallback,this, _1, _2));
-	ros::spin();
+    ros::NodeHandle nh;
+    amcl_pose_sub_.subscribe(nh, "amcl_pose", 20);
+    ground_truth_sub_.subscribe(nh, "base_pose_ground_truth", 20);
+    error_pub_ = nh.advertise<map_simplification::error>("map_error", 10);
+    lat_error_pub_ = nh.advertise<sensor_msgs::PointCloud>("lat_error", 10, true);
+    lon_error_pub_ = nh.advertise<sensor_msgs::PointCloud>("lon_error", 10, true);
+    amcl_point_pub_ = nh.advertise<sensor_msgs::PointCloud>("amcl_point", 10);
+    typedef sync_policies::ExactTime<geometry_msgs::PoseWithCovarianceStamped, nav_msgs::Odometry> syncPolicy;
+    Synchronizer<syncPolicy> sync(syncPolicy(10), amcl_pose_sub_, ground_truth_sub_);
+    //message_filters::TimeSynchronizer<geometry_msgs::PoseWithCovarianceStamped, nav_msgs::Odometry> syncPolicy(amcl_pose_sub_, ground_truth_sub_, 20);
+    sync.registerCallback(boost::bind(&localization_error::syncCallback,this, _1, _2));
+    ros::spin();
 }
 
 inline double getYaw(geometry_msgs::Quaternion orient)
 {
-	double y, p, r;
-	btQuaternion btq(orient.x, orient.y, orient.z, orient.w);
-	btMatrix3x3(btq).getEulerYPR(y, p, r);
-	return y;
+    double y, p, r;
+    btQuaternion btq(orient.x, orient.y, orient.z, orient.w);
+    btMatrix3x3(btq).getEulerYPR(y, p, r);
+    return y;
 }
 
 void localization_error::syncCallback(const geometry_msgs::PoseWithCovarianceStampedConstPtr amcl, const nav_msgs::OdometryConstPtr ground_truth)
 {
-	geometry_msgs::Point p1 = amcl->pose.pose.position;
-	geometry_msgs::Point p2 = ground_truth->pose.pose.position;
-	double error = fmutil::distance(p1, p2);
-	double amcl_yaw = getYaw(amcl->pose.pose.orientation);
-	double ground_yaw = getYaw(ground_truth->pose.pose.orientation);
+    geometry_msgs::Point p1 = amcl->pose.pose.position;
+    geometry_msgs::Point p2 = ground_truth->pose.pose.position;
+    double error = fmutil::distance(p1, p2);
+    double amcl_yaw = getYaw(amcl->pose.pose.orientation);
+    double ground_yaw = getYaw(ground_truth->pose.pose.orientation);
 
-	map_simplification::error error_msg;
-	error_msg.yaw_error = ground_yaw - amcl_yaw;
-	error_msg.abs_error = error;
-	error_msg.header = amcl->header;
-	error_msg.amcl_pose = p1;
-	error_msg.ground_truth = p2;
-	geometry_msgs::Point rel_pt;
-	rel_pt.x = p2.x-p1.x; rel_pt.y = p2.y-p1.y;
-	error_msg.lon_error = rel_pt.x * cos(amcl_yaw) + rel_pt.y * sin(amcl_yaw);
-	error_msg.lat_error = - rel_pt.x * sin(amcl_yaw) + rel_pt.y * cos(amcl_yaw);
-	error_pub_.publish(error_msg);
+    map_simplification::error error_msg;
+    error_msg.yaw_error = ground_yaw - amcl_yaw;
+    error_msg.abs_error = error;
+    error_msg.header = amcl->header;
+    error_msg.amcl_pose = p1;
+    error_msg.ground_truth = p2;
+    geometry_msgs::Point rel_pt;
+    rel_pt.x = p2.x-p1.x; rel_pt.y = p2.y-p1.y;
+    error_msg.lon_error = rel_pt.x * cos(amcl_yaw) + rel_pt.y * sin(amcl_yaw);
+    error_msg.lat_error = - rel_pt.x * sin(amcl_yaw) + rel_pt.y * cos(amcl_yaw);
+    error_pub_.publish(error_msg);
 
-	sensor_msgs::PointCloud amcl_pts;
-	amcl_pts.header = lon_error.header = lat_error.header = amcl->header;
-	amcl_pts.header.stamp = lon_error.header.stamp = lat_error.header.stamp = ros::Time::now();
-	geometry_msgs::Point32 xy_error;
-	xy_error.x = p2.x; xy_error.y = p2.y;
-	xy_error.z = error_msg.lon_error;
-	lon_error.points.push_back(xy_error);
-	xy_error.z = error_msg.lat_error;
-	lat_error.points.push_back(xy_error);
-	lat_error_pub_.publish(lat_error);
-	lon_error_pub_.publish(lon_error);
-	geometry_msgs::Point32 amcl_pt; amcl_pt.x = p1.x; amcl_pt.y = p1.y;
-	amcl_pts.points.push_back(amcl_pt);
-	amcl_point_pub_.publish(amcl_pts);
+    sensor_msgs::PointCloud amcl_pts;
+    amcl_pts.header = lon_error.header = lat_error.header = amcl->header;
+    amcl_pts.header.stamp = lon_error.header.stamp = lat_error.header.stamp = ros::Time::now();
+    geometry_msgs::Point32 xy_error;
+    xy_error.x = p2.x; xy_error.y = p2.y;
+    xy_error.z = fabs(error_msg.lon_error);
+    lon_error.points.push_back(xy_error);
+    xy_error.z = fabs(error_msg.lat_error);
+    lat_error.points.push_back(xy_error);
+    lat_error_pub_.publish(lat_error);
+    lon_error_pub_.publish(lon_error);
+    geometry_msgs::Point32 amcl_pt; amcl_pt.x = p1.x; amcl_pt.y = p1.y;
+    amcl_pts.points.push_back(amcl_pt);
+    amcl_point_pub_.publish(amcl_pts);
 }
 
 int main(int argc, char** argcv)
 {
-	ros::init(argc, argcv, "localization_error");
-	localization_error le;
+    ros::init(argc, argcv, "localization_error");
+    localization_error le;
 
-	return 0;
+    return 0;
 }
