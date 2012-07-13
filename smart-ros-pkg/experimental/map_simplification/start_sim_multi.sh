6707f7e600bc0098314a8f940d21177766f817be
tirtha <tirtha@golfcart2-master.(none)>
2012-07-13 16:19:18 +0800

    Slight changes to the script in map_simplification to make it more portable

--- smart-ros-pkg/experimental/map_simplification/start_sim_multi.sh
+++ smart-ros-pkg/experimental/map_simplification/start_sim_multi.sh
@@ -2,11 +2,10 @@
 
 thread_n=12
 total_it=8191
-#11627
-rootfolder="/home/golfcar/fuerte/smartfm/smart-ros-pkg/experimental/map_simplification"
-#`rospack find map_simplification`
-echo "map_simplification package found at:"
-echo $rootfolder
+rootfolder=`rospack find map_simplification`
+echo "root folder found at:"
+echo $map_simplification_folder
+
 #sleep 5
 for ((i = 0; i <= $total_it; i+=$thread_n ))
 do
@@ -38,4 +37,5 @@ do
     fi
     sleep 1
 done
+
 
