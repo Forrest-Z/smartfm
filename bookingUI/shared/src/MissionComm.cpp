aa52512c89ab79f6260a5826362d3dcd661f67a5
Brice Rebsamen <brice.rebsamen@gmail.com>
2012-08-10 15:35:07 +0800

    Simplified MissionComm and RoutePlanner
    simplified and commented MissionComm and RoutePlanner in bookingUI/shared

--- bookingUI/shared/src/MissionComm.cpp
+++ bookingUI/shared/src/MissionComm.cpp
@@ -6,7 +6,7 @@
 #include "MissionComm.h"
 
 MissionComm::MissionComm( RoutePlanner & rp, PassengerComm & pc )
-    : routePlanner_(rp), stationList_(rp.sp_.knownStations()),
+    : stationList_(rp.sp_.knownStations()), routePlanner_(rp),
     currentStation_(rp.currentStation_), passengerComm_(pc), state_(sUninit)
 {
     stateStr_[sWaitingMission] = "WaitingForAMission";
@@ -16,6 +16,12 @@ MissionComm::MissionComm( RoutePlanner & rp, PassengerComm & pc )
     stateStr_[sAtDropoff] = "AtDropoffLocation";
 }
 
+void MissionComm::changeState(State new_state)
+{
+    state_ = new_state;
+    updateVehicleStatus(stateStr_[state_]);
+}
+
 void MissionComm::run()
 {
     switch( state_ )
@@ -23,24 +29,20 @@ void MissionComm::run()
     case sUninit:
         stationList_.print();
         currentStation_ = stationList_.prompt("Current station? ");
-        deidentify();
-        identify();
-        updateCurrentLocation(currentStation_.str());
-        state_ = sWaitingMission;
+        initialize();
+        changeState(sWaitingMission);
         break;
 
     case sWaitingMission:
-        waitForMissionConfirmed();
+        waitForMission();
         if( currentStation_ != pickup_ ) {
             routePlanner_.setDestination(pickup_);
-            state_ = sGoingToPickup;
-            updateVehicleStatus(stateStr_[state_]);
+            changeState(sGoingToPickup);
             updateCurrentLocation("");
             routePlanner_.start();
         }
         else {
-            state_ = sAtPickup;
-            updateVehicleStatus(stateStr_[state_]);
+            changeState(sAtPickup);
             updateCurrentLocation(pickup_.str());
         }
         break;
@@ -50,8 +52,7 @@ void MissionComm::run()
 
         updateETA(routePlanner_.eta_);
         if( routePlanner_.hasReached() ) {
-            state_ = sAtPickup;
-            updateVehicleStatus(stateStr_[state_]);
+            changeState(sAtPickup);
             updateCurrentLocation(pickup_.str());
         }
         break;
@@ -60,8 +61,7 @@ void MissionComm::run()
         // TODO: check for mission cancel
         passengerComm_.waitForPassengerInAtPickup();
         routePlanner_.setDestination(dropoff_);
-        state_ = sGoingToDropoff;
-        updateVehicleStatus(stateStr_[state_]);
+        changeState(sGoingToDropoff);
         updateCurrentLocation("");
         routePlanner_.start();
         break;
@@ -69,18 +69,17 @@ void MissionComm::run()
     case sGoingToDropoff:
         updateETA(routePlanner_.eta_);
         if( routePlanner_.hasReached() ) {
-            state_ = sAtDropoff;
-            updateVehicleStatus(stateStr_[state_]);
+            changeState(sAtDropoff);
             updateCurrentLocation(dropoff_.str());
         }
         break;
 
     case sAtDropoff:
         passengerComm_.waitForPassengerOutAtDropoff();
-        if( checkMissionCompleted() ) {
-            state_ = sWaitingMission;
-            updateVehicleStatus(stateStr_[state_]);
-        }
+
+        // we must wait until the scheduler has acknowledged that the mission is now
+        // completed...
+        if( checkMissionCompleted() ) changeState(sWaitingMission);
         break;
 
     default:
@@ -96,17 +95,28 @@ void MissionComm::run()
 
 
 
-void PromptMissionComm::waitForMissionConfirmed()
+void PromptMissionComm::waitForMission()
 {
     stationList_.print();
     pickup_ = stationList_.prompt("Pickup station? ");
     dropoff_ = stationList_.prompt("Dropoff station? ");
 }
 
-void PromptMissionComm::updateStatus()
-{
-    std::cout << "New status: " << stateStr_[state_] <<std::endl;
-}
+bool PromptMissionComm::checkMissionCompleted() { return true; }
+
+void PromptMissionComm::initialize() { }
+
+void PromptMissionComm::updateMissionStatus(std::string status) { }
+
+void PromptMissionComm::updateVehicleStatus(std::string status) { }
+
+void PromptMissionComm::updateGeoLocation(float lat, float lon) { }
+
+void PromptMissionComm::updateETA(float eta) { }
+
+void PromptMissionComm::updateCurrentLocation(std::string loc) { }
+
+bool PromptMissionComm::checkMissionCancelled(unsigned id) { return false; }
 
 
 
@@ -117,7 +127,7 @@ DBMissionComm::DBMissionComm(RoutePlanner & rp, PassengerComm & pc, std::string 
 
 }
 
-void DBMissionComm::waitForMissionConfirmed()
+void DBMissionComm::waitForMission()
 {
     DBInterface::Task task = dbi.waitForNewMission();
     pickup_ = stationList_(task.pickup);
@@ -129,4 +139,29 @@ void DBMissionComm::waitForMissionConfirmed()
 bool DBMissionComm::checkMissionCompleted()
 {
     return strcasecmp(dbi.getTaskEntry(currentTaskID_).status.c_str(),"completed")==0;
+}
+
+void DBMissionComm::updateMissionStatus(std::string status)
+{ dbi.setMissionStatus(currentTaskID_, status); }
+
+void DBMissionComm::updateVehicleStatus(std::string status)
+{ dbi.setVehicleStatus(status); }
+
+void DBMissionComm::updateGeoLocation(float lat, float lon)
+{ dbi.setGeoLocation(lat,lon); }
+
+void DBMissionComm::updateETA(float eta) { dbi.setETA(eta); }
+
+void DBMissionComm::updateCurrentLocation(std::string loc)
+{ dbi.setCurrentLocation(loc); }
+
+bool DBMissionComm::checkMissionCancelled(unsigned id)
+{ return dbi.checkMissionCancelled(id); }
+
+
+void DBMissionComm::initialize()
+{
+    dbi.deleteVehicle();
+    dbi.identify();
+    updateCurrentLocation(currentStation_.str());
 }
