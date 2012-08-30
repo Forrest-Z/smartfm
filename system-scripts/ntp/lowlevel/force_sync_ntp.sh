f0702b9025bc748e8e93357fe9bee343f5e121fe
Brice Rebsamen <brice.rebsamen@gmail.com>
2012-08-30 10:33:41 +0800

    Added force_sync_ntp script
    This script is useful to quicly sync the lowlevel computer's clock with
    the master's.

---
+++ system-scripts/ntp/lowlevel/force_sync_ntp.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+# This script syncs the clock with the golfcart-master. It actually stops the
+# chrony daemon and sync the clock with ntpdate, then restarts the daemon.
+# This is a bit harsh but it's usefull when the offset is so large that it
+# would take chrony hours to converge...
+
+offset=`ntpdate -q golfcart-master | awk '$1 ~ ntpdate { offset=$10 } END { print offset }'`
+echo offset is $offset. Synchronizing...
+
+if [ `id -u` -eq 0 ]
+then
+
+(
+service chrony stop
+ntpdate golfcart-master
+service chrony start
+) > /dev/null 2>&1
+
+else
+        echo "You must have administrator privileges (sudo) to sync! Aborting."
+        exit 1
+fi
+
+offset=`ntpdate -q golfcart-master | awk '$1 ~ ntpdate { offset=$10 } END { print offset }'`
+echo ... offset is now $offset.
+exit 0
