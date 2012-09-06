36a71f2b5a6bac205c6d8aad1712378a745af312
demian <demian@golfcart-clone.(none)>
2012-09-06 15:35:57 +0800

    Added custom message to receive delay msg to enable rosbag recording

--- smart-ros-pkg/experimental/network_delay_experiment/src/simulated_laser_sub.cpp
+++ smart-ros-pkg/experimental/network_delay_experiment/src/simulated_laser_sub.cpp
@@ -7,12 +7,19 @@
 
 #include <ros/ros.h>
 #include <sensor_msgs/LaserScan.h>
+#include <network_delay_experiment/delay.h>
+
+ros::Publisher *delay_pub;
 
 void scanCallback(sensor_msgs::LaserScanConstPtr scan)
 {
     ros::WallTime walltime = ros::WallTime::now();
     ros::Time time_now = ros::Time(walltime.sec, walltime.nsec);
-    std::cout<<"Scan seq="<<scan->header.seq<<" Delay="<<(time_now - scan->header.stamp).toSec()<<std::endl;
+//    std::cout<<"Scan seq="<<scan->header.seq<<" Delay="<<(time_now - scan->header.stamp).toSec()<<std::endl;
+    network_delay_experiment::delay delay;
+    delay.seq = scan->header.seq;
+    delay.time = (time_now - scan->header.stamp).toSec();
+    delay_pub->publish(delay);  
 }
 
 int main(int argc, char** argcv)
@@ -20,7 +27,10 @@ int main(int argc, char** argcv)
     ros::init(argc, argcv, "simulated_laser_sub");
     ros::NodeHandle n;
     ros::Subscriber laser_sub;
-    laser_sub = n.subscribe<sensor_msgs::LaserScan>("simulated_scan", 10, &scanCallback);
+    ros::Publisher pub;
+    laser_sub = n.subscribe("simulated_scan", 10, &scanCallback);
+    pub = n.advertise<network_delay_experiment::delay>("delay", 10);
+    delay_pub = &pub;
     ros::spin();
 
 }
