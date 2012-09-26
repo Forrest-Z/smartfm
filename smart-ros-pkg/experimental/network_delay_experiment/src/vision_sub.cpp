5a21c1ed22ab3d134650a27ad0c16bb22f8bdc91
baoxing <marskahn@gmail.com>
2012-09-26 15:04:29 +0800

    modify the image subscription;

--- smart-ros-pkg/experimental/network_delay_experiment/src/vision_sub.cpp
+++ smart-ros-pkg/experimental/network_delay_experiment/src/vision_sub.cpp
@@ -4,16 +4,15 @@
 
 ros::Publisher *delay_pub;
 
-void ImageCallBack( const sensor_msgs::ImageConstPtr& image_msg,
-                    const sensor_msgs::CameraInfoConstPtr& info_msg)
+void ImageCallBack( const sensor_msgs::ImageConstPtr& image_msg)
 {
     ros::WallTime walltime = ros::WallTime::now();
     ros::Time time_now = ros::Time(walltime.sec, walltime.nsec);
     network_delay_experiment::delay delay;
-    delay.seq = info_msg->header.seq;
+    delay.seq = image_msg->header.seq;
     delay.time_now = time_now;
-    delay.scan_time = info_msg->header.stamp;
-    delay.nsec_delay = (time_now - info_msg->header.stamp).toNSec();
+    delay.scan_time = image_msg->header.stamp;
+    delay.nsec_delay = (time_now - image_msg->header.stamp).toNSec();
     delay_pub->publish(delay);  
 }
 
@@ -21,12 +20,11 @@ int main(int argc, char** argcv)
 {
     ros::init(argc, argcv, "image_sub");
     ros::NodeHandle n;
-    image_transport::CameraSubscriber cam_sub_;
+    
     ros::Publisher pub;
     
     image_transport::ImageTransport it_(n);
-    cam_sub_ = it_.subscribeCamera("/camera_front/image_raw", 1, &ImageCallBack);
-    
+    image_transport::Subscriber cam_sub_ = it_.subscribe("/camera_front/image_raw", 1, ImageCallBack);
     pub = n.advertise<network_delay_experiment::delay>("vision_delay", 10);
     delay_pub = &pub;
     ros::spin();
