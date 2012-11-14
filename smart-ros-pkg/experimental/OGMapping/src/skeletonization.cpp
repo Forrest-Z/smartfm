ccd064e326ab9b7f736fcb70ee5b7713e24a603b
baoxing <marskahn@gmail.com>
2012-11-14 21:19:09 +0800

    add graph analysis for OGMapper;

--- smart-ros-pkg/experimental/OGMapping/src/skeletonization.cpp
+++ smart-ros-pkg/experimental/OGMapping/src/skeletonization.cpp
@@ -5,6 +5,7 @@
 #include <opencv/highgui.h>
 #include <vector>
 #include <sensor_msgs/PointCloud.h>
+#include <fmutil/fm_math.h>
 
 #define BINARY_THRESH 200
 
@@ -167,10 +168,124 @@ void ThinSubiteration2(CvMat *pSrc, CvMat *pDst) {
 
 
 
+void Graph_Extraction(CvMat *pSrc, CvMat *pDst, CvMat *pDst2, std::vector<CvPoint2D32f> & node_points) 
+{ 
+			node_points.clear();
+        int rows = pSrc->rows; 
+        int cols = pSrc->cols; 
+        /// pad source 
+        CvMat *p_enlarged_src = cvCreateMat(rows + 2, cols + 2, CV_32FC1); 
+        CvMat *p_enlarged_dst = cvCreateMat(rows + 2, cols + 2, CV_32FC1); 
+        CvMat *p_enlarged_dst2 = cvCreateMat(rows + 2, cols + 2, CV_32FC1); 
+        
+        for(int i = 0; i < (rows+2); i++) { 
+                CV_MAT_ELEM(*p_enlarged_src, float, i, 0)	 = 0.0f; 
+                CV_MAT_ELEM(*p_enlarged_src, float, i, cols+1)	= 0.0f; 
+        } 
+        for(int j = 0; j < (cols+2); j++) { 
+                CV_MAT_ELEM(*p_enlarged_src, float, 0, j)	 = 0.0f; 
+                CV_MAT_ELEM(*p_enlarged_src, float, rows+1, j)	= 0.0f; 
+        } 
+        for(int i = 0; i < rows; i++) { 
+                for(int j = 0; j < cols; j++) { 
+                        if (CV_MAT_ELEM(*pSrc, float, i, j) >= 0.5f) { 
+                                CV_MAT_ELEM(*p_enlarged_src, float, i+1, j+1) = 1.0f; 
+                        } 
+                        else 
+                                CV_MAT_ELEM(*p_enlarged_src, float, i+1, j+1) = 0.0f; 
+                } 
+        } 
+        
+        for(int i = 0; i < rows; i++) { 
+                for(int j = 0; j < cols; j++) { 
+                        if ( CV_MAT_ELEM(*p_enlarged_src, float, i, j) == 1.0f) { 
+                                /// get 8 neighbors 
+                                /// calculate C(p) 
+                                int neighbor0 = (int) CV_MAT_ELEM(*p_enlarged_src, float, i-1, j-1); 
+                                int neighbor1 = (int) CV_MAT_ELEM(*p_enlarged_src, float, i-1, j); 
+                                int neighbor2 = (int) CV_MAT_ELEM(*p_enlarged_src, float, i-1, j+1); 
+                                int neighbor3 = (int) CV_MAT_ELEM(*p_enlarged_src, float, i, j+1); 
+                                int neighbor4 = (int) CV_MAT_ELEM(*p_enlarged_src, float, i+1, j+1); 
+                                int neighbor5 = (int) CV_MAT_ELEM(*p_enlarged_src, float, i+1, j); 
+                                int neighbor6 = (int) CV_MAT_ELEM(*p_enlarged_src, float, i+1, j-1); 
+                                int neighbor7 = (int) CV_MAT_ELEM(*p_enlarged_src, float, i, j-1); 
+                                
+                                int Count = neighbor0 + neighbor1 + neighbor2 + neighbor3 + neighbor4 +
+															neighbor5 + neighbor6 + neighbor7;
+
+										  if(Count > 2) {
+											  CV_MAT_ELEM(*p_enlarged_dst, float, i, j) = 1.0f;
+											  
+											  node_points.push_back(cvPoint2D32f(j, i));
+											  
+											  } 
+										  else
+										  {
+											  //only record the remained edge points;
+											  CV_MAT_ELEM(*p_enlarged_dst2, float, i, j) = 1.0f;
+										  }
+                        } 
+                } 
+        }
+
+        /// copy result 
+        for(int i = 0; i < rows; i++) { 
+                for(int j = 0; j < cols; j++) { 
+                        CV_MAT_ELEM(*pDst, float, i, j) = CV_MAT_ELEM(*p_enlarged_dst, float, i+1, j+1); 
+                        CV_MAT_ELEM(*pDst2, float, i, j) = CV_MAT_ELEM(*p_enlarged_dst2, float, i+1, j+1); 
+                        
+                } 
+        } 
+        /// clean memory 
+        cvReleaseMat(&p_enlarged_src); 
+        cvReleaseMat(&p_enlarged_dst); 
+        cvReleaseMat(&p_enlarged_dst2); 
+} 
+
+
+//http://nghiaho.com/?p=1102
+void Extract_Edges(CvMat* pSrc, std::vector < std::vector<CvPoint> > & edges)
+{
+    edges.clear();
+    CvMat *label_image = cvCloneMat( pSrc );
+
+    int label_count = 2; // starts at 2 because 0,1 are used already
+
+    for(int y=0; y < label_image->rows; y++) {
+        for(int x=0; x < label_image->cols; x++) {
+            if((int)CV_MAT_ELEM(*label_image, float, y, x) != 1) {
+                continue;
+            }
+            
+            cvFloodFill(label_image, cvPoint(x,y), cvScalar(label_count), cvScalar(0), cvScalar(0), NULL, 8, NULL);
+
+            std::vector <CvPoint> edge;
+
+            for(int i= 0 ; i < label_image->rows; i++) {
+                for(int j=0; j < label_image->cols; j++) {
+						 
+                    if((int)CV_MAT_ELEM(*label_image, float, i, j)!= label_count) 
+                    {
+                        continue;
+                    }
+							
+                    edge.push_back(cvPoint(j,i));
+                    //CV_MAT_ELEM(*label_image, float, i, j) = 0.0;
+                }
+            }
+
+            edges.push_back(edge);
+
+            label_count++;
+        }
+    }
+}
+
+
 int main(int argc, char** argv) 
 {
     IplImage *src=0, *img = 0, *temp = 0, *eroded = 0;
-    IplImage *skel =0;
+    IplImage *skel =0, *node =0, *edge = 0, *color_edge = 0;
     
     if(argc != 2){return -1;}
     if((src = cvLoadImage( argv[1], CV_LOAD_IMAGE_GRAYSCALE)) == 0){
@@ -185,6 +300,9 @@ int main(int argc, char** argv)
     temp = cvCreateImage(cvSize(destination->width,destination->height),IPL_DEPTH_8U, 1);
     eroded = cvCreateImage(cvSize(destination->width,destination->height),IPL_DEPTH_8U, 1);
     skel = cvCreateImage(cvSize(destination->width,destination->height),IPL_DEPTH_8U, 1);
+    node = cvCreateImage(cvSize(destination->width,destination->height),IPL_DEPTH_8U, 1);
+    edge = cvCreateImage(cvSize(destination->width,destination->height),IPL_DEPTH_8U, 1);
+    color_edge = cvCreateImage(cvSize(destination->width,destination->height),IPL_DEPTH_8U, 3); 
     
     cvThreshold(destination, img, BINARY_THRESH, 255, CV_THRESH_BINARY);
 	 
@@ -198,6 +316,9 @@ int main(int argc, char** argv)
     
     CvMat *mat = cvCreateMat( img->height, img->width, CV_32FC1);
     CvMat *Output_mat = cvCreateMat( img->height, img->width, CV_32FC1);
+    CvMat *node_mat = cvCreateMat( img->height, img->width, CV_32FC1);
+    CvMat *edge_mat = cvCreateMat( img->height, img->width, CV_32FC1);
+    
 	 cvConvert( img, mat);
 	 
 	 int rows = mat->rows; 
@@ -212,6 +333,75 @@ int main(int argc, char** argv)
         
 	 MorphologicalThinning(mat, Output_mat);
 	 
+	 std::vector<CvPoint2D32f> node_points;
+	 Graph_Extraction(Output_mat, node_mat, edge_mat, node_points);
+	 
+	 //do simple clustering for the node points;
+	 std::vector <std::vector<CvPoint2D32f> > node_clusters;
+	 for(size_t i=0; i<node_points.size(); i++)
+	 {
+		 CvPoint2D32f point_tmp = node_points[i];
+		 int cluster_serial = -1;
+		 
+		 for(size_t j=0; j<node_clusters.size(); j++)
+		 {
+			   if(cluster_serial !=-1) break;
+				for(size_t k=0; k<node_clusters[j].size(); k++)
+				{
+					CvPoint2D32f point_tmpp;
+					if(fmutil::distance(point_tmp, point_tmpp) < 5.0) 
+					{
+						cluster_serial = j;
+						break;
+					}
+				}
+		 }
+		 if(cluster_serial == -1)
+		 {
+				std::vector<CvPoint2D32f> cluster_tmp;
+				cluster_tmp.push_back(point_tmp);
+				node_clusters.push_back(cluster_tmp);
+		 }
+		 else
+		 {
+				node_clusters[cluster_serial].push_back(point_tmp);
+		 }
+	 }
+	 
+	 std::vector<CvPoint>  merged_nodes;
+	 for(size_t i=0; i<node_clusters.size(); i++)
+	 {
+		 float points_number = (float)node_clusters[i].size();
+		 float x_tmp =0.0; float  y_tmp = 0.0;
+		 for(size_t j=0; j<node_clusters[i].size(); j++)
+		 {
+			 x_tmp = x_tmp + node_clusters[i][j].x;
+			 y_tmp = y_tmp + node_clusters[i][j].y;
+		 }
+		 x_tmp = x_tmp / points_number;
+		 y_tmp = y_tmp / points_number;
+		 merged_nodes.push_back(cvPoint(x_tmp, y_tmp));
+
+		 cvCircle( color_edge, cvPoint(x_tmp, y_tmp), 5, CV_RGB(0,255,0), 2);
+	 }
+	 
+	 
+	 	 
+	 
+	 std::vector < std::vector<CvPoint> > edges;
+    Extract_Edges(edge_mat, edges);
+	 
+	 for(size_t i=0; i < edges.size(); i++) {
+					CvScalar ext_color;
+					ext_color = CV_RGB( rand()&255, rand()&255, rand()&255 ); 
+        
+        for(size_t j=0; j < edges[i].size(); j++) {
+            int x = edges[i][j].x;
+            int y = edges[i][j].y;
+				cvSet2D(color_edge, y, x, ext_color);
+        }
+    }	
+   
 	 for(int i = 0; i < rows; i++) { 
 		 for(int j = 0; j < cols; j++) { 
 			  float tmp_value = CV_MAT_ELEM(*Output_mat, float, i, j) ; 
@@ -219,6 +409,25 @@ int main(int argc, char** argv)
 		 } 
     }
     cvConvert( Output_mat, skel);
+    
+    
+	for(int i = 0; i < rows; i++) { 
+		 for(int j = 0; j < cols; j++) { 
+			  float tmp_value = CV_MAT_ELEM(*node_mat, float, i, j) ; 
+			  CV_MAT_ELEM(*node_mat, float, i, j) = floorf(tmp_value*255.0);
+		 } 
+    }
+    cvConvert( node_mat, node);
+    
+   for(int i = 0; i < rows; i++) { 
+		 for(int j = 0; j < cols; j++) { 
+			  float tmp_value = CV_MAT_ELEM(*edge_mat, float, i, j) ; 
+			  CV_MAT_ELEM(*edge_mat, float, i, j) = floorf(tmp_value*255.0);
+		 } 
+    }
+    cvConvert( edge_mat, edge);
+    
+    
     
     
     /*
@@ -238,9 +447,22 @@ int main(int argc, char** argv)
 	 
 	 */
     cvSaveImage( "/home/baoxing/skel.jpg", skel );
-
 	 cvNamedWindow("skeleton",1);
     cvShowImage("skeleton", skel);
+    
+    cvSaveImage( "/home/baoxing/node.jpg", node );
+	 cvNamedWindow("node",1);
+    cvShowImage("node", node);
+    
+    cvSaveImage( "/home/baoxing/edge.jpg", edge );
+	 cvNamedWindow("edge",1);
+    cvShowImage("edge", edge);
+    
+    cvSaveImage( "/home/baoxing/color_edge.jpg", color_edge );
+	 cvNamedWindow("color_edge",1);
+    cvShowImage("color_edge", color_edge);
+    
+    
 	 
 	 cvWaitKey();
 
@@ -249,10 +471,13 @@ int main(int argc, char** argv)
     cvReleaseImage(&temp);
     cvReleaseImage(&eroded);
     cvReleaseImage(&skel);
+    cvReleaseImage(&node);
+    cvReleaseImage(&edge);
 	 //cvReleaseStructuringElement(&kernel);
 	 
     cvDestroyWindow("skeleton");
-    
+    cvDestroyWindow("node");
+    cvDestroyWindow("edge");
     return 0;
 }
 
