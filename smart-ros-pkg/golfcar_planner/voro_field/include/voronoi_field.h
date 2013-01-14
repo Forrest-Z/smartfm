6bc4b4c1c3207da5e544a7fddda4cf0641ae0832
liuwei <liu_wei@nus.edu.sg>
2013-01-14 11:04:40 +0800

    Update voro_field

--- smart-ros-pkg/golfcar_planner/voro_field/include/voronoi_field.h
+++ smart-ros-pkg/golfcar_planner/voro_field/include/voronoi_field.h
@@ -3,19 +3,23 @@
 #include <ros/ros.h>
 #include <std_msgs/Bool.h>
 #include <nav_msgs/OccupancyGrid.h>
+#include <sensor_msgs/PointCloud.h>
+#include <geometry_msgs/Point32.h>
 #include <std_msgs/Int8MultiArray.h>
 #include <dynamicvoronoi.h>
+#include <time.h>
 
 using namespace std;
 
 class VoroField
 {
+	friend class DynamicVoronoi;
 public:
 		VoroField();
 		~VoroField();
 
-		nav_msgs::OccupancyGrid map_pub;
-		float resolution ;
+		sensor_msgs::PointCloud local_map_pub;
+		float resolution;
 		int height;
 		int width;
 		bool **gridCell;
@@ -25,7 +29,22 @@ public:
 	    ros::Publisher cost_map_pub;
 
 	    void map_CB(const nav_msgs::OccupancyGrid map_grid);
-	   // void cost_map_publish();
+	    void cost_map_publish();
 	    void voro_field_update();
+};
+
+class Time {
+public:
+  struct timeval tv;
+  Time(){reset();}
+  void reset(){
+    gettimeofday( &tv, NULL);
+  }
+  float get_since(){
+    struct timeval t2;
+    gettimeofday( &t2, NULL);
+    return (t2.tv_sec-tv.tv_sec) +
+      0.000001*(t2.tv_usec-tv.tv_usec);
+  }
 };
 
