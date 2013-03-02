0683b4674e01222250f7c60b454d0e161fc40fa8
baoxing <marskahn@gmail.com>
2013-03-02 20:07:51 +0800

    image_brightness_control, rewrite the structure;

--- smart-ros-pkg/golfcar_perception/image_brightness_control/src/brightness_control.cpp
+++ smart-ros-pkg/golfcar_perception/image_brightness_control/src/brightness_control.cpp
@@ -7,36 +7,55 @@ namespace golfcar_vision{
     private_nh_("~"),
     it_(nh_)
   {
-	  private_nh_.param("control_command_", control_command_, 100);
-	  private_nh_.param("control_command_", expected_centroid_, 115);
-	  private_nh_.param("visualization_flag", visualization_flag_, true);
-     cam_sub_ = it_.subscribeCamera("/camera_front/image_raw", 1, &brightness_control::ImageCallBack, this);
-     image_brightness_pub = nh_.advertise<image_brightness_control::image_brightness>("image_brightness",2);
-     control_command_pub = nh_.advertise<image_brightness_control::control_command>("control_command",2);
-     
-     if(visualization_flag_)
-     {
+	  visualization_ = true;
+	  
+	srv = new dynamic_reconfigure::Server<image_brightness_control::brightCTRConfig> (ros::NodeHandle("~"));
+	dynamic_reconfigure::Server<image_brightness_control::brightCTRConfig>::CallbackType f = boost::bind(&brightness_control::reconfig, this, _1, _2);
+	srv->setCallback(f);
+		
+	 cam_sub_ = it_.subscribeCamera("/camera_front/image_raw", 1, &brightness_control::ImageCallBack, this);
+	 image_brightness_pub = nh_.advertise<image_brightness_control::image_brightness>("image_brightness",2);
+	 control_command_pub = nh_.advertise<image_brightness_control::control_command>("/camera_front/control_command",2);
+
+	 if(visualization_)
+	 {
 		  cvNamedWindow("histogram",1);
-	  }
+	 }
+	 
   }
   
+  void brightness_control::reconfig(image_brightness_control::brightCTRConfig &config, uint32_t level) 
+  {
+	shutter_value_ 		= 	config.shutter_value;
+	gain_value_ 		= 	config.gain_value;
+	expected_centroid_ 	= 	config.expected_centroid;
+	shutter_control_ 	= 	config.shutter_control;
+	gain_control_ 		= 	config.gain_control;
+	visualization_ 		= 	config.visualization;
+	p_shutter_ 			=	config.p_shutter;
+	i_shutter_ 			=	config.i_shutter;
+	d_shutter_ 			=	config.d_shutter;
+	p_gain_ 			=	config.p_gain;
+	i_gain_ 			=	config.i_gain;
+	d_gain_ 			=	config.d_gain;
+  }
 	void brightness_control::DrawHistogram(IplImage* imgHist, CvHistogram *hist)
 	{
-		 float histMax = 1.0;
-		 for(int i=0;i<16;i++)
-		 {
-			  int scaleX = 256/16;
-			  float histValue = cvQueryHistValue_1D(hist, i);
-			  //float nextValue = cvQueryHistValue_1D(hist, i+1);
-			  CvPoint pt1 = cvPoint(i*scaleX, 64);
-			  CvPoint pt2 = cvPoint(i*scaleX+scaleX, 64);
-			  CvPoint pt3 = cvPoint(i*scaleX+scaleX, 64-histValue*64/histMax);
-			  CvPoint pt4 = cvPoint(i*scaleX, 64-histValue*64/histMax);
-			  int numPts = 5;
-			  CvPoint pts[] = {pt1, pt2, pt3, pt4, pt1};
-			  cvFillConvexPoly(imgHist, pts, numPts, cvScalar(255));
-		 }
-		 return;
+		float histMax = 1.0;
+		for(int i=0;i<16;i++)
+		{
+		  int scaleX = 256/16;
+		  float histValue = cvQueryHistValue_1D(hist, i);
+		  //float nextValue = cvQueryHistValue_1D(hist, i+1);
+		  CvPoint pt1 = cvPoint(i*scaleX, 64);
+		  CvPoint pt2 = cvPoint(i*scaleX+scaleX, 64);
+		  CvPoint pt3 = cvPoint(i*scaleX+scaleX, 64-histValue*64/histMax);
+		  CvPoint pt4 = cvPoint(i*scaleX, 64-histValue*64/histMax);
+		  int numPts = 5;
+		  CvPoint pts[] = {pt1, pt2, pt3, pt4, pt1};
+		  cvFillConvexPoly(imgHist, pts, numPts, cvScalar(255));
+		}
+		return;
 	}
 
   void brightness_control::ImageCallBack( const sensor_msgs::ImageConstPtr& image_msg,
@@ -53,7 +72,9 @@ namespace golfcar_vision{
             ROS_ERROR("Failed to convert image");
             return;
             }
-            
+
+        cvSetImageROI(color_image, cvRect(24, 306, 601, 156));
+
         gray_image = cvCreateImage(cvGetSize(color_image),8,1);
         cvCvtColor(color_image, gray_image, CV_BGR2GRAY);
         
@@ -66,7 +87,7 @@ namespace golfcar_vision{
         cvNormalizeHist(hist,1);
         image_brightness_control::image_brightness brightness_indicator;
         brightness_indicator.header = info_msg->header;
-        for(int i = 0; i < 16; i++)
+        for(int i = 0; i < numBins; i++)
         {
 			  float hist_element_value = cvQueryHistValue_1D(hist, i);
 			  brightness_indicator.hist_elements.push_back(hist_element_value);
@@ -74,52 +95,103 @@ namespace golfcar_vision{
         image_brightness_pub.publish(brightness_indicator);
         cvReleaseImage(&gray_image);
         
-        if(visualization_flag_)
+        if(visualization_)
         {
-			   IplImage* imgHist = cvCreateImage(cvSize(256, 64),8,1);
-			   cvZero(imgHist);
-				DrawHistogram(imgHist, hist);
-				cvShowImage("histogram", imgHist);
-				cvWaitKey(10);
-				cvReleaseImage(&imgHist);
+			IplImage* imgHist = cvCreateImage(cvSize(256, 64),8,1);
+			cvZero(imgHist);
+			DrawHistogram(imgHist, hist);
+			cvShowImage("histogram", imgHist);
+			cvWaitKey(1);
+			cvReleaseImage(&imgHist);
         }
         
-        generate_ctrl_cmd(brightness_indicator);
+        generate_ctrl_cmd(brightness_indicator);		
   }
   
   void brightness_control::generate_ctrl_cmd(image_brightness_control::image_brightness & brightness_indicator)
   {
 	  //simple control policy;
-	  
 	  double distance_weighted_pixel = 0;
 	  for(int i=0; i<16; i++)
 	  {
-		  int bin_center = 256/32 + 256/16*i;
+		  int bin_center = 256/32 + i*256/16;
 		  distance_weighted_pixel = distance_weighted_pixel + double(bin_center) * brightness_indicator.hist_elements[i];
 	  }
-	  
 	  int brightness_centroid = (int)distance_weighted_pixel;
-	  int control_step = (expected_centroid_ - brightness_centroid)/5;
+	  if(visualization_) ROS_INFO("brightness_centroid %d", brightness_centroid);
 	  
-	  if(control_step==0)
+	  control_command_.header = brightness_indicator.header;
+	  shutter_control(brightness_centroid);
+	  gain_control(brightness_centroid);
+	  control_command_pub.publish(control_command_);
+  }
+  
+  void brightness_control::shutter_control(int brightness_centroid)
+  {
+	  float error = expected_centroid_ - brightness_centroid;
+	  float i_err  = error;
+
+	  if(fabs(error) > 50)
 	  {
-		  if(expected_centroid_ - brightness_centroid >= 0) control_step = 1;
-		  else control_step = -1;
+	    i_err = error *50/fabs(error);
 	  }
-	  if(control_step >  10) control_step  =   10;
-	  if(control_step < -10) control_step = -10;
 	  
-	  control_command_ = control_command_ + control_step;
+	  float k_err = (error - last_err);
+	  if(fabs(k_err) > 20)
+	  {
+		  k_err = k_err*20/fabs(k_err);
+	  }
+
+	  float d_err = (error - 2*last_err+last_err2);
+	  float control_step = k_p* k_err+ k_i*i_err+k_d*d_err;
+
+	  last_err2 = last_err;
+	  last_err = error;
+
+	  if(fabs(control_step) > 10)
+	  {
+		   control_step = 10 * control_step / fabs(control_step);
+	  }
 	  
-	  if(control_command_<=1) control_command_=1;
-	  if(control_command_>=100) control_command_ = 100;
+	  if(fabs(error) < 20)
+	  {
+		  control_step = 0;
+	  }
+	  cmd = cmd + control_step;
+
+	  if(cmd < 10)
+	  {
+		  cmd = 10;
+	  }
+	  else if (cmd > 300)
+	  {
+		  cmd = 300;
+	  }
+
+	  control_command_.gain = cmd;
+  }
+  
+    void brightness_control::gain_control(int brightness_centroid)
+  {
+	  	  
+	  float error = expected_centroid_ - brightness_centroid;
+	  int tmp_gain = 0;
+	  if(fabs(error) > 80)
+	  {
+	    tmp_gain = error * 20.0/fabs(error);
+	  }
 	  
-	  if(visualization_flag_) ROS_INFO("brightness_centroid %d", brightness_centroid);
-	  
-	  image_brightness_control::control_command control_tmp;
-	  control_tmp.header = brightness_indicator.header;
-	  control_tmp.shutter = control_command_;
-	  control_command_pub.publish(control_tmp);
+	  gain_value_ += tmp_gain;
+	  if(gain_value_ < 40)
+	  {
+		  gain_value_=40;
+	  }
+	  else if(gain_value_> 250)
+	  {
+		  gain_value_=250;
+	  }
+	 
+	  control_command_.gain = gain_value_;
   }
   
   brightness_control::~brightness_control()
--- smart-ros-pkg/golfcar_perception/image_brightness_control/src/brightness_control.h
+++ smart-ros-pkg/golfcar_perception/image_brightness_control/src/brightness_control.h
@@ -16,6 +16,8 @@
 
 #include "image_brightness_control/image_brightness.h"
 #include "image_brightness_control/control_command.h"
+#include <dynamic_reconfigure/server.h>
+#include "image_brightness_control/brightCTRConfig.h"
 
 using namespace std;
 using namespace ros;
@@ -24,29 +26,38 @@ using namespace tf;
 namespace golfcar_vision{
 
     class brightness_control {
-        public:
-        brightness_control();
-        ~brightness_control();        
-    
-        private:
-        ros::NodeHandle nh_, private_nh_;
-        image_transport::ImageTransport it_;
-        image_transport::CameraSubscriber cam_sub_;
-	sensor_msgs::CvBridge bridge_;
-	void ImageCallBack( const sensor_msgs::ImageConstPtr& image_msg,
-					   const sensor_msgs::CameraInfoConstPtr& info_msg);
-   	ros::Publisher image_brightness_pub;
-	ros::Publisher control_command_pub;
-	int control_command_;
-	int expected_centroid_;
-	bool visualization_flag_;
-	void DrawHistogram(IplImage* imgHist, CvHistogram *hist);
-	void generate_ctrl_cmd(image_brightness_control::image_brightness & brightness_indicator);
-	double last_err;
-	double cmd;
-	double k_p, k_i;
-	int ctrl_gain;
-    };
+		public:
+		brightness_control();
+		~brightness_control();        
+
+		private:
+		ros::NodeHandle nh_, private_nh_;
+		image_transport::ImageTransport it_;
+		image_transport::CameraSubscriber cam_sub_;
+		sensor_msgs::CvBridge bridge_;
+		dynamic_reconfigure::Server<image_brightness_control::brightCTRConfig>  *srv;
+
+		void ImageCallBack( const sensor_msgs::ImageConstPtr& image_msg, const sensor_msgs::CameraInfoConstPtr& info_msg);
+					   
+		ros::Publisher image_brightness_pub;
+		ros::Publisher control_command_pub;
+
+		bool visualization_flag_;
+		void DrawHistogram(IplImage* imgHist, CvHistogram *hist);
+		double last_err, last_err2;
+		double cmd;
+		double k_p, k_i, k_d;
+
+		double p_shutter_, i_shutter_, d_shutter_;
+		double p_gain_, i_gain_, d_gain_;
+		int shutter_value_, gain_value_, expected_centroid_;
+		bool shutter_control_, gain_control_, visualization_;
+		void reconfig(image_brightness_control::brightCTRConfig &config, uint32_t level);
+		image_brightness_control::control_command control_command_;
+		void generate_ctrl_cmd(image_brightness_control::image_brightness & brightness_indicator);
+		void shutter_control(int brightness_centroid);
+		void gain_control(int brightness_centroid);
+		};
 };
 
 #endif
