ec7a153d4d6b4276bf6d6ae6ee177f6dc4ae568d
liuwei <liu_wei@nus.edu.sg>
2013-04-11 21:23:06 +0800

    Add rrts_ompl package(ompl based rrtstar) for research convenience, general framework finished and need further updated

---
+++ smart-ros-pkg/golfcar_planner/rrts_ompl/include/rrts_ompl/local_cspace.h
@@ -0,0 +1,20 @@
+/*
+ * local_cspace.h
+ *
+ *  Created on: Jan 24, 2013
+ *      Author: liuwlz
+ */
+
+#ifndef LOCAL_CSPACE_H_
+#define LOCAL_CSPACE_H_
+
+#include <ros/ros.h>
+#include <nav_msgs/OccupancyGrid.h>
+#include <geometry_msgs/Point32.h>
+#include <geometry_msgs/Pose.h>
+
+class local_map{
+public:
+
+};
+#endif /* LOCAL_CSPACE_H_ */
---
+++ smart-ros-pkg/golfcar_planner/rrts_ompl/src/local_cspace.cpp
@@ -0,0 +1,10 @@
+/*
+ * local_cspace.cpp
+ *
+ *  Created on: Jan 24, 2013
+ *      Author: liuwlz
+ */
+
+
+
+
