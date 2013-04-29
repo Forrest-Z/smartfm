20e8f7f286cf6a9464bd88a4145f8678ccdfadc5
pvt <pvazteixeira@gmail.com>
2013-04-29 11:49:55 +0800

    fixed issue with featureviewernode

--- smart-ros-pkg/experimental/infrastructure_camera_tracker/src/node/FeatureViewerNode.cpp
+++ smart-ros-pkg/experimental/infrastructure_camera_tracker/src/node/FeatureViewerNode.cpp
@@ -3,15 +3,17 @@
 #include <opencv2/opencv.hpp>
 #include <cv_bridge/cv_bridge.h>
 
-FeatureViewerNode::FeatureViewerNode(): node_handle("~"),
-                                        image_subscriber(node_handle, "image", 1),
+FeatureViewerNode::FeatureViewerNode(): //node_handle("~"),
+                                        img_transport(node_handle),
+                                        img_subscriber(img_transport, "image", 1),
                                         feature_subscriber(node_handle, "features", 1),
-                                        synchronizer(sync_policy(1), image_subscriber, feature_subscriber),
+                                        synchronizer(sync_policy(1), img_subscriber, feature_subscriber),
                                         window_name("feature viewer")
 {
   // bind the callback and tell boost::bind() to forward the first and second arguments it receives
-  boost::function<void (sensor_msgs::Image::ConstPtr&, infrastructure_camera_tracker::Features&)> callback( boost::bind( &FeatureViewerNode::callback, this, _1, _2 ) );
-  //synchronizer.registerCallback( &callback );
+  // boost::function<void (const sensor_msgs::Image::ConstPtr&, const infrastructure_camera_tracker::Features::ConstPtr&)> sync_callback( boost::bind( &FeatureViewerNode::callback, this, _1, _2 ) );
+  // synchronizer.registerCallback( sync_callback );                                        // this does not work
+  synchronizer.registerCallback( boost::bind(&FeatureViewerNode::callback, this, _1, _2) ); // but this does
 
   //cv::namedWindow(window_name, cv::WINDOW_AUTOSIZE);
 };
@@ -21,11 +23,11 @@ FeatureViewerNode::~FeatureViewerNode()
   // close window
 };
 
-void FeatureViewerNode::callback( sensor_msgs::Image::ConstPtr& image,  infrastructure_camera_tracker::Features& features)
+void FeatureViewerNode::callback( const sensor_msgs::Image::ConstPtr& image, const infrastructure_camera_tracker::Features::ConstPtr& features)
 {
-  //ROS_INFO("received image/features pair\n");
-  //cv::Mat frame = cv_bridge::toCvCopy(image, "bgr8")->image;
-  //cv::imshow(window_name, frame);
+  ROS_INFO("received image/features pair\n");
+  cv::Mat frame = cv_bridge::toCvCopy(image, "bgr8")->image;
+  cv::imshow(window_name, frame);
 
-  //cv::waitKey(10);
+  cv::waitKey(10);
 };
--- smart-ros-pkg/experimental/infrastructure_camera_tracker/src/node/FeatureViewerNode.hpp
+++ smart-ros-pkg/experimental/infrastructure_camera_tracker/src/node/FeatureViewerNode.hpp
@@ -3,9 +3,11 @@
 #include <sensor_msgs/Image.h>                                // Messages
 #include <infrastructure_camera_tracker/Features.h>
 
-//#include <image_transport/image_transport.h>
+#include <image_transport/image_transport.h>
+#include <image_transport/subscriber_filter.h>
 #include <message_filters/subscriber.h>
 #include <message_filters/synchronizer.h>
+#include <message_filters/time_synchronizer.h>
 #include <message_filters/sync_policies/exact_time.h>
 
 #include <string>
@@ -18,14 +20,11 @@ class FeatureViewerNode
     FeatureViewerNode();
     ~FeatureViewerNode();
 
-    void callback(  sensor_msgs::Image::ConstPtr& image,
-                    infrastructure_camera_tracker::Features& features);
-
   private:
     ros::NodeHandle node_handle;
 
-    //image_transport::ImageTransport image_transport;
-    message_filters::Subscriber<sensor_msgs::Image> image_subscriber;
+    image_transport::ImageTransport img_transport;
+    image_transport::SubscriberFilter img_subscriber;
     message_filters::Subscriber<infrastructure_camera_tracker::Features> feature_subscriber;
 
     typedef message_filters::sync_policies::ExactTime<sensor_msgs::Image, infrastructure_camera_tracker::Features> sync_policy;
@@ -33,4 +32,7 @@ class FeatureViewerNode
     message_filters::Synchronizer<sync_policy> synchronizer;
 
     std::string window_name;
+
+    void callback(  const sensor_msgs::Image::ConstPtr& image,
+                    const infrastructure_camera_tracker::Features::ConstPtr& features);
 };
