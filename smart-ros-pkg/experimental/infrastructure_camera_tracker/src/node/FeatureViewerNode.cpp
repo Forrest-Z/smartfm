8429eb9127e24b0ea0f13ce9e3862d3beb6ea020
pvt <pvazteixeira@gmail.com>
2013-04-29 15:04:58 +0800

    feature viewer now working properly

--- smart-ros-pkg/experimental/infrastructure_camera_tracker/src/node/FeatureViewerNode.cpp
+++ smart-ros-pkg/experimental/infrastructure_camera_tracker/src/node/FeatureViewerNode.cpp
@@ -3,7 +3,7 @@
 #include <opencv2/opencv.hpp>
 #include <cv_bridge/cv_bridge.h>
 
-FeatureViewerNode::FeatureViewerNode(): //node_handle("~"),
+FeatureViewerNode::FeatureViewerNode(): node_handle("~"),
                                         img_transport(node_handle),
                                         img_subscriber(img_transport, "image", 1),
                                         feature_subscriber(node_handle, "features", 1),
@@ -12,10 +12,11 @@ FeatureViewerNode::FeatureViewerNode(): //node_handle("~"),
 {
   // bind the callback and tell boost::bind() to forward the first and second arguments it receives
   // boost::function<void (const sensor_msgs::Image::ConstPtr&, const infrastructure_camera_tracker::Features::ConstPtr&)> sync_callback( boost::bind( &FeatureViewerNode::callback, this, _1, _2 ) );
-  // synchronizer.registerCallback( sync_callback );                                        // this does not work
-  synchronizer.registerCallback( boost::bind(&FeatureViewerNode::callback, this, _1, _2) ); // but this does
+  // synchronizer.registerCallback( sync_callback );                                                // this does not work
 
-  //cv::namedWindow(window_name, cv::WINDOW_AUTOSIZE);
+  synchronizer.registerCallback( boost::bind(&FeatureViewerNode::callback, this, _1, _2) );         // but this does
+
+  cv::namedWindow(window_name, cv::WINDOW_AUTOSIZE);
 };
 
 FeatureViewerNode::~FeatureViewerNode()
@@ -23,11 +24,19 @@ FeatureViewerNode::~FeatureViewerNode()
   // close window
 };
 
-void FeatureViewerNode::callback( const sensor_msgs::Image::ConstPtr& image, const infrastructure_camera_tracker::Features::ConstPtr& features)
+void FeatureViewerNode::callback( const sensor_msgs::Image::ConstPtr& image,
+                                  const infrastructure_camera_tracker::Features::ConstPtr& features)
 {
   ROS_INFO("received image/features pair\n");
   cv::Mat frame = cv_bridge::toCvCopy(image, "bgr8")->image;
-  cv::imshow(window_name, frame);
 
+  // plot features found
+  for (unsigned int i = 0; i < features->features.size(); ++i)
+  {
+    cv::circle(frame, cv::Point2f(features->features[i].x, features->features[i].y), 1, cv::Scalar(0, 255, 0), -1, CV_AA, 0 );
+  }
+
+  // display image
+  cv::imshow(window_name, frame);
   cv::waitKey(10);
 };
