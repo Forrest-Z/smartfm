eed298f097c2d5ea4c212dce0ee91668d0130d2a
volkan <volkan@Small-git.(none)>
2013-04-30 13:22:27 +0800

    Initial commit of Tjunction_momdp_sarsop

---
+++ smart-ros-pkg/golfcar_planner/Tjunction_momdp_sarsop/src/Vehicle_momdp_realPed.cpp
@@ -0,0 +1,84 @@
+/*
+ * pedestrian_momdp.cpp
+ *
+ *  Created on: Sep 15, 2011
+ *      Author: golfcar
+ */
+
+#include "Vehicle_momdp_realPed.h"
+
+
+pedestrian_momdp::pedestrian_momdp()
+{
+    ROS_INFO("Starting Pedestrian Avoidance ... ");
+
+    /// Setting up subsciption
+    ros::NodeHandle nh;
+    speedSub_ = nh.subscribe("odom", 1, &pedestrian_momdp::speedCallback, this);
+    pedSub_ = nh.subscribe("ped_local_frame_vector", 1, &pedestrian_momdp::pedPoseCallback, this); 
+
+    ros::NodeHandle n("~");
+
+    string  ped_id_file, policy_file, model_file;
+    double frequency;
+    int simLen, simNum;
+    bool use_sim_time, stationary;
+    n.param("pedestrian_id_file", ped_id_file, string(""));
+    n.param("policy_file", policy_file, string(""));
+    n.param("model_file", model_file, string(""));
+    n.param("simLen", simLen, 100);
+    n.param("simNum", simNum, 100);
+    n.param("frequency", frequency, 2.0);
+    n.param("stationary",stationary, false);
+    nh.param("use_sim_time", use_sim_time, false);
+
+    move_base_speed_=nh.subscribe("/speed_advisor_cmdvel",1, &pedestrian_momdp::moveSpeedCallback, this);
+    //goalPub_ = nh.advertise<geometry_msgs::PoseStamped>("move_base_simple/goal",1);
+
+    momdp = new ped_momdp(model_file, policy_file, simLen, simNum, stationary, frequency, use_sim_time, nh);
+
+    ros::spin();
+}
+
+pedestrian_momdp::~pedestrian_momdp()
+{
+    momdp->~ped_momdp();
+}
+
+void pedestrian_momdp::speedCallback(nav_msgs::Odometry odo)
+{
+    momdp->updateRobotSpeed(odo.twist.twist.linear.x);
+}
+
+void pedestrian_momdp::moveSpeedCallback(geometry_msgs::Twist speed)
+{
+    momdp->updateSteerAnglePublishSpeed(speed);
+
+}
+
+void pedestrian_momdp::pedPoseCallback(ped_momdp_sarsop::ped_local_frame_vector lPedLocal)
+{
+    for(int ii=0; ii< lPedLocal.ped_local.size(); ii++)
+    {
+        /// search for proper pedestrian to update
+        bool foundPed = momdp->updatePedRobPose(lPedLocal.ped_local[ii]);;
+
+        if(!foundPed)
+        {
+            ///if ped_id does not match the old one create a new pomdp problem.
+            ROS_INFO(" Creating  a new pedestrian problem #%d", lPedLocal.ped_local[ii].ped_id);
+            momdp->addNewPed(lPedLocal.ped_local[ii]);
+        }
+    }
+
+
+}
+
+
+int main(int argc, char** argv)
+{
+    ros::init(argc, argv, "mdp");
+    pedestrian_momdp mdp_node;
+
+
+}
---
+++ smart-ros-pkg/golfcar_planner/Tjunction_momdp_sarsop/src/momdp_realveh.h
@@ -0,0 +1,143 @@
+/*
+ * momdp.h
+ *
+ *  Created on: Mar 4, 2012
+ *      Author: golfcar
+ */
+
+#ifndef MOMDP_H_
+#define MOMDP_H_
+
+#include <nav_msgs/Odometry.h>
+#include <sensor_msgs/LaserScan.h>
+#include <ros/ros.h>
+#include <geometry_msgs/Twist.h>
+#include <sensor_msgs/PointCloud.h>
+#include <sensing_on_road/pedestrian_laser_batch.h>
+#include <dataAssoc_experimental/PedDataAssoc_vector.h>
+#include <dataAssoc_experimental/PedDataAssoc.h>
+#include "MOMDP.h"
+#include "ParserSelector.h"
+#include "AlphaVectorPolicy.h"
+#include "ROS_SimulationEngine.h"
+#include "GlobalResource.h"
+#include <rosgraph_msgs/Clock.h>
+#include <geometry_msgs/PoseWithCovarianceStamped.h>
+#include <geometry_msgs/PoseStamped.h>
+#include <ped_momdp_sarsop/peds_believes.h>
+#include <ped_momdp_sarsop/ped_local_frame.h>
+#include <ped_momdp_sarsop/ped_local_frame_vector.h>
+#include <pnc_msgs/speed_contribute.h>
+
+
+
+ #include <std_msgs/Float32.h>
+
+using namespace std;
+
+struct GOAL
+{
+    int x, y;
+};
+
+struct POSE
+{
+    double x;
+    double y;
+    double z;
+    double yaw;
+};
+
+struct PED_MOMDP
+{
+    int id;
+    POSE ped_pose;
+ //V   double rob_pose;
+    POSE rob_pose; //V
+
+    SharedPointer<BeliefWithState> currBelSt;
+    int currAction;
+    int currSVal; /// observed variable
+    ros::Time last_update;
+    /// some grid representation
+    //frame id
+};
+
+
+
+class ped_momdp
+{
+public:
+    vector<PED_MOMDP> lPedInView;
+
+    ped_momdp(string model_file, string policy_file, int simLen, int simNum, bool stationary, double frequency, bool use_sim_time, ros::NodeHandle& nh);
+    
+    ~ped_momdp();
+    
+    void updateRobotSpeed(double speed);
+
+    void addNewPed(ped_momdp_sarsop::ped_local_frame &ped);
+
+    bool updatePedRobPose(ped_momdp_sarsop::ped_local_frame &ped);
+
+    void updateSteerAnglePublishSpeed(geometry_msgs::Twist speed);
+
+    int ped_str_pub;
+
+    geometry_msgs::Twist volkan;// This is not the correct way to see the robot state. Just for 
+    
+
+
+
+private:
+    int X_SIZE, Y_SIZE;
+    double dX, dY, dgrid;
+    int RobInitSt,DrivInitSt;
+    double momdp_problem_timeout;
+    bool robot_pose_available;
+    double robotx_, roboty_, robotz_, robotspeedx_, momdp_speed_;
+    bool use_sim_time_, stationary_;
+    ros::Timer timer_;
+    vector<GOAL> lPedGoal;
+    SolverParams* solver_param;
+    SharedPointer<MOMDP> problem;
+    SharedPointer<AlphaVectorPolicy> policy;
+    ROS_SimulationEngine engine;
+    map<string, int> ObsStateMapping;
+    map<string, int> ObsSymbolMapping;
+    ros::Publisher believesPub_, cmdPub_, RobState_pub,	AnalysisCrash_pub, AnalysisTime_pub;
+
+    std_msgs::Float32 analysisvar;
+    double total_belief_value;
+
+
+
+
+    void initPedMOMDP(ped_momdp_sarsop::ped_local_frame& ped_local);
+    void controlLoopMOMDP(const ros::TimerEvent &e);//Controller Using MOMDP
+    void controlLoopConstantGap0(const ros::TimerEvent &e);//Controller Using Constant Gap Approach(Gap-0)
+    void controlLoopConstantGap1(const ros::TimerEvent &e);//Controller Using Constant Gap Approach(Gap-1)
+    void controlLoopConstantGap2(const ros::TimerEvent &e);//Controller Using Constant Gap Approach(Gap-2)
+    void controlLoopConstantGap3(const ros::TimerEvent &e);//Controller Using Constant Gap Approach(Gap-3)
+
+
+    void clean_momdp_problem();
+    
+
+    void publish_belief(); //No Need for this function, only for analysis!
+
+    int policy_initialize(string model_file, string policy_file, int simLen, int simNum);
+
+    void updateBelief(int id, int safeAction);
+
+    int getCurrentState(double drob, double ddriv, int rob_init_st, int driv_init_st);
+
+    int getCurrObs(int id,int driv_init_st);
+
+    int getXGrid(double x);
+
+    int getYGrid(double y);
+    
+    int getStGrid(double dist);
+};
+#endif /* MOMDP_H_ */
