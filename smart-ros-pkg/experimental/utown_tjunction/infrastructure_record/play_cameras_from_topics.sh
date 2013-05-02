39f69f66c55a81998bffe7997cbf68b323616fc9
Tawit <iamtawit@gmail.com>
2013-05-02 13:11:56 +0800

    add infrastructure_record folder

---
+++ smart-ros-pkg/experimental/utown_tjunction/infrastructure_record/play_cameras_from_topics.sh
@@ -0,0 +1,2 @@
+rosrun image_view image_view image:=/Cam1/camera/image_raw _image_transport:="compressed" &
+rosrun image_view image_view image:=/Cam0/camera/image_raw _image_transport:="compressed"
