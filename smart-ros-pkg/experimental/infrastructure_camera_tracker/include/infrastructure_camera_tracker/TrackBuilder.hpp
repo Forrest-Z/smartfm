ee08aabd9e13bac8aa344e3abcc48ba11db65e6e
pvt <pvazteixeira@gmail.com>
2013-05-07 17:18:07 +0800

    minimal state for the TrackBuilder class

--- smart-ros-pkg/experimental/infrastructure_camera_tracker/include/infrastructure_camera_tracker/TrackBuilder.hpp
+++ smart-ros-pkg/experimental/infrastructure_camera_tracker/include/infrastructure_camera_tracker/TrackBuilder.hpp
@@ -11,16 +11,51 @@ class TrackBuilder
   public:
     TrackBuilder();
     ~TrackBuilder();
+
+    void add( std::vector<long unsigned int>& ids,
+      std::vector<cv::Point2f>& positions,
+      std::vector<double>& timestamps);
+
+    void clear(void)
+    {
+      tracks.clear();
+    };
+
+    inline std::vector<Track> getActiveTracks(void) const
+    {
+      std::vector<Track> result;
+      for(std::map<long unsigned int, Track>::const_iterator it = tracks.begin(); it != tracks.end(); ++it)
+      {
+        if (it->second.isActive())
+        {
+          result.push_back(it->second);
+        }
+      }
+      return(result);
+    };
+
+    // std::vector<Track> getMovingTracks(void) const;
+
+    inline std::vector<Track> getTracks(void) const
+    {
+      std::vector<Track> result;
+      for(std::map<long unsigned int, Track>::const_iterator it = tracks.begin(); it != tracks.end(); ++it)
+      {
+        result.push_back(it->second);
+      }
+      return(result);
+    };
+
+    void update(std::vector<long unsigned int>& ids,
+      std::vector<cv::Point2f>& positions,
+      std::vector<double>& timestamps,
+      std::vector<bool>& active);
+
   private:
     std::map<long unsigned int, Track> tracks;
+    // std::map<long unsigned int, bool> moving;
 
-    std::vector<Track> getActiveTracks(void);
-    std::vector<Track> getMovingTracks(void);
-    std::vector<Track> getTracks(void);
-    void update(std::vector<long unsigned int> ids,
-                std::vector<cv::Point2f> positions,
-                std::vector<double> timestamps,
-                std::vector<bool> active);
+
 };
 
 #endif
