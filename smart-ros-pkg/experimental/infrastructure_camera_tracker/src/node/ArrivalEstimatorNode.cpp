6aa1a42459d3018b503ef1ac13ecae0f78444c21
pvt <pvazteixeira@gmail.com>
2013-05-14 13:26:52 +0800

    working on the ROS node for arrival estimation

--- smart-ros-pkg/experimental/infrastructure_camera_tracker/src/node/ArrivalEstimatorNode.cpp
+++ smart-ros-pkg/experimental/infrastructure_camera_tracker/src/node/ArrivalEstimatorNode.cpp
@@ -1,8 +1,10 @@
 #include "ArrivalEstimatorNode.hpp"
+#include "cv_msg_bridge.hpp"
 
 ArrivalEstimatorNode::ArrivalEstimatorNode(): node_handle("~")
 {
   feature_subscriber = node_handle.subscribe("features", 100, &ArrivalEstimatorNode::featureCallback, this);
+  line_subscriber = node_handle.subscribe("arrival_line", 100, &ArrivalEstimatorNode::lineCallback, this);
   arrival_publisher = node_handle.advertise<infrastructure_camera_tracker::ArrivalSet>("arrivals", 1000);
   track_publisher = node_handle.advertise<infrastructure_camera_tracker::TrackSet>("tracks", 1000);
 };
@@ -14,6 +16,8 @@ ArrivalEstimatorNode::~ArrivalEstimatorNode()
 
 void ArrivalEstimatorNode::featureCallback(const infrastructure_camera_tracker::TrackedFeatureSet::ConstPtr& msg)
 {
+  namespace ict = infrastructure_camera_tracker;
+
   last_header = msg->header;
 
   unsigned int N = msg->features.size();
@@ -33,24 +37,59 @@ void ArrivalEstimatorNode::featureCallback(const infrastructure_camera_tracker::
   if (msg->new_set)
   {
     // the arrival estimator can also publish tracks as it needs them for its own estimation
-    // ROS_INFO("Publising track set");
-    // publishTracks();
+    ROS_INFO("Publising track set");
+    publishTracks();
     track_builder.clear(); // if stitching isn't implemented
     track_builder.add(ids, points, timestamps);
   }
   else
   {
+    ict::ArrivalSet message;
+    ict::TrackedFeature feature;
+
     track_builder.update(ids, points, timestamps, active);
     // perform arrival estimation
     std::vector<Track> tracks = track_builder.getTracks();
+
+    // loop variables
+    cv::Point2f arrival_point, feature_velocity;
+    float arrival_time, x_arrival;
+
     for(unsigned int i = 0; i < tracks.size(); ++i)
     {
       if(tracks[i].isActive())
       {
+        // get moving average of the velocity
 
+        if (tracks[i].getMAVelocity(5, feature_velocity))
+        {
+          if (1 < cv::norm(feature_velocity))
+          {
+            if (arrival_line.getIntersection(tracks[i].getLast(), feature_velocity, arrival_point, arrival_time, x_arrival))
+            {
+              feature.position = toGeometryMsgsPoint(tracks[i].getLast());
+              feature.time = tracks[i].getLastTime();
+              feature.id = tracks[i].getID();
+              feature.active = true;
+              message.features.push_back(feature);
+              message.velocity.push_back(toGeometryMsgsPoint(feature_velocity));
+              message.arrival_point.push_back(toGeometryMsgsPoint(arrival_point));
+              message.arrival_time.push_back(arrival_time);
+              message.arrival_line   = arrival_line_msg;
+            }
+          }
+        }
       }
     }
+
+    // compute intersection
+    // publish!
   }
+};
+
+void ArrivalEstimatorNode::lineCallback(const infrastructure_camera_tracker::ArrivalLine::ConstPtr& msg)
+{
+
 };
 
 void ArrivalEstimatorNode::publishArrivals(void) const
--- smart-ros-pkg/experimental/infrastructure_camera_tracker/src/node/ArrivalEstimatorNode.hpp
+++ smart-ros-pkg/experimental/infrastructure_camera_tracker/src/node/ArrivalEstimatorNode.hpp
@@ -1,10 +1,12 @@
 #include <ros/ros.h>
 
 #include <infrastructure_camera_tracker/TrackedFeatureSet.h>  // subscribes
+#include <infrastructure_camera_tracker/ArrivalLine.h>        // subscribes
 #include <infrastructure_camera_tracker/ArrivalSet.h>         // publishes
 #include <infrastructure_camera_tracker/TrackSet.h>           // publishes
 
 #include <vector>
+#include <infrastructure_camera_tracker/ArrivalEstimator.hpp>
 #include <infrastructure_camera_tracker/TrackBuilder.hpp>
 
 class ArrivalEstimatorNode
@@ -17,10 +19,15 @@ class ArrivalEstimatorNode
     ros::Publisher  arrival_publisher;
     ros::Publisher  track_publisher;
     ros::Subscriber feature_subscriber;
+    ros::Subscriber line_subscriber;
     std_msgs::Header last_header;
-    TrackBuilder track_builder;
+    ArrivalEstimator arrival_line;
+    infrastructure_camera_tracker::ArrivalLine arrival_line_msg;
+    TrackBuilder  track_builder;
 
     void featureCallback(const infrastructure_camera_tracker::TrackedFeatureSet::ConstPtr& msg);
+
+    void lineCallback(const infrastructure_camera_tracker::ArrivalLine::ConstPtr& msg);
 
     void publishArrivals(void) const;
 
