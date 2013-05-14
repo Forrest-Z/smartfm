1f90c3ee7448656ceb02712e182d54d4afd46fa7
pvt <pvazteixeira@gmail.com>
2013-05-14 13:41:07 +0800

    adding support library to help in the conversion between ROS and OpenCV Point implementations

---
+++ smart-ros-pkg/experimental/infrastructure_camera_tracker/src/node/cv_msg_bridge.hpp
@@ -0,0 +1,16 @@
+#include <opencv2/opencv.hpp>
+#include <geometry_msgs/Point.h>
+
+inline geometry_msgs::Point toGeometryMsgsPoint(const cv::Point2f& point)
+{
+  geometry_msgs::Point pt;
+  pt.x = static_cast<double>(point.x);
+  pt.y = static_cast<double>(point.y);
+  pt.z = 0.0;
+  return(pt);
+};
+
+inline cv::Point2f toCVPoint(const geometry_msgs::Point& point)
+{
+  return(cv::Point2f(point.x, point.y));
+}
