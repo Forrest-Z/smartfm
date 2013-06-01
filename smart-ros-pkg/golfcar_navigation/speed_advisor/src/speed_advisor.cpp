0cdb45ea471f530e7bf2125135844bccab0e4261
golfcart <fmautonomy@gmail.com>
2013-06-01 17:56:21 +0800

    Updated speed_advisor reverse topic

--- smart-ros-pkg/golfcar_navigation/speed_advisor/src/speed_advisor.cpp
+++ smart-ros-pkg/golfcar_navigation/speed_advisor/src/speed_advisor.cpp
@@ -144,7 +144,7 @@ SpeedAdvisor::SpeedAdvisor()
 
     signal_type_ = -1;
 
-    bwd_drive_pub_ = nh_.advertise<std_msgs::Bool>("please_change_this_name", 1);
+    bwd_drive_pub_ = nh_.advertise<std_msgs::Bool>("direction_ctrl", 1);
     recommend_speed_pub_= nh_.advertise<geometry_msgs::Twist>("cmd_vel", 1);
     speed_contribute_pub_ = nh_.advertise<pnc_msgs::speed_contribute>("speed_status",1);
     left_blinker_pub_ = nh_.advertise<std_msgs::Bool>("left_blinker",1);
