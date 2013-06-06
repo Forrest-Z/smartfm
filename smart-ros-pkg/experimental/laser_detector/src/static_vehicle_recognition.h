3f24bdf2c816752ae0a10e33ea464da03be7d8c6
Xiaotong <shenxiaotong1990@126.com>
2013-06-06 22:31:20 +0800

    tried some pcl API for clustering

--- smart-ros-pkg/experimental/laser_detector/src/static_vehicle_recognition.h
+++ smart-ros-pkg/experimental/laser_detector/src/static_vehicle_recognition.h
@@ -9,12 +9,38 @@
 #define STATIC_VEHICLE_RECOGNITION_H_
 
 #include <ros/ros.h>
+#include "rolling_data_types.h"
+#include <pcl/point_cloud.h>
+#include <pcl_ros/point_cloud.h>
+#include <pcl/point_types.h>
+#include <pcl/ros/conversions.h>
+
+namespace golfcar_perception{
+
+
+typedef pcl::PointCloud<RollingPointXYZ> RollingPointCloud;
+typedef pcl::PointCloud<RollingPointXYZNormal> RollingPointCloudNormal;
+
+typedef pcl::PointCloud<pcl::PointXYZ> PointCloud;
+typedef pcl::PointCloud<pcl::PointXYZRGB> PointCloudRGB;
+typedef pcl::PointCloud<pcl::PointNormal> PointCloudNormal;
+typedef pcl::PointCloud<pcl::PointXYZRGBNormal> PointCloudRGBNormal;
+
 
 class static_vehicle_recognition{
 	ros::NodeHandle nh_;
 	ros::Subscriber accu_pc_sub_;
 	ros::Publisher vehicle_pub_;
+	ros::Publisher normals_pub_;
+	ros::Publisher plane_inlier_pub_;
+	ros::Publisher plane_outlier_pub_;
+	ros::Publisher ec_clusters_pub_;
+	bool publish_normal_flag_;
+public:
+	static_vehicle_recognition(void);
+	void accu_pc_callback(const RollingPointCloud::ConstPtr & accu_pc_in);
 };
 
+}
 
 #endif /* STATIC_VEHICLE_RECOGNITION_H_ */
