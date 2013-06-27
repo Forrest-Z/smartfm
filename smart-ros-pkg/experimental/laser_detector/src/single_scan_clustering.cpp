083501d2cb4f605d56613ba87942cccd2880e845
Xiaotong <shenxiaotong1990@126.com>
2013-06-27 19:34:29 +0800

    update the clustering algorithm

--- smart-ros-pkg/experimental/laser_detector/src/single_scan_clustering.cpp
+++ smart-ros-pkg/experimental/laser_detector/src/single_scan_clustering.cpp
@@ -8,9 +8,7 @@
 
 
 #include "single_scan_clustering.h"
-#include <stdio.h>
-#include <stdlib.h>
-#include <time.h>
+
 
 // update the scan from filtered point cloud, assume that the point cloud has index and distance channels
 void golfcar_perception::laser_scan_cloud::update(const sensor_msgs::LaserScan raw_scan)
@@ -26,10 +24,210 @@ void golfcar_perception::laser_scan_cloud::update(const sensor_msgs::LaserScan r
 	}
 }
 
+void golfcar_perception::laser_scan_cluster::compute_minmax(void)
+{
+	min_index_ = * std::min_element(index_.begin(),index_.end());
+	max_index_ = * std::max_element(index_.begin(),index_.end());
+
+	max_distance_ = * std::max_element(distance_.begin(),distance_.end());
+	min_distance_ = * std::min_element(distance_.begin(),distance_.end());
+
+}
+
+template <class PointT>
+inline double golfcar_perception::single_scan_clustering::compute_dist(PointT a, PointT b)
+{
+    return sqrt((a.x - b.x) * (a.x - b.x) + (a.y - b.y)*(a.y - b.y) + (a.z - b.z)*(a.z - b.z));
+}
+
 golfcar_perception::single_scan_clustering::single_scan_clustering(void):nh_("")
 {
 	laser_sub_= nh_.subscribe("/sickldmrs/scan3",5,&single_scan_clustering::scan_callback,this);
 	clusters_pub_ = nh_.advertise<sensor_msgs::PointCloud>("clusters_pts",10);
+
+	line_fitting_inlier_pub_ = nh_.advertise<sensor_msgs::PointCloud2>("line_inliers",10);
+	line_fitting_outlier_pub_ = nh_.advertise<sensor_msgs::PointCloud2 >("line_outliers",10);
+	corners_pub_ = nh_.advertise<sensor_msgs::PointCloud2>("line_corners",10);
+}
+
+void golfcar_perception::single_scan_clustering::sac_line_fitting(sensor_msgs::LaserScan::ConstPtr laser_in)
+{
+	const int num_lines = 8;
+	// convert the laser scan to a pcl point cloud
+	laser_geometry::LaserProjection laser_proj;
+	sensor_msgs::PointCloud pc;
+	laser_proj.projectLaser(*laser_in,pc,200,laser_geometry::channel_option::Index | laser_geometry::channel_option::Distance); // keep track of index to find corners
+	sensor_msgs::PointCloud2 pc2;
+	sensor_msgs::convertPointCloudToPointCloud2(pc,pc2);
+	pcl::PointCloud<pcl::PointXYZ> laser_pcl;
+	pcl::fromROSMsg(pc2,laser_pcl);
+	//copy the index and distance and do the same operation as the points: x - > index ,y -> distance
+	pcl::PointCloud<pcl::PointXYZ> index_dist_cloud;
+	index_dist_cloud.points.resize(pc.points.size());
+	for(unsigned int i = 0 ; i < pc.points.size() ; i++)
+	{
+		index_dist_cloud.points[i].x = pc.channels[0].values[i];
+		index_dist_cloud.points[i].y = pc.channels[1].values[i];
+	}
+
+	//record the clustering result and find the corners
+	std::vector<laser_scan_cluster> line_clusters;
+
+	// line fitting
+
+	pcl::PointCloud<pcl::PointXYZ> laser_outliers_pcl;
+	pcl::PointCloud<pcl::PointXYZI> laser_inliers_pcl;
+	pcl::SACSegmentation<pcl::PointXYZ> line_seg;
+	line_seg.setOptimizeCoefficients(true);
+	line_seg.setModelType(pcl::SACMODEL_LINE);
+	line_seg.setMaxIterations(500);
+	line_seg.setDistanceThreshold(0.2);
+
+	pcl::ModelCoefficients::Ptr line_coef(new pcl::ModelCoefficients);
+	pcl::PointIndices::Ptr line_inliers_ind (new pcl::PointIndices);
+
+	pcl::ExtractIndices<pcl::PointXYZ> extract;
+	pcl::ExtractIndices<pcl::PointXYZ> extract_info;
+
+	laser_outliers_pcl = laser_pcl;
+	laser_inliers_pcl.header = laser_pcl.header;
+
+	pcl::PointCloud<pcl::PointXYZ> pcl_container;
+	pcl::PointCloud<pcl::PointXYZ> index_dist_container;
+
+
+
+	srand(time(NULL));
+    int line_id = 0;
+	for(int j = 0; j< num_lines; j++)
+	{
+		//make sure the operations are the same and there are equal numbers of points in the clouds
+		assert(index_dist_cloud.points.size() == laser_outliers_pcl.points.size());
+		//cout<<" 1 size is "<<index_dist_cloud.points.size()<<" "<< laser_outliers_pcl.points.size();
+
+		line_seg.setInputCloud(laser_outliers_pcl.makeShared());
+		line_seg.segment(*line_inliers_ind,*line_coef);
+		extract.setNegative(false);
+		extract_info.setNegative(false);
+		extract.setInputCloud(laser_outliers_pcl.makeShared());
+		extract_info.setInputCloud(index_dist_cloud.makeShared());
+		extract.setIndices(line_inliers_ind);
+		extract_info.setIndices(line_inliers_ind);
+		line_id += 10;
+		extract.filter(pcl_container);
+		extract_info.filter(index_dist_container);
+
+		pcl::PointXYZI point_xyzi;
+		laser_scan_cluster line_cluster;
+		line_cluster.laser_cloud_ = pcl_container;
+		assert(index_dist_container.points.size() == pcl_container.points.size());
+		//cout<<" 2 size is "<<index_dist_container.points.size()<<" "<< pcl_container.points.size();
+		if(pcl_container.points.size() > 4 && pcl_container.points.size() < 20) // at least 8 points in the line, otherwise discard
+		{
+			//copy the points to the inliers and set different line id for visualization
+			for(unsigned int k = 0; k < pcl_container.points.size(); k++)
+			{
+				point_xyzi.x = pcl_container.points[k].x;
+				point_xyzi.y = pcl_container.points[k].y;
+				point_xyzi.z = pcl_container.points[k].z;
+				point_xyzi.intensity = line_id;
+				laser_inliers_pcl.points.push_back(point_xyzi);
+
+				//copy the index and distance here
+				line_cluster.distance_.push_back(index_dist_container.points[k].y);
+				line_cluster.index_.push_back(index_dist_container.points[k].x);
+				//copy the coefficients
+				cout<<line_coef->values.size()<<" coefficients" <<endl;
+			}
+
+			// make sure that line cluster contains some data, otherwise there is segmentation fault error
+			line_cluster.compute_minmax();
+			line_clusters.push_back(line_cluster);
+		}
+
+		//get the left outliers to do line fitting again
+		extract.setNegative(true);
+		extract.filter(laser_outliers_pcl);
+		extract_info.setNegative(true);
+		extract_info.filter(index_dist_cloud);
+		//std::cout<<"line is 148"<<std::endl;
+
+		if(laser_outliers_pcl.size() < 8)
+		{
+			break;
+		}
+
+
+	}
+	//std::cout<<"line is 151"<<std::endl;
+	sensor_msgs::PointCloud2 outliers_pc2, inliers_pc2;
+	pcl::toROSMsg(laser_inliers_pcl,inliers_pc2);
+	pcl::toROSMsg(laser_outliers_pcl,outliers_pc2);
+	line_fitting_outlier_pub_.publish(outliers_pc2);
+	line_fitting_inlier_pub_.publish(inliers_pc2);
+
+	pcl::PointCloud<pcl::PointXYZI> corner_filtered_pcl;
+	pcl::PointXYZI temp_point_xyzi;
+	int corner_id = 0;
+#if 1
+	// find the corners in the line clusters and combine them to a new point cloud
+	for(unsigned int i = 0; i < line_clusters.size();i++)
+		for(unsigned int j= 0; j < line_clusters.size(); j++)
+		{
+			if(j == i) continue;
+			if(compute_dist<geometry_msgs::Point32>(pc.points[line_clusters[i].min_index_] , pc.points[line_clusters[i].max_index_]) > 3.0)
+			{
+				continue;
+			}
+			if(compute_dist<geometry_msgs::Point32>(pc.points[line_clusters[j].min_index_] , pc.points[line_clusters[j].max_index_]) > 3.0)
+			{
+				continue;
+			}
+
+			if(max(compute_dist<geometry_msgs::Point32>(pc.points[line_clusters[i].min_index_] , pc.points[line_clusters[j].max_index_]),
+					compute_dist<geometry_msgs::Point32>(pc.points[line_clusters[j].min_index_] , pc.points[line_clusters[i].max_index_])) > 4.2)
+			{
+				continue;
+			}
+			corner_id += 10;
+			//compute_dist<geometry_msgs::Point32>(pc.points[line_clusters[i].min_index_] , pc.points[line_clusters[j].max_index_]);
+
+			// proximity criterion
+			if((fabs(line_clusters[i].min_index_ - line_clusters[j].max_index_) <= 6  &&
+					compute_dist<geometry_msgs::Point32>(pc.points[line_clusters[i].min_index_] , pc.points[line_clusters[j].max_index_]) < 0.3)
+				|| (fabs(line_clusters[j].min_index_ - line_clusters[i].max_index_) <= 6  &&
+						compute_dist<geometry_msgs::Point32>(pc.points[line_clusters[j].min_index_] , pc.points[line_clusters[i].max_index_]) < 0.3))
+					{
+						for(unsigned int k = 0; k < line_clusters[i].laser_cloud_.points.size(); k++)
+						{
+							temp_point_xyzi.x = line_clusters[i].laser_cloud_.points[k].x;
+							temp_point_xyzi.y = line_clusters[i].laser_cloud_.points[k].y;
+							temp_point_xyzi.z = line_clusters[i].laser_cloud_.points[k].z;
+							temp_point_xyzi.intensity = corner_id;
+							corner_filtered_pcl.points.push_back(temp_point_xyzi);
+						}
+
+						for(unsigned int k = 0; k < line_clusters[j].laser_cloud_.points.size(); k++)
+						{
+							temp_point_xyzi.x = line_clusters[j].laser_cloud_.points[k].x;
+							temp_point_xyzi.y = line_clusters[j].laser_cloud_.points[k].y;
+							temp_point_xyzi.z = line_clusters[j].laser_cloud_.points[k].z;
+							temp_point_xyzi.intensity = corner_id;
+							corner_filtered_pcl.points.push_back(temp_point_xyzi);
+
+						}
+
+					}
+
+		}
+#endif
+	corner_filtered_pcl.header = laser_in->header;
+	sensor_msgs::PointCloud2 corner_filtered_pc2;
+	pcl::toROSMsg(corner_filtered_pcl,corner_filtered_pc2);
+	corners_pub_.publish(corner_filtered_pc2);
+
+
+
 }
 
 void golfcar_perception::single_scan_clustering::scan_callback(sensor_msgs::LaserScan::ConstPtr laser_in)
@@ -52,6 +250,9 @@ void golfcar_perception::single_scan_clustering::scan_callback(sensor_msgs::Lase
            clusters_pc_.channels[0].values[i] = i;
       }
       */
+
+	 // use sansac to find lines in the laser scans
+	 sac_line_fitting(laser_in);
 
 	  // use laser projection to filter the scan
       laser_filtered_.update(*laser_in);
--- smart-ros-pkg/experimental/laser_detector/src/single_scan_clustering.h
+++ smart-ros-pkg/experimental/laser_detector/src/single_scan_clustering.h
@@ -14,7 +14,30 @@
 #include <geometry_msgs/Point32.h>
 #include <laser_geometry/laser_geometry.h>
 
+//for line fitting in PCL
+#include <pcl/ModelCoefficients.h>
+#include <pcl/sample_consensus/model_types.h>
+#include <pcl/sample_consensus/method_types.h>
+#include <pcl/sample_consensus/sac_model_line.h>
+#include <pcl/sample_consensus/impl/sac_model_line.hpp>
+#include <pcl/segmentation/sac_segmentation.h>
+#include <pcl/point_types.h>
+#include <pcl/point_types_conversion.h>
 
+#include <pcl/filters/extract_indices.h>
+#include <pcl/filters/impl/extract_indices.hpp>
+
+#include <sensor_msgs/point_cloud_conversion.h>
+#include <sensor_msgs/PointCloud2.h>
+
+
+
+// for rand()
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#include <algorithm>
+#include <cmath>
 
 namespace golfcar_perception{
 
@@ -32,6 +55,19 @@ public:
 	void update(const sensor_msgs::LaserScan raw_scan);
 };
 
+class laser_scan_cluster{
+public:
+	pcl::PointCloud<pcl::PointXYZ> laser_cloud_;
+	std::vector<int> index_;
+	std::vector<double> distance_;
+	std::vector<float> line_coef;
+	int min_index_;
+	int max_index_;
+	int min_distance_;
+	int max_distance_;
+	void compute_minmax(void);
+};
+
 class single_scan_clustering{
 public:
 	single_scan_clustering(void);
@@ -39,10 +75,21 @@ public:
 	ros::Subscriber laser_sub_;
 	ros::Publisher clusters_pub_;
 
+	ros::Publisher line_fitting_inlier_pub_;
+	ros::Publisher line_fitting_outlier_pub_;
+
+	ros::Publisher corners_pub_;
+
 	sensor_msgs::PointCloud clusters_pc_;
 	laser_geometry::LaserProjection laser_projection_;
 	laser_scan_cloud laser_filtered_;
+
+
+
 	void scan_callback(sensor_msgs::LaserScan::ConstPtr laser_in);
+	void sac_line_fitting(sensor_msgs::LaserScan::ConstPtr laser_in);
+	template <class PointT>
+	inline double compute_dist(PointT a, PointT b);
 
 };
 
