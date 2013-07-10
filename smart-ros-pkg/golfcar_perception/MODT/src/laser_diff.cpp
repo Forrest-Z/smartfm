46cd2bd72d6bb27ffccc80425bf9bf8b6425ef53
baoxing <marskahn@gmail.com>
2013-07-10 19:04:31 +0800

    try to use accumulte image for moving object detection, but noise is still a big issue;

--- smart-ros-pkg/golfcar_perception/MODT/src/laser_diff.cpp
+++ smart-ros-pkg/golfcar_perception/MODT/src/laser_diff.cpp
@@ -29,7 +29,7 @@
 
 using namespace std;
 using namespace ros;
-using namespace tf;
+//using namespace tf;
 using namespace cv;
 
 typedef boost::shared_ptr<nav_msgs::Odometry const> OdomConstPtr;
@@ -55,39 +55,34 @@ private:
 	message_filters::Subscriber<sensor_msgs::LaserScan>     *laser_sub1_;
 	message_filters::Subscriber<sensor_msgs::LaserScan>     *laser_sub2_;
 	message_filters::Subscriber<sensor_msgs::LaserScan>     *laser_sub3_;
+
 	message_filters::TimeSynchronizer<sensor_msgs::LaserScan, sensor_msgs::LaserScan, sensor_msgs::LaserScan, sensor_msgs::LaserScan> *sync_;
 	void scanProcess (const sensor_msgs::LaserScan::ConstPtr& scan_in0, const sensor_msgs::LaserScan::ConstPtr& scan_in1,
 					  const sensor_msgs::LaserScan::ConstPtr& scan_in2, const sensor_msgs::LaserScan::ConstPtr& scan_in3);
-	void extract_moving_points ();
+	void vertical_laser_pair(const sensor_msgs::LaserScan::ConstPtr& scan_in_a, const sensor_msgs::LaserScan::ConstPtr& scan_in_b, sensor_msgs::LaserScan & vertical_scan);
 	bool pointInPolygon(geometry_msgs::Point32 p, vector<geometry_msgs::Point32> poly);
+	void DP_segment (sensor_msgs::PointCloud scan_pointcloud, geometry_msgs::PoseStamped laser_origin);
+	void extract_moving_points();
+	void spacePt2ImgP(geometry_msgs::Point32 & spacePt, Point & imgPt);
+	void ImgPt2spacePt(Point & imgPt, geometry_msgs::Point32 & spacePt);
+
+	void process_moving_image(Mat &img_moving, Mat &img_t);
 
 	tf::MessageFilter<sensor_msgs::LaserScan>				*tf_filter_;
 	void scanCallback (const sensor_msgs::LaserScan::ConstPtr& scan_in);
 
-	void DP_segment (sensor_msgs::PointCloud scan_pointcloud, geometry_msgs::PoseStamped laser_origin);
-
 	ros::Publisher 											vertical_laser_pub_;
+	ros::Publisher                              			approx_polygon_pub_;
+	ros::Publisher 											accumulate_laser_pub_;
 
 	size_t 													interval_;
-	double 													shrink_range_;
-
-	vector<sensor_msgs::LaserScan>                         	scan_vector_;
-	vector<sensor_msgs::PointCloud>                        	raw_cloud_vector_;
-	vector<sensor_msgs::PointCloud>                        	shrink_cloud_vector_;
-
+	vector<sensor_msgs::PointCloud>                        	cloud_vector_;
 	vector<geometry_msgs::PoseStamped>						laser_pose_vector_;
+	geometry_msgs::PoseStamped								laser_pose_current_;
 
-	sensor_msgs::LaserScan                                  laser_scan_;
-	sensor_msgs::PointCloud                                 current_laser_cloud_;
-	geometry_msgs::PoseStamped								current_laser_pose_;
-
-	ros::Publisher                              			moving_cloud_pub_;
-	ros::Publisher                              			process_laser_pub_;
-
-	ros::Publisher                              			polygon_t0_pub_;
-	ros::Publisher                              			polygon_t2_pub_;
-
-	ros::Publisher                              			approx_polygon_pub_;
+	double													img_side_length_, img_resolution_;
+	Mat														accumulated_image_;
+	Point													LIDAR_pixel_coord_;
 };
 
 laser_diff::laser_diff()
@@ -97,17 +92,8 @@ laser_diff::laser_diff()
 	private_nh_.param("odom_frame_id",      odom_frame_id_,     std::string("odom"));
 
 	int inverval_tmp;
-	private_nh_.param("interval",		    inverval_tmp,       	10);
+	private_nh_.param("interval",		    inverval_tmp,       	50);
 	interval_ = (size_t) inverval_tmp;
-
-	//corresponding to minimum/maximum speed;
-	private_nh_.param("shrink_range",		shrink_range_,      0.2);
-
-	moving_cloud_pub_   =   nh_.advertise<sensor_msgs::PointCloud>("moving_cloud", 2);
-	process_laser_pub_  =   nh_.advertise<sensor_msgs::PointCloud>("laser_under_process", 2);
-
-	polygon_t0_pub_		=   nh_.advertise<geometry_msgs::PolygonStamped>("polygon_t0", 2);
-	polygon_t2_pub_		=   nh_.advertise<geometry_msgs::PolygonStamped>("polygon_t2", 2);
 
 	approx_polygon_pub_		=   nh_.advertise<geometry_msgs::PolygonStamped>("approx_polygon", 2);
 
@@ -123,67 +109,34 @@ laser_diff::laser_diff()
 	tf_filter_ = new tf::MessageFilter<sensor_msgs::LaserScan>(*laser_sub1_, tf_, odom_frame_id_, 10);
 	tf_filter_->registerCallback(boost::bind(&laser_diff::scanCallback, this, _1));
 	tf_filter_->setTolerance(ros::Duration(0.05));
+
+
+	private_nh_.param("img_side_length",      img_side_length_,     50.0);
+	private_nh_.param("img_resolution",       img_resolution_,     0.2);
+	accumulated_image_		= Mat((int)(2*img_side_length_/img_resolution_), (int)(2*img_side_length_/img_resolution_), CV_8UC1);
+	LIDAR_pixel_coord_.x 	= (int)(img_side_length_/img_resolution_)-1;
+	LIDAR_pixel_coord_.y 	= (int)(img_side_length_/img_resolution_)-1;
 }
 
 
 void laser_diff::scanProcess (const sensor_msgs::LaserScan::ConstPtr& scan_in0, const sensor_msgs::LaserScan::ConstPtr& scan_in1,  const sensor_msgs::LaserScan::ConstPtr& scan_in2,  const sensor_msgs::LaserScan::ConstPtr& scan_in3)
 {
-	//1st step: extract vertical laser parts;
-	sensor_msgs::LaserScan vertical_laser;
-	vertical_laser = *scan_in1;
-	float thetha = 0.8*M_PI/180.0;
-	for(size_t i=0; i<vertical_laser.ranges.size(); i++)
-	{
-		float scan_range0 = scan_in0->ranges[i];
-		float scan_range1 = scan_in1->ranges[i];
-		float scan_range2 = scan_in2->ranges[i];
-		float scan_range3 = scan_in3->ranges[i];
+	sensor_msgs::LaserScan verti_laser1, verti_laser2;
+	vertical_laser_pair(scan_in1, scan_in0, verti_laser1);
+	vertical_laser_pair(scan_in2, scan_in3, verti_laser2);
+	sensor_msgs::PointCloud verti_cloud1, verti_cloud2, verti_cloud;
 
-		float longer_range, shorter_range;
-		if(scan_range0 > scan_in0->range_min)
-		{
-			longer_range = scan_range0 > scan_range1 ? scan_range0:scan_range1;
-			shorter_range = scan_range0 <= scan_range1 ? scan_range0:scan_range1;
-		}
-		else if(scan_range2 > scan_in0->range_min)
-		{
-			longer_range = scan_range2 > scan_range3 ? scan_range2:scan_range3;
-			shorter_range = scan_range2 <= scan_range3 ? scan_range2:scan_range3;
-		}
-
-		if(shorter_range<scan_in0->range_min)vertical_laser.ranges[i] = 0.0;
-		else
-		{
-			float angle = atan2(longer_range*thetha, longer_range-shorter_range)+thetha/2.0;
-			if(angle<M_PI/18.0) vertical_laser.ranges[i] = 0.0;
-			else
-			{
-				if(scan_range2 > scan_in0->range_min) vertical_laser.ranges[i] = scan_in2->ranges[i];
-			}
-		}
-	}
-	vertical_laser_pub_.publish(vertical_laser);
-
-
-
-	try{projector_.transformLaserScanToPointCloud(odom_frame_id_, *scan_in1, current_laser_cloud_, tf_);}
+	try{projector_.transformLaserScanToPointCloud(odom_frame_id_, verti_laser1, verti_cloud1, tf_);}
+	catch (tf::TransformException& e){ROS_DEBUG("Wrong!!!!!!!!!!!!!"); std::cout << e.what();return;}
+	try{projector_.transformLaserScanToPointCloud(odom_frame_id_, verti_laser2, verti_cloud2, tf_);}
 	catch (tf::TransformException& e){ROS_DEBUG("Wrong!!!!!!!!!!!!!"); std::cout << e.what();return;}
 
-	//laser_scan_ = *scan_in1;
-	laser_scan_ = vertical_laser;
-
-	sensor_msgs::LaserScan shrink_scan = laser_scan_;
-	sensor_msgs::PointCloud current_shrink_pointcloud;
-	for(size_t i=0; i<shrink_scan.ranges.size();i++)
-	{
-		shrink_scan.ranges[i] = (shrink_scan.ranges[i]-shrink_range_)>0.0 ? (shrink_scan.ranges[i]-shrink_range_) : 0.0;
-	}
-
-	try{projector_.transformLaserScanToPointCloud(odom_frame_id_, shrink_scan, current_shrink_pointcloud, tf_);}
-	catch (tf::TransformException& e){ROS_DEBUG("Wrong!!!!!!!!!!!!!"); std::cout << e.what();return;}
+	verti_cloud = verti_cloud1;
+	for(size_t i=0; i<verti_cloud2.points.size(); i++)verti_cloud.points.push_back(verti_cloud2.points[i]);
+	cloud_vector_.push_back(verti_cloud);
 
 	geometry_msgs::PoseStamped ident;
-	ident.header = laser_scan_.header;
+	ident.header = scan_in0->header;
 	ident.pose.position.x=0;
 	ident.pose.position.y=0;
 	ident.pose.position.z=0;
@@ -193,35 +146,141 @@ void laser_diff::scanProcess (const sensor_msgs::LaserScan::ConstPtr& scan_in0, 
 	ident.pose.orientation.w=0;
 	try
 	{
-		this->tf_.transformPose(odom_frame_id_, ident, current_laser_pose_);
+		this->tf_.transformPose(odom_frame_id_, ident, laser_pose_current_);
 	}
 	catch(tf::TransformException e)
 	{
 		ROS_WARN("Failed to compute odom pose, skipping scan (%s)", e.what());
 		return;
 	}
+	laser_pose_vector_.push_back(ident);
 
-	DP_segment(current_laser_cloud_, current_laser_pose_);
+	DP_segment(verti_cloud, laser_pose_current_);
 
-	scan_vector_.push_back(laser_scan_);
-	laser_pose_vector_.push_back(current_laser_pose_);
-	raw_cloud_vector_.push_back(current_laser_cloud_);
-	shrink_cloud_vector_.push_back(current_shrink_pointcloud);
-	//ROS_INFO("%ld, %ld",raw_cloud_vector_.size(), laser_pose_vector_.size());
+	assert(cloud_vector_.size() == laser_pose_vector_.size());
 
-	assert(scan_vector_.size() == laser_pose_vector_.size());
-	assert(raw_cloud_vector_.size() == laser_pose_vector_.size());
-	assert(shrink_cloud_vector_.size() == laser_pose_vector_.size());
-
-	if(scan_vector_.size()==interval_*2+1)
+	if(cloud_vector_.size()==interval_*2)
 	{
 		extract_moving_points();
+		cloud_vector_.erase(cloud_vector_.begin());
 		laser_pose_vector_.erase(laser_pose_vector_.begin());
-		scan_vector_.erase(scan_vector_.begin());
-		raw_cloud_vector_.erase(raw_cloud_vector_.begin());
-		shrink_cloud_vector_.erase(shrink_cloud_vector_.begin());
 	}
 }
+
+void laser_diff::extract_moving_points()
+{
+	accumulated_image_ = Scalar(0);
+	Mat current_image = accumulated_image_.clone();
+	Mat image_t = accumulated_image_.clone();
+	for(size_t i=0; i<interval_; i++)
+	{
+		for(size_t j=0; j<cloud_vector_[i].points.size(); j++)
+		{
+			geometry_msgs::Point32 spacePt_tmp = cloud_vector_[i].points[j];
+			Point imgpt_tmp;
+			spacePt2ImgP(spacePt_tmp, imgpt_tmp);
+			if(imgpt_tmp.y>= 0 && imgpt_tmp.y <accumulated_image_.rows && imgpt_tmp.x>= 0 && imgpt_tmp.x <accumulated_image_.cols)
+			{
+				if(accumulated_image_.at<uchar>(imgpt_tmp.y,imgpt_tmp.x) <255) accumulated_image_.at<uchar>(imgpt_tmp.y,imgpt_tmp.x) = accumulated_image_.at<uchar>(imgpt_tmp.y,imgpt_tmp.x)+1;
+
+			}
+		}
+	}
+
+	for(size_t i=interval_; i<2*interval_; i++)
+	{
+		for(size_t j=0; j<cloud_vector_[i].points.size(); j++)
+		{
+			geometry_msgs::Point32 spacePt_tmp = cloud_vector_[i].points[j];
+			Point imgpt_tmp;
+			spacePt2ImgP(spacePt_tmp, imgpt_tmp);
+			if(imgpt_tmp.y>= 0 && imgpt_tmp.y <accumulated_image_.rows && imgpt_tmp.x>= 0 && imgpt_tmp.x <accumulated_image_.cols)
+			{
+				if(current_image.at<uchar>(imgpt_tmp.y,imgpt_tmp.x) <255) current_image.at<uchar>(imgpt_tmp.y,imgpt_tmp.x) = current_image.at<uchar>(imgpt_tmp.y,imgpt_tmp.x)+1;
+				if(i==2*interval_-1)
+				{
+					if(image_t.at<uchar>(imgpt_tmp.y,imgpt_tmp.x) <255) image_t.at<uchar>(imgpt_tmp.y,imgpt_tmp.x) = image_t.at<uchar>(imgpt_tmp.y,imgpt_tmp.x)+1;
+				}
+			}
+		}
+	}
+
+
+	threshold( current_image, current_image, 0, 255, 0 );
+	threshold( image_t, image_t, 0, 255, 0 );
+	imshow("current_image", current_image);
+	imshow("image_t", image_t);
+	waitKey(1);
+
+	threshold(accumulated_image_, accumulated_image_, 0, 255, 0);
+
+
+	int dilation_size = 1;
+	Mat element = getStructuringElement( MORPH_RECT, Size( 2*dilation_size + 1, 2*dilation_size+1 ), Point( dilation_size, dilation_size ) );
+	dilate(accumulated_image_, accumulated_image_, element);
+	threshold(accumulated_image_, accumulated_image_, 0, 255, 1);
+	imshow("accumulated_image_", accumulated_image_);
+	waitKey(1);
+
+	/*
+	 * try opencv API for scan matching, it helps, but performance not robust;
+	 * 1) it turns out not robust, the estimation is not consistent but jumps a lot;
+	 * 2) it will try to match moving points when other surrounding features not enough, since no odometry information used;
+	 */
+
+	/*
+	ROS_INFO("1");
+	Mat affine_tranform = estimateRigidTransform(accumulated_image_, current_image, false);
+	warpAffine(accumulated_image_, accumulated_image_, affine_tranform, accumulated_image_.size());
+	cout << affine_tranform << endl;
+	ROS_INFO("2");
+	*/
+
+	bitwise_and(accumulated_image_, current_image, current_image);
+	morphologyEx(current_image, current_image, MORPH_CLOSE, element);
+
+	imshow("moving_objects", current_image);
+	waitKey(1);
+
+	process_moving_image(current_image, image_t);
+
+}
+
+void laser_diff::process_moving_image(Mat &img_moving, Mat &img_t)
+{
+	Mat moving_tmp = img_moving.clone();
+	vector<vector<Point> > contours;
+	vector<Vec4i> hierarchy;
+	findContours( moving_tmp, contours, hierarchy, CV_RETR_EXTERNAL, CV_CHAIN_APPROX_SIMPLE, Point(0,0) );
+
+	vector<Moments> mu(contours.size() );
+	for( int i = 0; i < contours.size(); i++ ) {mu[i] = moments( contours[i], false );}
+	vector<double> area( contours.size() );
+	for( int i = 0; i < contours.size(); i++ ) {area[i] = contourArea(contours[i]);}
+	vector<double> length( contours.size() );
+	for( int i = 0; i < contours.size(); i++ ) {length[i] = arcLength(contours[i], true);}
+
+	Mat moving_tmp2 = img_moving.clone();
+	int dilation_size = 2;
+	Mat element = getStructuringElement( MORPH_RECT, Size( 2*dilation_size + 1, 2*dilation_size+1 ), Point( dilation_size, dilation_size ) );
+	morphologyEx(moving_tmp2, moving_tmp2, CV_MOP_OPEN, element);
+	imshow("erode", moving_tmp2);
+	waitKey(1);
+}
+
+
+
+void laser_diff::spacePt2ImgP(geometry_msgs::Point32 & spacePt, Point & imgPt)
+{
+	imgPt.x = int((spacePt.x - laser_pose_current_.pose.position.x)/img_resolution_) + LIDAR_pixel_coord_.x;
+	imgPt.y =  accumulated_image_.rows -( int((spacePt.y - laser_pose_current_.pose.position.y)/img_resolution_) + LIDAR_pixel_coord_.y);
+}
+void laser_diff::ImgPt2spacePt(Point & imgPt, geometry_msgs::Point32 & spacePt)
+{
+	spacePt.x = (imgPt.x-LIDAR_pixel_coord_.x)*img_resolution_+laser_pose_current_.pose.position.x;
+	spacePt.y = (accumulated_image_.rows - imgPt.y - LIDAR_pixel_coord_.y)*img_resolution_+laser_pose_current_.pose.position.y;
+}
+
 
 void laser_diff::DP_segment (sensor_msgs::PointCloud scan_pointcloud, geometry_msgs::PoseStamped laser_origin)
 {
@@ -255,54 +314,25 @@ void laser_diff::DP_segment (sensor_msgs::PointCloud scan_pointcloud, geometry_m
 	approx_polygon_pub_.publish(approxy_polygon);
 }
 
-void laser_diff::extract_moving_points ()
+void laser_diff::vertical_laser_pair(const sensor_msgs::LaserScan::ConstPtr& scan_in_a, const sensor_msgs::LaserScan::ConstPtr& scan_in_b, sensor_msgs::LaserScan & vertical_scan)
 {
-	ROS_INFO("extracting moving points!");
-	sensor_msgs::PointCloud  	cloud_t1 		= 	raw_cloud_vector_[interval_];
-
-	sensor_msgs::PointCloud  shrink_cloud_t0 	= 	shrink_cloud_vector_.front();
-	sensor_msgs::PointCloud  shrink_cloud_t2 	= 	shrink_cloud_vector_.back();
-	geometry_msgs::PoseStamped 	laser_pose_t0	=	laser_pose_vector_.front();
-	geometry_msgs::PoseStamped 	laser_pose_t2	=	laser_pose_vector_.back();
-
-	geometry_msgs::Point32 	 origin_t0;
-	origin_t0.x = laser_pose_t0.pose.position.x;
-	origin_t0.y = laser_pose_t0.pose.position.y;
-	origin_t0.z = laser_pose_t0.pose.position.z;
-	geometry_msgs::PolygonStamped	poly_shrink_t0;
-	poly_shrink_t0.header 	= current_laser_cloud_.header;
-	poly_shrink_t0.polygon.points	= 	shrink_cloud_t0.points;
-	poly_shrink_t0.polygon.points.push_back(origin_t0);
-
-	geometry_msgs::Point32 	 origin_t2;
-	origin_t2.x = laser_pose_t2.pose.position.x;
-	origin_t2.y = laser_pose_t2.pose.position.y;
-	origin_t2.z = laser_pose_t2.pose.position.z;
-	geometry_msgs::PolygonStamped	poly_shrink_t2;
-	poly_shrink_t2.header 	= current_laser_cloud_.header;
-	poly_shrink_t2.polygon.points	= 	shrink_cloud_t2.points;
-	poly_shrink_t2.polygon.points.push_back(origin_t2);
-
-	sensor_msgs::PointCloud moving_cloud_t1;
-	moving_cloud_t1.header = current_laser_cloud_.header;
-
-	for(size_t i=0; i<cloud_t1.points.size(); i++)
+	vertical_scan = *scan_in_a;
+	float thetha = 0.8*M_PI/180.0;
+	for(size_t i=0; i<vertical_scan.ranges.size(); i++)
 	{
-		geometry_msgs::Point32 point_tmp = cloud_t1.points[i];
-		bool moving_towards = pointInPolygon(point_tmp, poly_shrink_t0.polygon.points);
-		bool moving_outwards = pointInPolygon(point_tmp, poly_shrink_t2.polygon.points);
-		moving_outwards = false;
-		if(moving_towards||moving_outwards)
+		float scan_range0 = scan_in_a->ranges[i];
+		float scan_range1 = scan_in_b->ranges[i];
+		float longer_range, shorter_range;
+		longer_range = scan_range0 > scan_range1 ? scan_range0:scan_range1;
+		shorter_range = scan_range0 <= scan_range1 ? scan_range0:scan_range1;
+
+		if(shorter_range<scan_in_a->range_min)vertical_scan.ranges[i] = 0.0;
+		else
 		{
-			moving_cloud_t1.points.push_back(point_tmp);
+			float angle = atan2(longer_range*thetha, longer_range-shorter_range)+thetha/2.0;
+			if(angle<M_PI/4.0) vertical_scan.ranges[i] = 0.0;
 		}
 	}
-
-	cloud_t1.header = current_laser_cloud_.header;
-	moving_cloud_pub_.publish(moving_cloud_t1);
-	process_laser_pub_.publish(cloud_t1);
-	polygon_t0_pub_.publish(poly_shrink_t0);
-	polygon_t2_pub_.publish(poly_shrink_t2);
 }
 
 //http://alienryderflex.com/polygon/
