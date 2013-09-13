2b34b67bf69397716e08feadab3e5ec9f8198f0f
baoxing <marskahn@gmail.com>
2013-09-13 21:02:22 +0800

    activity_semantics;

--- smart-ros-pkg/lowlevel/dead_reckoning/src/odom_republish.py
+++ smart-ros-pkg/lowlevel/dead_reckoning/src/odom_republish.py
@@ -14,51 +14,32 @@ This is useful to select which of multiple odometry sources becomes the main
 odometry ('odom') for the navigation stack and the rest of the high level code.
 '''
 
-class OdomRepublish:
 
-    def __init__(self):
-        self.initialized = False
-        self.pre_x = 0.0
-        self.pre_y = 0.0
-        self.pre_z = 0.0
-        self.odo_sub = rospy.Subscriber('input_topic', Odometry, self.odoCallBack)
-        self.odo_pub = rospy.Publisher('odom', Odometry)
-        self.odomBroadcaster = TransformBroadcaster()
+rospy.init_node('odom_republish')
 
-        self.diag_updater = DIAG.Updater()
-        self.diag_updater.setHardwareID('none')
-        diag_param_fs = DIAG.FrequencyStatusParam({'min': 5}, 0.1, 5)
-        self.diag_task_fs = DIAG.FrequencyStatus(diag_param_fs)
-        self.diag_updater.add(self.diag_task_fs)
+odo_pub = rospy.Publisher('odom', Odometry)
+odomBroadcaster = TransformBroadcaster()
 
-    def odoCallBack(self, msg):
-        msg.header.frame_id = 'odom'
-        
-        self.diag_task_fs.tick()
-        self.diag_updater.update()
+diag_updater = DIAG.Updater()
+diag_updater.setHardwareID('none')
+diag_param_fs = DIAG.FrequencyStatusParam({'min': 5}, 0.1, 5)
+diag_task_fs = DIAG.FrequencyStatus(diag_param_fs)
+diag_updater.add(diag_task_fs)
 
-        if self.initialized == False:
-          self.pre_x = msg.pose.pose.position.x
-          self.pre_y = msg.pose.pose.position.y
-          self.pre_z = msg.pose.pose.position.z
-          self.initialized = True
-        p = msg.pose.pose.position
-        p.x = p.x - self.pre_x
-        p.y = p.y - self.pre_y
-        p.z = p.z - self.pre_z
-        o = msg.pose.pose.orientation
-        self.odomBroadcaster.sendTransform(
-            (p.x, p.y, p.z),
-            (o.x, o.y, o.z, o.w),
-            msg.header.stamp, 'base_link', 'odom'
-        )
-        msg.pose.pose.position = p
-        self.odo_pub.publish(msg)
+def odoCallBack(msg):
+    msg.header.frame_id = 'odom'
+    odo_pub.publish(msg)
+    diag_task_fs.tick()
+    diag_updater.update()
 
-if __name__=='__main__':
-      rospy.init_node('odom_republish')
-      node = OdomRepublish()
-     
+    p = msg.pose.pose.position
+    o = msg.pose.pose.orientation
+    odomBroadcaster.sendTransform(
+        (p.x, p.y, p.z),
+        (o.x, o.y, o.z, o.w),
+        msg.header.stamp, 'base_link', 'odom'
+    )
 
-      rospy.spin()
 
+odo_sub = rospy.Subscriber('input_topic', Odometry, odoCallBack)
+rospy.spin()
