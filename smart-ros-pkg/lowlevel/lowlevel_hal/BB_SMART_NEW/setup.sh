a4ff8108cdc69ad5a8875131dfbc54f3657bf36b
Demian <demian@outlook.com>
2013-10-02 14:55:52 +0000

    Added BB_SMART_NEW. A HAL settings that works with the BigBad Cape.

---
+++ smart-ros-pkg/lowlevel/lowlevel_hal/BB_SMART_NEW/setup.sh
@@ -0,0 +1,70 @@
+#!/bin/bash
+
+dtbo_err () {
+	echo "Error loading device tree overlay file: $DTBO" >&2
+	exit 1
+}
+
+pin_err () {
+	echo "Error exporting pin:$PIN" >&2
+	exit 1
+}
+
+dir_err () {
+	echo "Error setting direction:$DIR on pin:$PIN" >&2
+	exit 1
+}
+
+SLOTS=/sys/devices/bone_capemgr.*/slots
+
+# Make sure required device tree overlay(s) are loaded
+for DTBO in BB-SMART-FM2 cape-bone-iio bone_eqep0 bone_eqep1 bone_eqep2 ; do
+
+	if grep -q $DTBO $SLOTS ; then
+		echo $DTBO overlay found
+	else
+		echo Loading $DTBO overlay
+		sudo -A su -c "echo $DTBO > $SLOTS" || dtbo_err
+		sleep 1
+	fi
+done;
+
+sudo chown -R linuxcnc:linuxcnc /sys/devices/ocp.*/*epwmss/
+
+if [ ! -r /sys/devices/ocp.*/helper.*/AIN0 ] ; then
+	echo Analog input files not found in /sys/devices/ocp.*/helper.* >&2
+	exit 1;
+fi
+
+if [ ! -r /sys/class/uio/uio0 ] ; then
+	echo PRU control files not found in /sys/class/uio/uio0 >&2
+	exit 1;
+fi
+
+# Export GPIO pins
+# This really only needs to be done to enable the low-level clocks for the GPIO
+# modules.  There is probably a better way to do this...
+while read PIN DIR JUNK ; do
+        case "$PIN" in
+        ""|\#*)	
+		continue ;;
+        *)
+		[ -r /sys/class/gpio/gpio$PIN ] && continue
+                sudo -A su -c "echo $PIN > /sys/class/gpio/export" || pin_err
+		sudo -A su -c "echo $DIR > /sys/class/gpio/gpio$PIN/direction" || dir_err
+                ;;
+        esac
+
+done <<- EOF
+	38	out
+	39	out
+	34	out
+	35	out
+	66	out
+	67	out
+	69	out
+	68	out
+	45	out
+	44	out
+	23	out
+EOF
