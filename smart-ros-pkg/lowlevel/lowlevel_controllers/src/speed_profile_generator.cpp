3eb0f32dcf799ef797a654b1b9a83e0bcb2d3596
Demian <demian@outlook.com>
2013-10-03 22:43:00 +0800

    Now works fine with speed target changed at a=0, j=0, v=v_target

--- smart-ros-pkg/lowlevel/lowlevel_controllers/src/speed_profile_generator.cpp
+++ smart-ros-pkg/lowlevel/lowlevel_controllers/src/speed_profile_generator.cpp
@@ -11,30 +11,57 @@
 double max_jerk_, max_acc_, max_speed_;
 double target_speed_;
 double speed_now_, acc_now_;
-double v0_;
-double change_interval_time_;
+double v0_, v1_, v2_;
+double intermediate_speed_;
 double start_time_;
 ros::Publisher speed_pub;
 int speed_interval_;
+int acceleration_sign_;
+bool target_active_;
 bool fast_target_;
-
 using namespace std;
-
-void checkForNewTarget(double new_target){
-  fast_target_ = (fabs(new_target - speed_now_) < (max_acc_*max_acc_/max_jerk_));
-  double time_now = ros::Time::now().toSec();
-  if(fast_target_) change_interval_time_ = time_now + sqrt(fabs(max_speed_-v0_)/max_jerk_);
-  else change_interval_time_ = time_now + max_acc_/max_jerk_;
-  
-  if(new_target>target_speed_) speed_interval_ = 1;
-  else if(new_target < target_speed_) speed_interval_ = 5;
-  cout<<"New target: "<<speed_interval_<<" "<<change_interval_time_-start_time_<<endl;
-    
-}
-
+int counter_;
 void targetSpeedCallback(std_msgs::Float32 target_speed){
-  checkForNewTarget(target_speed.data);
-  target_speed_ = target_speed.data;
+  double new_target = target_speed.data;
+  target_active_ = true;
+  
+  if(target_speed_ == new_target) target_active_ = false;
+  target_speed_ = new_target;
+  
+  
+  if(target_active_){
+    if(new_target > speed_now_) acceleration_sign_ = 1;
+    else if(new_target < speed_now_) acceleration_sign_ = -1;
+    double j = acceleration_sign_ * max_jerk_;
+    double a = acceleration_sign_ * max_acc_;
+    double a_max_sq = max_acc_ * max_acc_;
+
+    cout<<"Target changed: amount="<<target_speed_-v0_<<" j="<<j<<" speed_now="<<speed_now_<<" acc_now="<<acc_now_;
+    switch(speed_interval_){
+      case 1:
+	v0_ = speed_now_ - acc_now_*acc_now_/(2*max_jerk_);
+	break;
+      case 2:
+	v0_ = speed_now_ - a_max_sq/(2*max_jerk_);
+	break;
+    }
+    if( fabs(target_speed_-v0_)>= a_max_sq/max_jerk_){
+      cout<<" Normal target"<<endl;
+      fast_target_ = false;
+      v1_ = v0_+ a_max_sq/(2*j);
+      v2_ = target_speed_ -(a_max_sq/(2*j));
+      cout <<" v0: "<<v0_<<" vt: "<<target_speed_<<" v1: "<<v1_<<" v2: "<<v2_<<endl;
+    }
+    else {
+      cout<<" Quick target"<<endl;
+      fast_target_ = true;
+      v1_ = (target_speed_ +v0_)/2;
+      cout <<" v0: "<<v0_<<" vt: "<<target_speed_<<" v1: "<<v1_<<endl;
+    }
+    v0_ = target_speed_;
+    speed_interval_ = 1;
+    target_active_ = false;
+  }
 }
 
 double newSpeedDynamicModel(double j, double dt){
@@ -46,84 +73,79 @@ double newSpeedDynamicModel(double j, double dt){
 void callback(const ros::TimerEvent & event)
 {
     double dt = (event.current_real - event.last_real).toSec();
-    double time_now = ros::Time::now().toSec();
-    bool time_up = ((time_now - change_interval_time_) > 0);
-    double j;
-    
+    double j = acceleration_sign_ * max_jerk_;
+    double a = acceleration_sign_ * max_acc_;
+    double a_max_sq = max_acc_ * max_acc_;
+    bool change_state = false;
     //Ref: http://ieeexplore.ieee.org/xpl/login.jsp?tp=&arnumber=1026947&url=http%3A%2F%2Fieeexplore.ieee.org%2Fiel5%2F7974%2F22065%2F01026947.pdf%3Farnumber%3D1026947
+    
+    
+    
     switch (speed_interval_){
       case 1:
-	j = max_jerk_;
-	if(time_up) {
-	  if(fast_target_) {
-	    j = -max_jerk_;
-	    speed_interval_=3;
-	    change_interval_time_ +=sqrt((max_speed_-v0_)/max_jerk_);
+	j = j;
+	if(acceleration_sign_>0){
+	  if(speed_now_>v1_) change_state = true;
+	}
+	else{
+	  if(speed_now_<v1_) change_state = true;
+	}
+	if(change_state){
+	  if(fast_target_){
+	    j = -j;
+	    speed_interval_ = 3;
 	  }
 	  else {
 	    j = 0;
-	    speed_interval_++;
-	    change_interval_time_ += (target_speed_ - 2 * v0_)/max_acc_ - max_acc_/max_jerk_;
+	    speed_interval_ = 2;
 	  }
 	}
 	break;
       case 2:
 	j = 0;
-	if(time_up){
-	  j = -max_jerk_;
-	  speed_interval_++;
-	  change_interval_time_ +=  max_acc_/max_jerk_;
+	if(acceleration_sign_>0){
+	  if(speed_now_>v2_) change_state = true;
+	}
+	else{
+	  if(speed_now_<v2_) change_state = true;
+	}
+	if(change_state){
+	  j = -j;
+	  acc_now_ = a;
+	  speed_interval_ = 3;
 	}
 	break;
       case 3:
-	j = -max_jerk_;
-	if(time_up) {
+	j = -j;
+	if(acceleration_sign_>0){
+	  if(acc_now_<=0 || speed_now_>target_speed_) change_state = true;
+	}
+	else{
+	  if(acc_now_>=0 || speed_now_<target_speed_) change_state = true;
+	}
+	if(change_state){
+	  speed_interval_ = 4;
+	  acc_now_ = 0;
 	  j = 0;
-	  speed_interval_++;
+	  speed_now_ = target_speed_;
 	}
 	break;
       case 4:
-	v0_ = target_speed_;
 	speed_now_ = target_speed_;
-	break;
-      case 5:
-	j = -max_jerk_;
-	if(time_up){
-	  if(fast_target_){
-	    j = -max_jerk_;
-	    speed_interval_=7;
-	    change_interval_time_ += sqrt((v0_-max_speed_)/max_jerk_);
-	  }
-	  else {
-	    j = 0;
-	    speed_interval_++;
-	    change_interval_time_ += ( 2 * v0_ - target_speed_)/max_acc_ - max_acc_/max_jerk_;
-	  }
-	}
-	break;
-      case 6:
 	j = 0;
-	if(time_up){
-	  j = -max_jerk_;
-	  speed_interval_++;
-	  change_interval_time_ += max_acc_/max_jerk_;
-	}
-	break;
-      case 7:
-	j = -max_jerk_;
-	if(time_up) {
-	  j = 0;
-	  speed_interval_ = 4;
-	}
+	acc_now_ = 0;
 	break;
     }
     
-    if(speed_interval_!=4)
-      speed_now_ = newSpeedDynamicModel(j, dt);
-    cout<<speed_interval_<<" "<<j<<" "<<change_interval_time_-start_time_<<" "<<time_now-start_time_<<" "<<acc_now_<<endl;
-    geometry_msgs::Twist tw;
-    tw.linear.x = speed_now_;
-    speed_pub.publish(tw);
+    speed_now_ = newSpeedDynamicModel(j, dt);
+    //cout<<"State: "<<speed_interval_<<", v1: "<<v1_<<", v2: "<<v2_<<" acc_now: "<<acc_now_<<" speed_now: "<<speed_now_<<endl;
+    if(counter_++ ==10){
+      geometry_msgs::Twist tw;
+      tw.linear.x = speed_now_;
+      speed_pub.publish(tw);
+      counter_ = 1;
+    }
+    
 }
 
 int main(int argc, char **argv)
@@ -132,16 +154,17 @@ int main(int argc, char **argv)
     target_speed_ = 0;
     speed_interval_ = 4;
     speed_now_ = acc_now_ = 0;
-    fast_target_ = 0;
+    acceleration_sign_ = 1;
+    target_active_ = false;
+    fast_target_ = false;
     double frequency;
     ros::NodeHandle n;
     ros::NodeHandle private_nh("~");
-    change_interval_time_ = start_time_ = ros::Time::now().toSec();
     speed_pub = n.advertise<geometry_msgs::Twist>("cmd_vel", 1);
     private_nh.param("max_jerk", max_jerk_, 1.0);
     private_nh.param("max_acc", max_acc_, 0.5);
     private_nh.param("max_speed", max_speed_, 1.5);
-    private_nh.param("frequency", frequency, 100.0);
+    private_nh.param("frequency", frequency, 1000.0);
     ros::Timer timer = n.createTimer(ros::Duration(1/frequency), callback);
     ros::Subscriber target_sub = n.subscribe("target_speed", 1, targetSpeedCallback);
     
