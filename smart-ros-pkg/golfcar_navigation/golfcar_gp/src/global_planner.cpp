b5a4848733adbe0e8a3f433f3ff5f08d9de00408
Demian <demian@outlook.com>
2013-10-15 18:42:30 +0800

    Updated global_planner that can accept new external path

--- smart-ros-pkg/golfcar_navigation/golfcar_gp/src/global_planner.cpp
+++ smart-ros-pkg/golfcar_navigation/golfcar_gp/src/global_planner.cpp
@@ -23,8 +23,20 @@
 #include <pnc_msgs/poi.h>
 
 using namespace std;
-
-
+int start_number, end_number;
+ros::Publisher *goal_pub_ptr_;
+nav_msgs::Path rrt_path_;
+void repubCallback(nav_msgs::Path p){
+  cout<<"New path received from rrt, passing it to local planner"<<endl;
+  geometry_msgs::PoseStamped goal;
+  goal.header.stamp = ros::Time::now();
+  goal.header.frame_id = "/map";
+  goal.pose.position.x = (double) start_number;
+  goal.pose.position.y = (double) end_number;
+  goal.pose.orientation.z = 1.0;
+  goal_pub_ptr_->publish(goal);
+  rrt_path_ = p;
+}
 
 
 namespace golfcar_gp
@@ -42,19 +54,20 @@ public:
     ros::Publisher g_plan_pub_;
     ros::Publisher slowZone_pub_;
     ros::Publisher poi_pub_;
-    ros::Subscriber stage_sub_;
+    
+    ros::Subscriber g_plan_repub_sub_;
 
     StationPaths sp_;
 };
-
-
 
 void GlobalPlan::initialize(std::string name, costmap_2d::Costmap2DROS* costmap_ros){
     ros::NodeHandle global_node;
     g_plan_pub_ = global_node.advertise<nav_msgs::Path>("global_plan", 1, true);
     slowZone_pub_ = global_node.advertise<geometry_msgs::PoseArray>("slowZone", 1, true);
     poi_pub_ = global_node.advertise<pnc_msgs::poi>("poi", 1, true);
-
+    g_plan_repub_sub_ = global_node.subscribe("global_plan_repub", 1, repubCallback);
+    ros::Publisher goal_pub = global_node.advertise<geometry_msgs::PoseStamped>("move_base_simple/goal", 1);
+    goal_pub_ptr_ = &goal_pub;
     ROS_INFO("gplan initialized");
 }
 
@@ -73,66 +86,83 @@ bool GlobalPlan::makePlan(const geometry_msgs::PoseStamped&  start,
                           const geometry_msgs::PoseStamped& goal,
                           std::vector<geometry_msgs::PoseStamped>& plan)
 {
-    //all in according to the pixels from global map
-    geometry_msgs::Pose2D target;
-    StationPath station_path = sp_.getPath(sp_.knownStations()((int)goal.pose.position.x),
-                                           sp_.knownStations()((int)goal.pose.position.y));
-    geometry_msgs::PoseArray slowZones;
-    slowZones.header.frame_id = "/map";
-    slowZones.header.stamp = ros::Time::now();
+    if(rrt_path_.poses.size() == 0){
+      //all in according to the pixels from global map
+      geometry_msgs::Pose2D target;
+      start_number = (int)goal.pose.position.x;
+      end_number = (int)goal.pose.position.y;
+      StationPath station_path = sp_.getPath(sp_.knownStations()(start_number),
+					    sp_.knownStations()(end_number));
+      geometry_msgs::PoseArray slowZones;
+      slowZones.header.frame_id = "/map";
+      slowZones.header.stamp = ros::Time::now();
 
-    for(unsigned int i=0;i<sp_.slowZones_.size();i++)
-    {
-        geometry_msgs::Pose pose;
-        pose.orientation.w = 1.0;
-        pose.position.x = sp_.slowZones_[i].x_;
-        pose.position.y = sp_.slowZones_[i].y_;
-        pose.position.z = sp_.slowZones_[i].r_;
-        slowZones.poses.push_back(pose);
+      for(unsigned int i=0;i<sp_.slowZones_.size();i++)
+      {
+	  geometry_msgs::Pose pose;
+	  pose.orientation.w = 1.0;
+	  pose.position.x = sp_.slowZones_[i].x_;
+	  pose.position.y = sp_.slowZones_[i].y_;
+	  pose.position.z = sp_.slowZones_[i].r_;
+	  slowZones.poses.push_back(pose);
+      }
+
+      slowZone_pub_.publish(slowZones);
+
+      //publish point of interest
+      pnc_msgs::poi poi;
+      add_pts(station_path.leftsig_pts_, &poi.sig_pts, 0);
+      add_pts(station_path.rightsig_pts_, &poi.sig_pts, 1);
+      add_pts(station_path.offsig_pts_, &poi.sig_pts, 2);
+
+      //poi.rightsig_pts.insert(poi.rightsig_pts.begin(),station_path.rightsig_pts_.begin(),station_path.rightsig_pts_.end());
+      //poi.offsig_pts.insert(poi.offsig_pts.begin(),station_path.offsig_pts_.begin(),station_path.offsig_pts_.end());// = station_path.leftsig_pts_;
+      poi.int_pts.insert(poi.int_pts.begin(),station_path.ints_pts_.begin(),station_path.ints_pts_.end());// = station_path.leftsig_pts_;
+
+      poi_pub_.publish(poi);
+
+      vector<geometry_msgs::PoseStamped> final_targets;
+      geometry_msgs::PoseStamped ps;
+
+      nav_msgs::Path p;
+      p.poses.resize(station_path.size());
+      for(unsigned int i=0; i<station_path.size(); i++)
+      {
+	  p.poses[i].header.frame_id = "/map";
+	  p.poses[i].header.stamp = ros::Time::now();
+	  p.poses[i].pose.position.x = station_path[i].x_;
+	  p.poses[i].pose.position.y = station_path[i].y_;
+	  p.poses[i].pose.orientation.w = 1.0;
+	  geometry_msgs::PoseStamped pl;
+	  pl.header.frame_id = "/map";
+	  pl.header.stamp = ros::Time::now();
+	  pl.pose.position.x = station_path[i].x_;
+	  pl.pose.position.y = station_path[i].y_;
+	  pl.pose.orientation.w = 1.0;
+	  plan.push_back(pl);
+      }
+      cout<<"Plan with "<<plan.size()<<" points sent."<<endl;
+      p.header.stamp = ros::Time();
+      p.header.frame_id = "/map";
+
+      //p.poses.push_back(start);
+      //p.poses.push_back(goal);
+      g_plan_pub_.publish(p);
+      //plan.push_back(targets);
     }
-
-    slowZone_pub_.publish(slowZones);
-
-    //publish point of interest
-    pnc_msgs::poi poi;
-    add_pts(station_path.leftsig_pts_, &poi.sig_pts, 0);
-    add_pts(station_path.rightsig_pts_, &poi.sig_pts, 1);
-    add_pts(station_path.offsig_pts_, &poi.sig_pts, 2);
-
-    //poi.rightsig_pts.insert(poi.rightsig_pts.begin(),station_path.rightsig_pts_.begin(),station_path.rightsig_pts_.end());
-    //poi.offsig_pts.insert(poi.offsig_pts.begin(),station_path.offsig_pts_.begin(),station_path.offsig_pts_.end());// = station_path.leftsig_pts_;
-    poi.int_pts.insert(poi.int_pts.begin(),station_path.ints_pts_.begin(),station_path.ints_pts_.end());// = station_path.leftsig_pts_;
-
-    poi_pub_.publish(poi);
-
-    vector<geometry_msgs::PoseStamped> final_targets;
-    geometry_msgs::PoseStamped ps;
-
-    nav_msgs::Path p;
-    p.poses.resize(station_path.size());
-    for(unsigned int i=0; i<station_path.size(); i++)
-    {
-        p.poses[i].header.frame_id = "/map";
-        p.poses[i].header.stamp = ros::Time::now();
-        p.poses[i].pose.position.x = station_path[i].x_;
-        p.poses[i].pose.position.y = station_path[i].y_;
-        p.poses[i].pose.orientation.w = 1.0;
-        geometry_msgs::PoseStamped pl;
-        pl.header.frame_id = "/map";
-        pl.header.stamp = ros::Time::now();
-        pl.pose.position.x = station_path[i].x_;
-        pl.pose.position.y = station_path[i].y_;
-        pl.pose.orientation.w = 1.0;
-        plan.push_back(pl);
+    //send the rrt path to the local planner
+    else {
+      for(size_t i=0; i<rrt_path_.poses.size(); i++){
+	geometry_msgs::PoseStamped ps_temp;
+	ps_temp.header.frame_id = "/map";
+	ps_temp.header.stamp = ros::Time::now();
+	ps_temp.pose.position.x = rrt_path_.poses[i].pose.position.x;
+	ps_temp.pose.position.y = rrt_path_.poses[i].pose.position.y;
+	ps_temp.pose.orientation.w = 1.0;
+	plan.push_back(ps_temp);
+      }
+      cout<<"Plan with rrt "<<plan.size()<<" points sent."<<endl;
     }
-    cout<<"Plan with "<<plan.size()<<" points sent."<<endl;
-    p.header.stamp = ros::Time();
-    p.header.frame_id = "/map";
-
-    //p.poses.push_back(start);
-    //p.poses.push_back(goal);
-    g_plan_pub_.publish(p);
-    //plan.push_back(targets);
     return true;
 }
 
