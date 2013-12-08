c2e279c35ef3f2bc40e724958e694af114ae9ab2
Demian <demian@outlook.com>
2013-12-09 03:17:02 +0800

    Updated Launch_v2. Now parameters is a soft link to facilitate different parameters for different vehicles. Same thing goes to 1Localization.launch where link is used to launch either a normal AMCL or a synthetic LIDAR based AMCL

--- smart-ros-pkg/Launch_v2/update_param.bash
+++ smart-ros-pkg/Launch_v2/update_param.bash
@@ -1,6 +1,6 @@
 #!/bin/bash
 package_path=`rospack find Launch_v2`
 robot_name=`echo $ROBOT`
-rosrun Launch_v2 rename_frame.py $package_path/golfcart_parameters.yaml $package_path/golfcart_parameters_temp.yaml env_ROBOT
-rosparam load $package_path/golfcart_parameters_temp.yaml $robot_name
+rosrun Launch_v2 rename_frame.py $package_path/parameters.yaml $package_path/parameters_temp.yaml env_ROBOT
+rosparam load $package_path/parameters_temp.yaml $robot_name
 roslaunch Launch_v2 $1
