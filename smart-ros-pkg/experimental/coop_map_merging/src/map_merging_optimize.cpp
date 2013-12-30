c398cd7c491cb92ce91c686c79bede11795b3e10
Demian <demian@outlook.com>
2013-12-31 02:35:59 +0800

    Now fully working with ICP and CSM

--- smart-ros-pkg/experimental/coop_map_merging/src/map_merging_optimize.cpp
+++ smart-ros-pkg/experimental/coop_map_merging/src/map_merging_optimize.cpp
@@ -11,6 +11,8 @@
 #include <mrpt/slam.h>
 #include <mrpt/gui.h>
 
+#include "csm_cuda/csm.h"
+
 using namespace mrpt;
 using namespace mrpt::utils;
 using namespace mrpt::slam;
@@ -18,7 +20,7 @@ using namespace mrpt::slam;
 class MapMergingOptimize{
   laser_geometry::LaserProjection projector_;
   ros::Publisher transformed_template_pub_, transformed_matching_pub_;
-  ros::Publisher matched_matching_pub_;
+  ros::Publisher matched_matching_ICP_pub_, matched_matching_CSM_pub_;
   tf::TransformListener *tf_;
 public:
   MapMergingOptimize(){
@@ -38,8 +40,10 @@ public:
 				  ("accumulated_pts_tfed", 1);
     transformed_matching_pub_ = nh.advertise<sensor_msgs::PointCloud2>
 				  ("matching_pts_tfed", 1);
-    matched_matching_pub_ = nh.advertise<sensor_msgs::PointCloud2>
-				  ("matched_matching_pub_", 1);
+    matched_matching_ICP_pub_ = nh.advertise<sensor_msgs::PointCloud2>
+				  ("matched_matching_ICP", 1);
+    matched_matching_CSM_pub_ = nh.advertise<sensor_msgs::PointCloud2>
+				  ("matched_matching_CSM", 1);
     message_filters::Synchronizer<MySyncPolicy>
       sync(MySyncPolicy(10), pose_sub, observation_sub, scan_sub);
     sync.registerCallback(boost::bind(&MapMergingOptimize::callback,
@@ -82,6 +86,29 @@ public:
     pcl_template.header = pose->header;
     
     MICPMatching(pcl_match, pcl_template);
+    CSMMatching(pcl_match, pcl_template);
+  }
+  
+  void CSMMatching(pcl::PointCloud<pcl::PointXYZ> matching_cloud, 
+		    pcl::PointCloud<pcl::PointXYZ> template_cloud){
+    CsmGPU<pcl::PointXYZ> csmGPU(0.1, cv::Point2d(50.0, 75.0), template_cloud, false);
+    poseResult result;
+    result.x = result.y = result.r = 0.0;
+    result = csmGPU.getBestMatch(1.0, 1.0, 2, 5, 5, 45, matching_cloud, result);
+    result = csmGPU.getBestMatch(0.1, 0.1, 0.5, 1, 1, 5, matching_cloud, result);
+    cout<<"Best score 2 found: "<< result.x<<", "<<result.y<<", "<<result.r<<": "<<result.score<<endl;
+    pcl::PointCloud<pcl::PointXYZ> cloud_out;
+    Eigen::Matrix4f transform;
+    double d = result.r/180*M_PI;
+    transform<<cos(d),-sin(d),0,result.x,
+		sin(d),cos(d),0,result.y,
+		0,0,1,0,
+		0,0,0,1;
+    pcl::transformPointCloud(matching_cloud, cloud_out, transform);
+    sensor_msgs::PointCloud2 pc2;
+    pcl::toROSMsg(cloud_out, pc2);
+    matched_matching_CSM_pub_.publish(pc2);
+    
   }
   
   void MICPMatching(pcl::PointCloud<pcl::PointXYZ> matching_cloud, 
@@ -120,8 +147,7 @@ public:
     pcl::transformPointCloud<pcl::PointXYZ>(matching_cloud, matching_cloud, tf);
     sensor_msgs::PointCloud2 pc2;
     pcl::toROSMsg(matching_cloud, pc2);
-    matched_matching_pub_.publish(pc2);
-    
+    matched_matching_ICP_pub_.publish(pc2); 
   }
 };
 
