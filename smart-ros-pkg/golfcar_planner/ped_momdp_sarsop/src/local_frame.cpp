cd17c7c32350858cd2c698ae0799ba6727b6180a
golfcart <fmautonomy@gmail.com>
2013-12-30 21:19:14 +0800

    modified momdp

--- smart-ros-pkg/golfcar_planner/ped_momdp_sarsop/src/local_frame.cpp
+++ smart-ros-pkg/golfcar_planner/ped_momdp_sarsop/src/local_frame.cpp
@@ -5,6 +5,7 @@
 #include <sensing_on_road/pedestrian_laser_batch.h>
 #include <ped_momdp_sarsop/ped_local_frame_vector.h>
 #include <sensing_on_road/pedestrian_vision_batch.h>
+
 using namespace std;
 struct ped_transforms
 {
@@ -23,6 +24,7 @@ private:
     void publishTransform(const ros::TimerEvent& event);
     void pedCallback(sensing_on_road::pedestrian_vision_batchConstPtr ped_batch);
     bool getObjectPose(string& target_frame, tf::Stamped<tf::Pose>& in_pose, tf::Stamped<tf::Pose>& out_pose) const;
+	void PublishLocalFrames();
     vector<ped_transforms> ped_transforms_;
     tf::TransformBroadcaster br_;
     tf::TransformListener tf_;
@@ -40,7 +42,8 @@ local_frame::local_frame()
     local_pub_ = nh.advertise<ped_momdp_sarsop::ped_local_frame_vector>("ped_local_frame_vector", 1);
 
     ros::NodeHandle n("~");
-    n.param("global_frame", global_frame_, string("/odom"));
+    //n.param("global_frame", global_frame_, string("/odom"));
+	n.param("global_frame", global_frame_, string("/golfcart/map"));
     n.param("threshold", threshold_, 3.0);
     n.param("offsetx", offsetx_, 0.0);
     n.param("offsety", offsety_, 3.0);
@@ -55,6 +58,7 @@ void local_frame::pedCallback(sensing_on_road::pedestrian_vision_batchConstPtr p
     for(size_t i=0;i<ped_batch->pd_vector.size();i++)
     {
         //find if the transform is already available
+		/*
         size_t matched_ped;
         bool matched=false;
         for(size_t j=0; j<ped_transforms_.size(); j++)
@@ -66,17 +70,21 @@ void local_frame::pedCallback(sensing_on_road::pedestrian_vision_batchConstPtr p
                 continue;
             }
 
-        }
-        if(matched)
+        }*/
+        //if(matched)
+		if(true)
         {
             //transform found, just update the position
             if(ped_batch->pd_vector[i].confidence > 0.1)
             {
                 ped_momdp_sarsop::ped_local_frame plf;
                 plf.header.stamp = ped_batch->header.stamp;
+				/*
                 stringstream frame_id;
                 frame_id<<"ped_"<<ped_transforms_[matched_ped].label;
                 plf.header.frame_id = frame_id.str();
+				*/
+				plf.header.frame_id="/golfcart/map";
 
                 tf::Stamped<tf::Pose> in_pose, out_pose;
 
@@ -93,7 +101,7 @@ void local_frame::pedCallback(sensing_on_road::pedestrian_vision_batchConstPtr p
 
                 //then update the robot pose with the same frame
                 in_pose.setIdentity();
-                in_pose.frame_id_ = "/golfcart/base_link";
+                in_pose.frame_id_ = "base_link";
                 if(!getObjectPose(plf.header.frame_id, in_pose, out_pose)) continue;
 
                 plf.rob_pose.x = out_pose.getOrigin().getX();
@@ -106,7 +114,6 @@ void local_frame::pedCallback(sensing_on_road::pedestrian_vision_batchConstPtr p
         else
         {
 	    //if(ped_batch->pd_vector[i].confidence > threshold_/100)
-		if(ped_batch->pd_vector[i].confidence > 0.1)
             {
 				ROS_INFO("New pedestrian received, creating new transform");
 				ROS_INFO("%d transformations", ped_transforms_.size());
@@ -114,7 +121,7 @@ void local_frame::pedCallback(sensing_on_road::pedestrian_vision_batchConstPtr p
 				tf::Transform transform;
 				tf::Stamped<tf::Pose> in_pose, out_pose;
 				//use the current base_link pose as the new frame
-				in_pose.frame_id_ = "/golfcart/base_link";
+				in_pose.frame_id_ = "base_link";
 				in_pose.setIdentity();
 				in_pose.setOrigin(btVector3(offsetx_,offsety_,0.0));
 				if(!getObjectPose(global_frame_, in_pose, out_pose)) continue;
@@ -131,7 +138,6 @@ void local_frame::pedCallback(sensing_on_road::pedestrian_vision_batchConstPtr p
     //finally publish all the transformed points
     local_pub_.publish(plf_vector);
 }
-
 bool local_frame::getObjectPose(string& target_frame, tf::Stamped<tf::Pose>& in_pose, tf::Stamped<tf::Pose>& out_pose) const
 {
     out_pose.setIdentity();
@@ -163,9 +169,31 @@ void local_frame::publishTransform(const ros::TimerEvent& event)
         br_.sendTransform(tf::StampedTransform(ped_transforms_[i].transform, ros::Time::now(), global_frame_, frame_id.str()));
     }
 }
+
+/*
+void local_frame::PublishLocalFrames()
+{
+	ped_momdp_sarsop::ped_local_frame_vector plf_vector;
+	Car car=world.GetCarPose();
+	for(int i=0;i<world.NumPedInView();i++)
+	{
+		Pedestrian ped=world.GetPedPose(i);	
+		ped_momdp_sarsop::ped_local_frame plf;
+		plf.ped_id=ped.id;
+		plf.ped_pose.x=ped.w;
+		plf.ped_pose.y=ped.h;
+		plf.rob_pose.x=car.w;
+		plf.rob_pose.y=car.h;
+		plf_vector.push_back(plf);	
+	}
+	local_pub_.publish(plf_vector);
+	world.OneStep();
+	
+}*/
 int main(int argc, char** argv){
     ros::init(argc, argv, "local_frame");
     local_frame *lf = new local_frame();
+	//PublishLocalFrames();   //this is for simulation
     //
 
     return 0;
--- smart-ros-pkg/golfcar_planner/ped_momdp_sarsop/src/pedestrian_momdp_realPed.cpp
+++ smart-ros-pkg/golfcar_planner/ped_momdp_sarsop/src/pedestrian_momdp_realPed.cpp
@@ -6,7 +6,13 @@
  */
 
 #include "pedestrian_momdp_realPed.h"
+#include <time.h>
+#include <geometry_msgs/PoseStamped.h>
+#include <geometry_msgs/PolygonStamped.h>
+#include <geometry_msgs/PoseArray.h>
+#include "world_simulator.h"
 
+//WorldSimulator RealWorld;
 pedestrian_momdp::pedestrian_momdp()
 {
     ROS_INFO("Starting Pedestrian Avoidance ... ");
@@ -33,47 +39,71 @@ pedestrian_momdp::pedestrian_momdp()
 
     move_base_speed_=nh.subscribe("/speed_advisor_cmdvel",1, &pedestrian_momdp::moveSpeedCallback, this);
     //goalPub_ = nh.advertise<geometry_msgs::PoseStamped>("move_base_simple/goal",1);
-
-    momdp = new ped_momdp(model_file, policy_file, simLen, simNum, stationary, frequency, use_sim_time, nh);
-
-    ros::spin();
+/*
+	momdp = new ped_momdp(model_file, policy_file, simLen, simNum, stationary, frequency, use_sim_time, nh);
+	momdp->RealWorldPt=&RealWorld;
+	//momde->world=&world;
+//	Executer* exec=new Executer();
+//	momdp->Executers.push_back(exec);
+	momdp->window_pub=nh.advertise<geometry_msgs::PolygonStamped>("/my_window",1000);
+	momdp->pa_pub=nh.advertise<geometry_msgs::PoseArray>("my_poses",1000);
+	momdp->car_pub=nh.advertise<geometry_msgs::PoseStamped>("car_pose",1000);
+*/
+	//momdp->simLoop();
+	new WorldSimulator();
+	cout<<"here"<<endl;
+    //ros::spin();
 }
 
 pedestrian_momdp::~pedestrian_momdp()
 {
-    momdp->~ped_momdp();
+    //momdp->~ped_momdp();
 }
 
 void pedestrian_momdp::speedCallback(nav_msgs::Odometry odo)
 {
-    momdp->updateRobotSpeed(odo.twist.twist.linear.x);
+	//cout<<"speed callback!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!"<<endl;
+    //momdp->updateRobotSpeed(odo.twist.twist.linear.x);
+	//RealWorld.UpdateVelReal(odo.twist.twist.linear.x);
 }
 
 void pedestrian_momdp::moveSpeedCallback(geometry_msgs::Twist speed)
 {
-    speed.angular.z = -speed.angular.z;
+
     momdp->updateSteerAnglePublishSpeed(speed);
 
 }
 
 void pedestrian_momdp::pedPoseCallback(ped_momdp_sarsop::ped_local_frame_vector lPedLocal)
 {
+	if(lPedLocal.ped_local.size()==0) return;
+	ped_momdp_sarsop::ped_local_frame ped=lPedLocal.ped_local[0];
+	Car world_car;
+	world_car.w=ped.rob_pose.x*ModelParams::rln;
+	world_car.h=ped.rob_pose.y*ModelParams::rln;
+	//RealWorld.UpdateRobPoseReal(world_car);
+	//update the ped poses
     for(int ii=0; ii< lPedLocal.ped_local.size(); ii++)
     {
+		Pedestrian world_ped;
+		ped_momdp_sarsop::ped_local_frame ped=lPedLocal.ped_local[ii];
+		world_ped.id=ped.ped_id;
+		world_ped.w = ped.ped_pose.x*ModelParams::rln;
+		world_ped.w = ped.ped_pose.y*ModelParams::rln;
+
+			
         /// search for proper pedestrian to update
-        bool foundPed = momdp->updatePedRobPose(lPedLocal.ped_local[ii]);;
+        //bool foundPed = momdp->updatePedRobPose(lPedLocal.ped_local[ii]);;
+		//RealWorld.UpdatePedPoseReal(world_ped);
 
-        if(!foundPed)
-        {
+        //if(!foundPed)
+        //{
             ///if ped_id does not match the old one create a new pomdp problem.
-            ROS_INFO(" Creating  a new pedestrian problem #%d", lPedLocal.ped_local[ii].ped_id);
-            momdp->addNewPed(lPedLocal.ped_local[ii]);
-            //Executer* exec=new Executer();
-            //momdp->Executers.push_back(exec);
-        }
+            //ROS_INFO(" Creating  a new pedestrian problem #%d", lPedLocal.ped_local[ii].ped_id);
+            //momdp->addNewPed(lPedLocal.ped_local[ii]);
+			//ROS_INFO("Create a new pedestrian!", lPedLocal.ped_local[ii].ped_id);
+			//world.addNewPed(lPedLocal.ped_local[ii]);                                                  }
     }
-
-
 }
 
 
@@ -81,6 +111,10 @@ int main(int argc, char** argv)
 {
     ros::init(argc, argv, "mdp");
 
+
+	
+    srand(unsigned(time(0)));
     pedestrian_momdp mdp_node;
+
 
 }
