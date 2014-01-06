9fdbca8e5cc202543a7cef2cb9381a55f1f117e4
baoxing <marskahn@gmail.com>
2014-01-06 19:30:14 +0800

    MODT: try ICP, shows interesting and promising results; need to further improve the performance;       try grid search next step, to see;

--- smart-ros-pkg/golfcar_perception/MODT/src/vehicle_tracking_ICP.h
+++ smart-ros-pkg/golfcar_perception/MODT/src/vehicle_tracking_ICP.h
@@ -1,5 +1,5 @@
-#ifndef MODT_VEHICLE_TRACKING_H
-#define MODT_VEHICLE_TRACKING_H
+#ifndef MODT_VEHICLE_TRACKING_ICP_H
+#define MODT_VEHICLE_TRACKING_ICP_H
 
 #include <sensor_msgs/PointCloud.h>
 #include <ros/ros.h>
@@ -21,8 +21,10 @@ namespace mrpt{
 	
 	class model_free_track
 	{
+		public:
 		int object_id;
 		int type_label;
+		ros::Time update_time;
 		sensor_msgs::PointCloud contour_points;
 		std::vector<geometry_msgs::Point32> anchor_points;
 
@@ -36,10 +38,13 @@ namespace mrpt{
     {
 	     public:
 
+		int object_total_id_;
     	//need a datatype to serve as the track history;
 		std::vector<model_free_track> object_tracks_;
 
     	ros::Subscriber segpose_batch_sub_;
+    	ros::Publisher contour_cloud_pub_, anchor_point_pub_;
+    	ros::Time latest_input_time_;
     	vehicle_tracking();
 		~vehicle_tracking(){};
 
@@ -47,15 +52,19 @@ namespace mrpt{
 
 		//basically data association step, but it can be removed in the futher with a smarter segmentation code;
 		//since data association is already solved implicitly in the pcl segmentation part;
-		void register_cluster2history(const MODT::segment_pose_batches& batches);
+		void register_cluster2history(MODT::segment_pose_batches& batches);
 
 		//perform some tracker's basic operations;
 		//1st: initiate new tracks; 2nd: merge and split; 3rd: update with measurements; 4th: delete obsolete tracks;
-		void update_motionShape();
+		void update_motionShape(MODT::segment_pose_batches& batches, std::vector<std::pair<size_t, size_t> > &track_measure_pairs, std::vector<size_t> &unregistered_measurements);
 
 		//update object's position and shape using ICP;
-		void ICP_motion2shape();
+		void ICP_motion2shape(MODT::segment_pose_batch& old_meas, MODT::segment_pose_batch& new_meas, tf::Pose& oldMeas_poseinOdom, tf::Pose& newMeas_poseinOdom);
 		void construct_ICP_scans(geometry_msgs::Pose &lidar_pose, sensor_msgs::PointCloud &segment_pointcloud, CObservation2DRangeScan& scan);
+
+		void attached_points_transform(geometry_msgs::Point32& pt_input, geometry_msgs::Point32& pt_output, tf::Pose& oldMeas_poseinOdom, tf::Pose& newMeas_poseinOdom);
+
+		void tracks_visualization();
     };
 };
 
