8622ad0eb502edd86109595929fa878b1cc3d84e
baoxing <marskahn@gmail.com>
2014-01-23 18:24:47 +0800

    MODT: try to use SLAM-like method to solve the tracking problem;

---
+++ smart-ros-pkg/golfcar_perception/MODT/src/PF_tracker/OGM_model.h
@@ -0,0 +1,39 @@
+#ifndef MODT_OGM_MODEL_H
+#define MODT_OGM_MODEL_H
+
+#include <sensor_msgs/PointCloud.h>
+#include <ros/ros.h>
+#include <geometry_msgs/PolygonStamped.h>
+#include <tf/transform_broadcaster.h>
+#include <opencv/cv.h>
+#include <opencv/highgui.h>
+
+using namespace cv;
+
+class OGM_model
+{
+	public:
+	cv::Point 	anchor_point;
+	cv::Mat		grid_image;
+	double		resolution;
+
+	OGM_model()
+	{
+		tracker = new constspeed_ekf_tracker();
+		track_status = 0;
+		vehicle_evidence = 0;
+	}
+
+	double measurement_score()
+	{
+
+	}
+
+	void update_model()
+	{
+
+	}
+
+};
+
+#endif
