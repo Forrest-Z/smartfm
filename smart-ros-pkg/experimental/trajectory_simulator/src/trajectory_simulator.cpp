9047b2057889d6862e89986bb37863683fd39ac7
Demian <demian@outlook.com>
2014-01-27 04:50:48 +0800

    Finally, something gets going

--- smart-ros-pkg/experimental/trajectory_simulator/src/trajectory_simulator.cpp
+++ smart-ros-pkg/experimental/trajectory_simulator/src/trajectory_simulator.cpp
@@ -29,12 +29,16 @@ public:
   wheelbase_(wheelbase), time_step_(time_step), time_now_(0.0), pose_(init_pose){}
     
   void SetSpeed(double x, double y, double a){
+    //we want to maintain a fixed distance, not time fixed
+    
     
     vel_.x = x * cos(a);
     vel_.a = x * sin(a)/wheelbase_;
     
-    
-    double dx = vel_.x * time_step_;
+    //figure out the time step for a fixed amount of distance
+    //let's do a 5 cm resolution.
+    double dx = 0.05;
+    time_step_ = dx/vel_.x;
     pose_.a = normalize(pose_.a + vel_.a * time_step_);
     double cosa = cos(pose_.a);
     double sina = sin(pose_.a);
@@ -83,7 +87,63 @@ class TrajectorySimulator{
     pp_-> path_n_ =0;
   }
 public:
+  
+  template <typename T> int sgn(T val) {
+    return (T(0) < val) - (val < T(0));
+}
+
+  double solveCubic(double a, double b, double c, double d){
+    //http://www.1728.org/cubic2.htm
+    if(fabs(a)<1e-9){
+      cout<<fabs(a)<<endl;
+      a=b;
+      b=c;
+      c=d;
+      double temp = b*b - 4*a*c;
+      cout <<a<<" "<<b<<" "<<c<<" "<< temp<<endl;
+      return (-b + sqrt(temp))/(2*a);
+    }
+    double f = ((3*c/a)-((b*b)/(a*a)))/3;
+    double g = ((2*pow(b,3)/pow(a,3))-(9*b*c/pow(a,2))+(27*d/a))/27.0;
+    double h = pow(g,2)/4.0+pow(f,3)/27.0;
+    cout<<"h = "<<h<<endl;
+    if(h > 0){
+      double r = -g/2.0+sqrt(h);
+      double s = pow(r, 1/3.0);
+      double t = -g/2.0-sqrt(h);
+      double u = sgn(t) * pow(fabs(t),1/3.0);
+      return s+u-(b/(3*a));
+    }
+    else {
+      double i = sqrt((pow(g,2)/4)-h);
+      double j = pow(i,(1/3.0));
+      double k = acos(-(g/(2*i)));
+      double l = -j;
+      double m = cos(k/3.0);
+      double n = sqrt(3.0)*sin(k/3.0);
+      double p = -(b/(3*a));
+      double x1= 2*j*cos(k/3.0)-(b/(3*a));
+      double x2= l*(m+n)+p;
+      double x3= l*(m-n)+p;
+      double minx = 999.0;
+      bool match_criteria = false;
+      cout<<x1<<" "<<x2<<" "<<x3<<endl;
+      if(x1 < minx && x1>=0) {minx = x1; match_criteria=true;}
+      if(x2 < minx && x2>=0) {minx = x2; match_criteria=true;}
+      if(x3 < minx && x3>=0) {minx = x3; match_criteria=true;}
+      assert(match_criteria);
+      return minx;
+    }
+    
+    
+  }
+  
   TrajectorySimulator(int argc, char** argv){
+    double a=atof(argv[1]);
+    double b=atof(argv[2]);
+    double c=atof(argv[3]);
+    double d=atof(argv[4]);
+    cout<<solveCubic(a,b,c,d)<<endl;
     ros::init(argc, argv, "trajectory_simulator");
     ros::NodeHandle nh;
     ros::NodeHandle priv_nh("~");
@@ -123,7 +183,8 @@ public:
     bool path_exist = true;
     fmutil::Stopwatch sw("Simulation took");
     sensor_msgs::PointCloud curve_pc, max_speed_pc, speed_pc, acc_pc, jerk_pc;
-    double speed_now = 0, acc_now = 0;
+    vector<int> speed_intervals;
+    double speed_now = 3.0, acc_now = 0;
     while(path_exist && ros::ok()){
       geometry_msgs::PoseStamped p;
       p.header.frame_id =global_frame_;
@@ -135,8 +196,9 @@ public:
       pp_->vehicle_base_ = p.pose;
       double steer_angle, dist_to_goal;
       path_exist = pp_->steering_control(&steer_angle, &dist_to_goal);
-      //using low speed to obtain an over fitted curve for a tighter turn
-      model.SetSpeed(3.0, 0.0, steer_angle);
+      
+      model.SetSpeed(speed_now, 0.0, steer_angle);
+      pp_->updateCommandedSpeed(speed_now);
       
       geometry_msgs::Point32 curve_pt, max_speed_pt, speed_pt, acc_pt, jerk_pt;
       curve_pt.x = model.pose_.x;
@@ -150,24 +212,71 @@ public:
       curve_pt.z = turning_rad;
       max_speed_pt.z = sqrt(max_lat_acc*turning_rad);
       if(max_speed_pt.z > max_speed) max_speed_pt.z = max_speed;
-
-
-      acc_pt.z = (max_speed_pt.z - speed_now)/time_step;
-      double required_jerk = (acc_pt.z - acc_now)/time_step;
-      if( required_jerk > max_jerk)	
-	required_jerk = max_jerk;
-      else if(required_jerk < -max_jerk)
-	required_jerk = -max_jerk;
-      acc_now += required_jerk * time_step;
-      jerk_pt.z = required_jerk;
-      speed_now += acc_now * time_step;
-      speed_pt.z = speed_now;
+      
       curve_pc.points.push_back(curve_pt);
       max_speed_pc.points.push_back(max_speed_pt);
       speed_pc.points.push_back(speed_pt);
       acc_pc.points.push_back(acc_pt);
       jerk_pc.points.push_back(jerk_pt);
-      pp_->updateCommandedSpeed(5.0);
+    }
+    for(int i=0; i<1; i++){
+      int min_speed_idx=0;
+      double min_speed=max_speed_pc.points[0].z;
+      for(size_t j=1; j<max_speed_pc.points.size(); j++){
+	double speed_temp = max_speed_pc.points[j].z ;
+	if(speed_temp< min_speed){
+	  min_speed = speed_temp;
+	  min_speed_idx=j;
+	}
+      }
+      
+      double v1 = 0 + 0.5 * max_acc*max_acc/max_jerk;
+      double v2 = min_speed - max_acc*max_acc*0.5*max_jerk;
+      double v3 = min_speed;
+      
+      int interval_idx = 0;
+      for(size_t j=1; j<max_speed_pc.points.size(); j++){
+	//figure out the time for each segment
+	double jerk = 0;
+	double acc_pt_pre = acc_pc.points[j-1].z;
+	double speed_pt_pre = speed_pc.points[j-1].z;
+	double acc_pt = acc_pc.points[j].z;
+	double speed_pt = speed_pc.points[j].z;
+	double time_step;
+	switch (interval_idx){
+	  case 0:
+	    jerk = max_jerk;
+	    time_step = solveCubic(jerk, acc_pt_pre, speed_pt_pre, -0.05);
+	    acc_pt = acc_pt_pre + time_step * jerk;
+	    speed_pt = speed_pt_pre + time_step * acc_pt;
+	    if(speed_pt > v1) interval_idx++;
+	    break;
+	  case 1:
+	    jerk = 0.0;
+	    time_step = solveCubic(jerk, acc_pt_pre, speed_pt_pre, -0.05);
+	    acc_pt = acc_pt_pre + time_step * jerk;
+	    speed_pt = speed_pt_pre + time_step * acc_pt;
+	    if(speed_pt > v2) interval_idx++;
+	    break;
+	  case 2:
+	    jerk = -max_jerk;
+	    time_step = solveCubic(jerk, acc_pt_pre, speed_pt_pre, -0.05);
+	    acc_pt = acc_pt_pre + time_step * jerk;
+	    speed_pt = speed_pt_pre + time_step * acc_pt;
+	    if(speed_pt > v3) {
+	      interval_idx++;
+	      acc_pt = 0.0;
+	      speed_pt = v3;
+	    }
+	    break;
+	  case 3:
+	    speed_pt = v3;
+	    break;
+	}
+	acc_pc.points[j].z = acc_pt;
+	jerk_pc.points[j].z = jerk;
+	speed_pc.points[j].z = speed_pt;
+      }
     }
     sw.end();
     simulated_path.header.frame_id = global_frame_;
