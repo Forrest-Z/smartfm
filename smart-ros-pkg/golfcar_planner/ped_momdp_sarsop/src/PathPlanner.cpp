9dc32d194e0f75aba5e89cfca662e5fef53f7bf6
Haoyu Bai <baihaoyu@gmail.com>
2014-06-18 13:32:58 +0800

    switch to new pathplan

--- smart-ros-pkg/golfcar_planner/ped_momdp_sarsop/src/PathPlanner.cpp
+++ smart-ros-pkg/golfcar_planner/ped_momdp_sarsop/src/PathPlanner.cpp
@@ -2,6 +2,7 @@
 #include <tf/transform_listener.h>
 #include <costmap_2d/costmap_2d_ros.h>
 #include <navfn/navfn_ros.h>
+#include <ped_pathplan/pathplan_ros.h>
 
 
 int main(int argc, char** argv) {
@@ -10,8 +11,9 @@ int main(int argc, char** argv) {
     tf::TransformListener tf(ros::Duration(10));
     costmap_2d::Costmap2DROS costmap("ped_costmap", tf);
 
-    navfn::NavfnROS navfn;
-    navfn.initialize("planner", &costmap);
+    //navfn::NavfnROS p;
+    ped_pathplan::PathPlanROS p;
+    p.initialize("planner", &costmap);
 
     ros::spin();
     return 0;
