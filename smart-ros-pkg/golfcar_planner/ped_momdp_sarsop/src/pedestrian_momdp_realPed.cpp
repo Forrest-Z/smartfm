6aaf28ddfd3d47328603db55f6390c54b787d686
Haoyu Bai <baihaoyu@gmail.com>
2014-07-03 14:48:16 +0800

    auto adjust frames based on simulation param

--- smart-ros-pkg/golfcar_planner/ped_momdp_sarsop/src/pedestrian_momdp_realPed.cpp
+++ smart-ros-pkg/golfcar_planner/ped_momdp_sarsop/src/pedestrian_momdp_realPed.cpp
@@ -22,9 +22,11 @@ pedestrian_momdp::pedestrian_momdp()
 {
     ROS_INFO("Starting Pedestrian Avoidance ... ");
 
+    
+
 	cerr << "DEBUG: Setting up subscription..." << endl;
-    /// Setting up subsciption
     ros::NodeHandle nh;
+    /// Setting up subsciption
     speedSub_ = nh.subscribe("odom", 1, &pedestrian_momdp::speedCallback, this);
     pedSub_ = nh.subscribe("ped_local_frame_vector", 1, &pedestrian_momdp::pedPoseCallback, this); 
 	//pathSub_=nh.subscribe("global_plan", 1, &pedestrian_momdp::pathCallback,this);
@@ -33,24 +35,15 @@ pedestrian_momdp::pedestrian_momdp()
     ros::NodeHandle n("~");
 
 	pathPublished=false;
-    string  ped_id_file, policy_file, model_file;
-    double frequency;
-    int simLen, simNum;
-    bool use_sim_time, stationary;
-    n.param("pedestrian_id_file", ped_id_file, string(""));
-    n.param("policy_file", policy_file, string(""));
-    n.param("model_file", model_file, string(""));
-    n.param("simLen", simLen, 100);
-    n.param("simNum", simNum, 100);
-    n.param("frequency", frequency, 2.0);
-    n.param("stationary",stationary, false);
-    nh.param("use_sim_time", use_sim_time, false);
+    bool simulation;
+    n.param("simulation", simulation, false);
+    ModelParams::init_params(simulation);
 
     move_base_speed_=nh.subscribe("momdp_speed_dummy",1, &pedestrian_momdp::moveSpeedCallback, this);
     //goalPub_ = nh.advertise<geometry_msgs::PoseStamped>("move_base_simple/goal",1);
 
 	cerr << "DEBUG: Creating ped_momdp instance" << endl;
-	momdp = new ped_momdp(model_file, policy_file, simLen, simNum, stationary, frequency, use_sim_time, nh);
+	momdp = new ped_momdp(nh);
 
 	momdp->window_pub=nh.advertise<geometry_msgs::PolygonStamped>("/my_window",1000);
 	momdp->pa_pub=nh.advertise<geometry_msgs::PoseArray>("my_poses",1000);
@@ -162,7 +155,7 @@ void pedestrian_momdp::pedPoseCallback(ped_momdp_sarsop::ped_local_frame_vector 
 int main(int argc, char** argv)
 {
     ros::init(argc, argv, "mdp");
-	
+
     srand(unsigned(time(0)));
     pedestrian_momdp mdp_node;
 }
