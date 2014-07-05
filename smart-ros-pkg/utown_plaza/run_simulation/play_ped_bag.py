ebfaccf17311147b22dfce930e066d1a087dbc66
Haoyu Bai <baihaoyu@gmail.com>
2014-07-04 23:40:24 -0700

    randomly play bag when running simulation

---
+++ smart-ros-pkg/utown_plaza/run_simulation/play_ped_bag.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python
+import sys
+import os
+import yaml
+import random
+import rosbag
+
+ROSBAG = '/opt/ros/fuerte/bin/rosbag'
+BAGFN = '3394_new.bag'
+
+def get_duration(bagfn):
+    b = rosbag.Bag(bagfn)
+    info = b._get_yaml_info()
+    b.close()
+    info = yaml.load(info)
+    duration = float(info['duration'])
+    return duration
+
+def main(bagfn):
+    t = get_duration(bagfn)
+    start = random.uniform(0, t)
+
+    os.execv(ROSBAG, [ROSBAG, 'play', '-s', str(start), '-l', bagfn])
+
+
+if __name__=='__main__':
+    main(BAGFN)
+    #main(sys.argv[1])
+    #print get_duration(sys.argv[1])
