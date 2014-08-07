854f88ea991c596ab8e0c11facc2344f644b3d34
Haoyu Bai <baihaoyu@gmail.com>
2014-08-07 22:05:24 +0800

    param for sim type

--- run.sh
+++ run.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
 set -e
+
+SIMTYPE=$1
+SIMPARAM=sim_${SIMTYPE}.yaml
+
 cd ~/smartfm/smart-ros-pkg/utown_plaza/run_simulation
+[ -e $SIMPARAM ] || ( echo $SIMPARAM does not exist! && exit -1 )
 
 # setup env
 source /opt/ros/fuerte/setup.bash
@@ -10,9 +15,9 @@ source aws_access.sh
 
 while true; do
     rm -f *.bag
-    roslaunch all.launch > pomdp.log
+    roslaunch all.launch simparam:=${SIMPARAM} > pomdp.log
     python utils.py count
-    s3put -b golfcar.test -p $(pwd) --region ap-southeast-1 -c 100 -k default *.bag
+    s3put -b golfcar.test -p $(pwd) --region ap-southeast-1 -c 100 -k $SIMTYPE *.bag
     rm -f *.bag
     sleep 1
 done
