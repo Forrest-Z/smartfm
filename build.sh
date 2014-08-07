3ce9aeca5932ca41797109c936572196761258a7
Haoyu Bai <baihaoyu@gmail.com>
2014-08-07 16:05:45 +0800

    update scripts

--- build.sh
+++ build.sh
@@ -1,7 +1,19 @@
 #!/bin/bash
+set -e
 
 source env.sh
+
+function build_bookingui {
+    ln -s ~/smartfm/bookingUI/shared/SvgPath ~/
+    cd ~/smartfm/bookingUI/shared
+    cmake .
+    make
+}
+
 cd ~/smartfm/smart-ros-pkg/third_party/geometry/bullet
 make
 
+[ -e ~/SvgPath ] || build_bookingui
+
 rosmake ped_momdp_sarsop ped_pathplan golfcar_ppc
+
---
+++ smart-ros-pkg/utown_plaza/run_simulation/fabfile.py
@@ -0,0 +1,30 @@
+from fabric.api import *
+from fabtools import require
+from fabtools.python import virtualenv
+import fabtools
+from fabtools.utils import run_as_root
+from fabric.contrib.project import rsync_project as rsync
+from fabric.contrib import files
+
+env.use_ssh_config = True
+
+@task
+def ros():
+    require.deb.source('ubuntu-multiverse', 'http://ap-southeast-1.ec2.archive.ubuntu.com/ubuntu/', 'precise', 'multiverse')
+    require.deb.source('ubuntu-multiverse-update', 'http://ap-southeast-1.ec2.archive.ubuntu.com/ubuntu/', 'precise-updates', 'multiverse')
+    require.deb.source('ros', 'http://packages.ros.org/ros/ubuntu', 'precise', 'main')
+    require.deb.key('B01FA116', url='http://packages.ros.org/ros.key')
+    require.deb.uptodate_index()
+    #require.deb.packages(['ros-fuerte-desktop-full', 'libbullet-dev'])
+    require.deb.packages(['colorgcc', 'ros-fuerte-desktop-full', 'ros-fuerte-octomap'])
+
+@task
+def repo():
+    require.git.working_copy('shaojun@bigbird:~/smartfm.git', path='smartfm/', branch='simulation')
+    rsync(remote_dir='~/smartfm/smart-ros-pkg/utown_plaza/run_simulation/pedbags/', local_dir='pedbags/')
+    with cd('~/smartfm'):
+        run('./build.sh')
+
+#@task
+#def setup():
+
